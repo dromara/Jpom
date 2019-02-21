f14967a4f8a77a5cf4b96df03e488aa81215d121
jiangzeyin <bwcx_jzy@163.com>
2019-02-21 13:54:25 +0800

    添加分支

---
+++ script/push.bat
@@ -0,0 +1,27 @@
+@echo off
+
+chcp 65001
+
+cd ../
+
+echo 拉取主分支
+
+call git fetch github master:master
+
+call git fetch gitee master:master
+
+echo 开始切换分支
+call git checkout master
+
+echo 开始合并分支
+call git merge dev
+
+echo 推送到gitee
+call git push gitee master
+
+echo 推送到github
+call git push github dev
+
+call git push github master
+
+call git checkout dev
