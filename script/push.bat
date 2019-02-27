b77bd380d674bb7e26c236e0d0aab7ea7510c1cf
jiangzeyin <bwcx_jzy@163.com>
2019-02-27 21:11:06 +0800

    更新命令

--- script/push.bat
+++ script/push.bat
@@ -4,19 +4,33 @@ chcp 65001
 
 cd ../
 
-echo 拉取主分支
+
+echo 拉取远程分支[master]
+call git checkout dev
 
 call git fetch github master:master
 
 call git fetch gitee master:master
 
-echo 开始切换分支
+echo 拉取远程分支[dev]
 call git checkout master
 
-echo 开始合并分支
+call git fetch github dev:dev
+
+call git fetch gitee dev:dev
+
+echo 开始合并分支[master]
+call git checkout dev
+call git merge master
+
+echo 开始合并分支[dev]
+call git checkout master
 call git merge dev
 
 echo 推送到gitee
+
+call git push gitee dev
+
 call git push gitee master
 
 echo 推送到github
@@ -25,10 +39,8 @@ call git push github dev
 call git push github master
 
 echo 推送tags
+call git push github --tags
 
 call git push gitee --tags
 
-call git push github --tags
-
-echo 切回分支
 call git checkout dev
