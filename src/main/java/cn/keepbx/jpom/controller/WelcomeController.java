439f297578a215503850af77fdbb8804d0babe1e
arno <arnohand@163.com>
2019-03-01 18:48:14 +0800

    首页饼状图

--- src/main/java/cn/keepbx/jpom/controller/WelcomeController.java
+++ src/main/java/cn/keepbx/jpom/controller/WelcomeController.java
@@ -1,8 +1,18 @@
 package cn.keepbx.jpom.controller;
 
+import cn.jiangzeyin.common.DefaultSystemLog;
+import cn.jiangzeyin.common.JsonMessage;
+import cn.jiangzeyin.common.spring.SpringUtil;
+import cn.keepbx.jpom.service.manage.CommandService;
+import cn.keepbx.jpom.socket.top.TopManager;
+import cn.keepbx.jpom.system.ConfigException;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * 欢迎页
@@ -13,9 +23,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/")
 public class WelcomeController extends BaseController {
 
+    @Resource
+    private CommandService commandService;
+
     @RequestMapping(value = "welcome", method = RequestMethod.GET)
     public String welcome() {
         setAttribute("userInfo", getSocketPwd());
         return "welcome";
     }
+
+
+    @RequestMapping(value = "getTop", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String getTop() {
+        if (commandService == null) {
+            commandService = SpringUtil.getBean(CommandService.class);
+        }
+        String topInfo = "";
+        try {
+            String s = commandService.execCommand(CommandService.CommandOp.top, null, null);
+            topInfo = TopManager.getTopInfo(s);
+        } catch (ConfigException e) {
+            DefaultSystemLog.ERROR().error(e.getMessage(), e);
+        }
+        return JsonMessage.getString(200, "", topInfo);
+    }
+
 }
--- src/main/java/cn/keepbx/jpom/socket/top/TopManager.java
+++ src/main/java/cn/keepbx/jpom/socket/top/TopManager.java
@@ -1,11 +1,14 @@
 package cn.keepbx.jpom.socket.top;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.cron.CronUtil;
 import cn.jiangzeyin.common.DefaultSystemLog;
 import cn.jiangzeyin.common.spring.SpringUtil;
 import cn.keepbx.jpom.service.manage.CommandService;
 import cn.keepbx.jpom.socket.SocketSession;
 import cn.keepbx.jpom.system.ConfigException;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 
 import javax.websocket.Session;
 import java.io.IOException;
@@ -65,10 +68,119 @@ public class TopManager {
             } catch (ConfigException e) {
                 DefaultSystemLog.ERROR().error(e.getMessage(), e);
             }
-            send(result);
+            String topInfo = getTopInfo(result);
+            send(topInfo);
         });
         CronUtil.restart();
         watch = true;
+    }
+
+    public static String getTopInfo(String content) {
+        if (StrUtil.isEmpty(content)) {
+            return "top查询失败";
+        }
+        String[] split = content.split("\n");
+        String cpus = split[2];
+        String mem = split[3];
+        JSONArray cpu = getCpu(cpus);
+        JSONArray memory = getMemory(mem);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("cpu", cpu);
+        jsonObject.put("memory", memory);
+        jsonObject.put("top", true);
+        return jsonObject.toJSONString();
+    }
+
+    /**
+     * 获取内存信息
+     *
+     * @param info 内存信息
+     * @return 内存信息
+     */
+    private static JSONArray getMemory(String info) {
+        if (StrUtil.isEmpty(info)) {
+            return null;
+        }
+        int index = info.indexOf(":") + 1;
+        String[] split = info.substring(index).split(",");
+        JSONArray memory = new JSONArray();
+        for (String str : split) {
+            str = str.trim();
+//            509248k total — 物理内存总量（509M）
+//            495964k used — 使用中的内存总量（495M）
+//            13284k free — 空闲内存总量（13M）
+//            25364k buffers — 缓存的内存量 （25M）
+            if (str.endsWith("free")) {
+                memory.add(putObject("用户空间占用", str.replace("free", "").trim()));
+            }
+            if (str.endsWith("used")) {
+                memory.add(putObject("用户空间占用", str.replace("used", "").trim()));
+            }
+            if (str.endsWith("buff/cache")) {
+                memory.add(putObject("用户空间占用", str.replace("buff/cache", "").trim()));
+            }
+        }
+        return memory;
+    }
+
+    /**
+     * 获取cpu信息
+     *
+     * @param info cpu信息
+     * @return cpu信息
+     */
+    private static JSONArray getCpu(String info) {
+        if (StrUtil.isEmpty(info)) {
+            return null;
+        }
+        int i = info.indexOf(":");
+        String[] split = info.substring(i + 1).split(",");
+        JSONArray cpu = new JSONArray();
+        for (String str : split) {
+            str = str.trim();
+//            1.3% us — 用户空间占用CPU的百分比。
+//            1.0% sy — 内核空间占用CPU的百分比。
+//            0.0% ni — 改变过优先级的进程占用CPU的百分比
+//            97.3% id — 空闲CPU百分比
+//            0.0% wa — IO等待占用CPU的百分比
+//            0.3% hi — 硬中断（Hardware IRQ）占用CPU的百分比
+//            0.0% si — 软中断（Software Interrupts）占用CPU的百分比
+            String value = str.substring(0, str.length() - 2).trim();
+            String tag = str.substring(str.length() - 2);
+            switch (tag) {
+                case "us":
+                    cpu.add(putObject("用户空间占用", value));
+                    break;
+                case "sy":
+                    cpu.add(putObject("内核空间占用", value));
+                    break;
+                case "ni":
+                    cpu.add(putObject("改变过优先级的进程占用", value));
+                    break;
+                case "id":
+                    cpu.add(putObject("空闲CPU占用", value));
+                    break;
+                case "wa":
+                    cpu.add(putObject("IO等待占用", value));
+                    break;
+                case "hi":
+                    cpu.add(putObject("硬中断占用", value));
+                    break;
+                case "si":
+                    cpu.add(putObject("软中断占用占用", value));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return cpu;
+    }
+
+    private static JSONObject putObject(String name, Object value) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", name);
+        jsonObject.put("value", value);
+        return jsonObject;
     }
 
     /**
