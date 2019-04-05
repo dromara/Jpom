aa47b722c24847505a56da685deae580f7074f55
jiangzeyin <bwcx_jzy@163.com>
2019-04-05 12:04:16 +0800

    更新文件

--- script/Jpom.bat
+++ script/Jpom.bat
@@ -1,22 +1,24 @@
-@REM ----------------------------------------------------------------------------
-@REM  Copyright 2001-2006 The Apache Software Foundation.
+@REM The MIT License (MIT)
 @REM
-@REM  Licensed under the Apache License, Version 2.0 (the "License");
-@REM  you may not use this file except in compliance with the License.
-@REM  You may obtain a copy of the License at
+@REM Copyright (c) 2019 不忘初心
 @REM
-@REM       http://www.apache.org/licenses/LICENSE-2.0
+@REM Permission is hereby granted, free of charge, to any person obtaining a copy of
+@REM this software and associated documentation files (the "Software"), to deal in
+@REM the Software without restriction, including without limitation the rights to
+@REM use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+@REM the Software, and to permit persons to whom the Software is furnished to do so,
+@REM subject to the following conditions:
 @REM
-@REM  Unless required by applicable law or agreed to in writing, software
-@REM  distributed under the License is distributed on an "AS IS" BASIS,
-@REM  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@REM  See the License for the specific language governing permissions and
-@REM  limitations under the License.
-@REM ----------------------------------------------------------------------------
+@REM The above copyright notice and this permission notice shall be included in all
+@REM copies or substantial portions of the Software.
 @REM
-@REM   Copyright (c) 2001-2006 The Apache Software Foundation.  All rights
-@REM   reserved.
-
+@REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+@REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+@REM FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+@REM COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+@REM IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+@REM CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+@REM
 
 @echo off
 setlocal enabledelayedexpansion
