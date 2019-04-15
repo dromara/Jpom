083c8b83a260b8437863cbff209e754fbc840a9d
jiangzeyin <bwcx_jzy@163.com>
2019-04-15 09:56:11 +0800

    在线代码审查，codacy

--- src/main/java/cn/keepbx/jpom/controller/manage/ConsoleController.java
+++ src/main/java/cn/keepbx/jpom/controller/manage/ConsoleController.java
@@ -1,7 +1,5 @@
 package cn.keepbx.jpom.controller.manage;
 
-import cn.hutool.extra.servlet.ServletUtil;
-import cn.jiangzeyin.common.JsonMessage;
 import cn.keepbx.jpom.common.BaseController;
 import cn.keepbx.jpom.model.ProjectInfoModel;
 import cn.keepbx.jpom.model.UserModel;
@@ -10,10 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 
 /**
