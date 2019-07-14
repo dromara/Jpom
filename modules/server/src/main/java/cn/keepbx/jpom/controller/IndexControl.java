80310805a01f3103fcc2b811d580fda77f371508
bwcx_jzy <bwcx_jzy@163.com>
2019-07-14 19:28:47 +0800

    菜单使用json配置

--- modules/server/src/main/java/cn/keepbx/jpom/controller/IndexControl.java
+++ modules/server/src/main/java/cn/keepbx/jpom/controller/IndexControl.java
@@ -1,5 +1,8 @@
 package cn.keepbx.jpom.controller;
 
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.CharsetUtil;
 import cn.keepbx.jpom.common.BaseServerController;
 import cn.keepbx.jpom.common.GlobalDefaultExceptionHandler;
 import cn.keepbx.jpom.common.interceptor.NotLogin;
@@ -7,13 +10,17 @@ import cn.keepbx.jpom.model.data.NodeModel;
 import cn.keepbx.jpom.model.system.JpomManifest;
 import cn.keepbx.jpom.service.node.NodeService;
 import cn.keepbx.jpom.service.user.UserService;
+import com.alibaba.fastjson.JSONArray;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 首页
@@ -52,7 +59,15 @@ public class IndexControl extends BaseServerController {
         setAttribute("jpomManifest", JpomManifest.getInstance());
         // 是否显示节点分发菜单
         List<NodeModel> list = nodeService.list();
-        setAttribute("showOutGiving", list != null && list.size() > 1);
+        Map<String, Boolean> dynamic = new HashMap<>(10);
+        dynamic.put("showOutGiving", list != null && list.size() > 1);
+        // 动态
+        setAttribute("dynamicMenus", dynamic);
+        // 菜单
+        InputStream inputStream = ResourceUtil.getStream("classpath:/menus/index.json");
+        String json = IoUtil.read(inputStream, CharsetUtil.CHARSET_UTF_8);
+        JSONArray jsonArray = JSONArray.parseArray(json);
+        setAttribute("menus", jsonArray);
         return "index";
     }
 
