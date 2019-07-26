bd7ecb34bf130bd0535c4c5c45c7ed185cee6b3d
bwcx_jzy <bwcx_jzy@163.com>
2019-07-26 11:33:24 +0800

    push

---
+++ script/push.bat
@@ -0,0 +1,20 @@
+@REM Jpom多分支、多远端合并代码命令
+
+@echo off
+
+cd ../
+
+
+echo 推送到gitee
+
+
+call git push gitee master
+
+echo 推送到github
+
+call git push github master
+
+echo 推送tags
+call git push github --tags
+
+call git push gitee --tags
