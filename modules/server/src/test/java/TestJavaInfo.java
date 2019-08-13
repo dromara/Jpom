28bc40aa381baa4190e730ac343824f786ad7383
bwcx_jzy <bwcx_jzy@163.com>
2019-08-13 10:22:30 +0800

    代码兼容jdk11

--- modules/server/src/test/java/TestJavaInfo.java
+++ modules/server/src/test/java/TestJavaInfo.java
@@ -1,9 +1,9 @@
 import cn.hutool.system.SystemUtil;
+import cn.keepbx.util.JvmUtil;
 import com.sun.tools.attach.AgentInitializationException;
 import com.sun.tools.attach.AgentLoadException;
 import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
-import sun.management.ConnectorAddressLink;
 
 import java.io.IOException;
 
@@ -11,13 +11,13 @@ import java.io.IOException;
  * Created by jiangzeyin on 2019/3/20.
  */
 public class TestJavaInfo {
-    public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException {
+    public static void main(String[] args) throws IOException, AttachNotSupportedException, AgentLoadException, AgentInitializationException, ClassNotFoundException {
         System.out.println(SystemUtil.getJavaRuntimeInfo().getHomeDir());
-        VirtualMachine virtualMachine = VirtualMachine.attach("16772");
+//        VirtualMachine virtualMachine = VirtualMachine.attach("16772");
 //        String agent = StrUtil.format("{}{}lib{}management-agent.jar", SystemUtil.getJavaRuntimeInfo().getHomeDir(), File.separator, File.separator);
 //        virtualMachine.loadAgent(agent);
 //        virtualMachine.loadAgent(agent);
-        ConnectorAddressLink.export(String.valueOf(12044));
-        System.out.println(ConnectorAddressLink.importFrom(12044));
+        JvmUtil.importFrom(7292);
+        System.out.println(JvmUtil.importFrom(7292));
     }
 }
