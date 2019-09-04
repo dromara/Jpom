b24381301189cd97906eb0ae8c8b911eae275b85
bwcx_jzy <bwcx_jzy@163.com>
2019-09-04 02:01:17 -0500

    调整cookie

---
+++ modules/common/src/test/java/cn/ContiPerTest.java
@@ -0,0 +1,40 @@
+package cn;
+
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.http.HttpUtil;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.UUID;
+
+/**
+ * @author bwcx_jzy
+ * @date 2019/9/3
+ */
+public class ContiPerTest {
+    @Rule
+    public ContiPerfRule i = new ContiPerfRule();
+
+    @Test
+    @PerfTest(invocations = 200000000, threads = 16)
+    public void test1() throws Exception {
+        IdUtil.fastSimpleUUID();
+    }
+
+
+    @Test
+    @PerfTest(invocations = 200000000, threads = 16)
+    public void test2() throws Exception {
+        UUID.randomUUID().toString();
+    }
+
+    @Test
+    @PerfTest(invocations = 20000, threads = 16)
+    public void testHttp() {
+        HttpUtil.createGet("https://baidu.com")
+                .timeout(2000)
+                .execute();
+    }
+}
---
+++ modules/common/src/test/java/cn/TestT.java
@@ -0,0 +1,14 @@
+package cn;
+
+import java.time.Duration;
+
+/**
+ * @author bwcx_jzy
+ * @date 2019/9/4
+ */
+public class TestT {
+    public static void main(String[] args) {
+        Duration parse = Duration.parse("PT1H");
+        System.out.println(parse);
+    }
+}
