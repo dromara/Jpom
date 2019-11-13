c84f8f3621e029bdec4ce2c16aba8b21082d75cd
bwcx_jzy <bwcx_jzy@163.com>
2019-11-13 16:25:54 +0800

    类名

--- modules/common/src/test/java/cn/TestTailer.java
+++ modules/common/src/test/java/cn/TestTailer.java
@@ -5,7 +5,7 @@ import cn.hutool.core.io.file.Tailer;
 
 import java.io.File;
 
-public class jiangzeyin {
+public class TestTailer {
     public static void main(String[] args) {
 
         Tailer tailer = new Tailer(new File("D:\\Idea\\Jpom\\modules\\server\\target\\classes\\log\\request\\request-2019-07-21.0.log"), new LineHandler() {
