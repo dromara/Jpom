6aeca3da6ee315cbba734cbc453fdc6db50e21fc
bwcx_jzy <zeyin.jiang@aylaso.com>
2020-06-19 18:44:32 +0800

    修护构建命令中判断是否包含【rm、del、rd】bug

--- modules/common/src/main/java/io/jpom/util/CommandUtil.java
+++ modules/common/src/main/java/io/jpom/util/CommandUtil.java
@@ -146,7 +146,7 @@ public class CommandUtil {
             }
         } else {
             for (String s : split) {
-                if (StrUtil.containsIgnoreCase(s, "rm")) {
+                if (StrUtil.containsAnyIgnoreCase(s, "rm", "\rm")) {
                     return true;
                 }
             }
