d05cefed3db917b975b8798ec02cfb1d0d391d7f
Eleven <idiotalex@163.com>
2020-11-01 19:37:30 +0800

    更换前端框架  reat => vue

--- web-vue/src/layouts/BlankLayout.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Layout = ({ children }) => <>{children}</>;
-
-export default Layout;
---
+++ web-vue/vue.config.js
@@ -0,0 +1,5 @@
+// vue.config.js
+module.exports = {
+  // 输出目录
+  outputDir: "../modules/server/src/main/resources/dist"
+}
