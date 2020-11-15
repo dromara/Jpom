1497d577835d341c2eba288b8b9751221e674085
Eleven <idiotalex@163.com>
2020-11-15 23:28:58 +0800

    添加 node api

--- web-vue/src/api/node.js
+++ web-vue/src/api/node.js
@@ -1,3 +1,18 @@
 import axios from './config';
 
-// 
+// 分组
+export function getNodeGroupList() {
+  return axios({
+    url: '/node/list_group.json',
+    method: 'post'
+  })
+}
+
+// node 列表
+export function getNodeList(params) {
+  return axios({
+    url: '/node/list_data.json',
+    method: 'post',
+    data: params
+  })
+}
