fc5da5c9834273ec04732524bc3366342f6f013d
idiotalex@163.com <idiotalex@163.com>
2021-01-14 13:52:40 +0800

    remove getProjectData api, because it was exist

--- modules/server/src/main/java/io/jpom/controller/node/manage/ProjectManageControl.java
+++ modules/server/src/main/java/io/jpom/controller/node/manage/ProjectManageControl.java
@@ -103,19 +103,6 @@ public class ProjectManageControl extends BaseServerController {
     }
 
     /**
-     * @author Hotstrip
-     * get project group
-     * 获取项目的分组信息
-     * @return
-     */
-    @RequestMapping(value = "project-group-list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String projectGroupList() {
-        List<String> allGroup = projectInfoService.getAllGroup(getNode());
-        return JsonMessage.getString(200, "success", allGroup);
-    }
-
-    /**
      * 查询所有项目
      *
      * @return json
