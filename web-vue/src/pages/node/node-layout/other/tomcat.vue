614233efcdde1f1c2b7a05c9364ed80151130191
Eleven <idiotalex@163.com>
2021-01-22 14:53:23 +0800

    add tomcat page

--- web-vue/src/pages/node/node-layout/other/tomcat.vue
+++ web-vue/src/pages/node/node-layout/other/tomcat.vue
@@ -1,13 +1,76 @@
 <template>
   <div>
-    Tomcat Page
+    <div ref="filter" class="filter">
+      <a-button type="primary" @click="handleAdd">新增</a-button>
+      <a-button type="primary" @click="handleFilter">刷新</a-button>
+    </div>
+    <!-- 数据表格 -->
+    <a-table :data-source="list" :loading="loading" :columns="columns" :scroll="{x: '80vw', y: 500}" :pagination="false" bordered :rowKey="(record, index) => index">
+      <a-tooltip slot="name" slot-scope="text" placement="topLeft" :title="text">
+        <span>{{ text }}</span>
+      </a-tooltip>
+      <a-switch slot="status" slot-scope="text" :checked="text" disabled checked-children="开" un-checked-children="关"/>
+      <a-tooltip slot="port" slot-scope="text" placement="topLeft" :title="text">
+        <span>{{ text }}</span>
+      </a-tooltip>
+      <template slot="operation" slot-scope="text, record">
+        <a-button type="primary" @click="handleEdit(record)">编辑</a-button>
+        <a-button type="primary" @click="handleFile(record)">文件</a-button>
+        <a-button type="primary" @click="handleConsole(record)">控制台</a-button>
+        <a-button type="primary" @click="handleMonitor(record)">监控</a-button>
+        <a-button type="primary" @click="handleReplica(record)">副本集</a-button>
+        <a-button type="danger" @click="handleDelete(record)">删除</a-button>
+      </template>
+    </a-table>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      loading: false,
+      list: [],
+      temp: {},
+      editTomcatVisible: false,
+      columns: [
+        {title: '项目名称', dataIndex: 'name', width: 150, ellipsis: true, scopedSlots: {customRender: 'name'}},
+        {title: '创建时间', dataIndex: 'createTime', width: 180, ellipsis: true, scopedSlots: {customRender: 'createTime'}},
+        {title: '修改时间', dataIndex: 'modifyTime', width: 180, ellipsis: true, scopedSlots: {customRender: 'modifyTime'}},
+        {title: '最后操作人', dataIndex: 'modifyUser', width: 150, ellipsis: true, scopedSlots: {customRender: 'modifyUser'}},
+        {title: '运行状态', dataIndex: 'status', width: 100, ellipsis: true, scopedSlots: {customRender: 'status'}},
+        {title: 'PID', dataIndex: 'pid', width: 100, ellipsis: true, scopedSlots: {customRender: 'pid'}},
+        {title: '端口', dataIndex: 'port', width: 100, ellipsis: true, scopedSlots: {customRender: 'port'}},
+        {title: '操作', dataIndex: 'operation', scopedSlots: {customRender: 'operation'}, width: 500}
+      ],
     }
+  },
+  mounted() {
+    this.handleFilter();
+  },
+  methods: {
+    // 加载数据
+    loadData() {
+
+    },
+    // 筛选
+    handleFilter() {
+      this.loadData();
+    },
+    // 添加
+    handleAdd() {
+      this.temp = {
+        type: 'add'
+      };
+      this.editTomcatVisible = true;
+    },
   }
 }
 </script>
+<style scoped>
+.filter {
+  margin-bottom: 10px;
+}
+.ant-btn {
+  margin-right: 10px;
+}
+</style>
