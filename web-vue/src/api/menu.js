321d52d268f805fe3b4d0864c38d0d69dcb6d219
bwcx_jzy <zeyin.jiang@aylaso.com>
2021-03-02 14:31:45 +0800

    fix page menus api url

--- web-vue/src/api/menu.js
+++ web-vue/src/api/menu.js
@@ -3,18 +3,18 @@ import axios from './config';
 // 获取系统菜单列表
 export function getSystemMenu() {
   return axios({
-    url: 'menus_data.json',
+    url: '/menus_data.json',
     method: 'post'
   })
 }
 
 /**
  * 节点菜单
- * @param {String} nodeId 
+ * @param {String} nodeId
  */
 export function getNodeMenu(nodeId) {
   return axios({
-    url: 'menus_data.json',
+    url: '/menus_data.json',
     method: 'post',
     data: {nodeId}
   })
