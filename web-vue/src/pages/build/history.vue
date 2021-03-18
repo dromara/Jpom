f1aaa6ac8e366983fb91c2f4d36145cacdc19360
bwcx_jzy <bwcx_jzy@163.com>
2021-03-18 16:46:58 +0800

    构建历史日志弹窗宽度

--- web-vue/src/pages/build/history.vue
+++ web-vue/src/pages/build/history.vue
@@ -40,7 +40,7 @@
       </template>
     </a-table>
     <!-- 构建日志 -->
-    <a-modal v-model="buildLogVisible" title="构建日志" :footer="null" :maskClosable="false" @cancel="closeBuildLogModel">
+    <a-modal :width="'80vw'" v-model="buildLogVisible" title="构建日志" :footer="null" :maskClosable="false" @cancel="closeBuildLogModel">
       <build-log v-if="buildLogVisible" :temp="temp" />
     </a-modal>
   </div>
