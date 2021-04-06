87d1dfffa93f841b512ab5a737ec764c69ae13a7
idiotalex@163.com <idiotalex@163.com>
2021-04-06 16:27:21 +0800

    add user operate monitor dialog component

--- web-vue/src/pages/monitor/operate-log.vue
+++ web-vue/src/pages/monitor/operate-log.vue
@@ -20,17 +20,52 @@
         <a-button type="danger" @click="handleDelete(record)">删除</a-button>
       </template>
     </a-table>
+    <!-- 编辑区 -->
+    <a-modal v-model="editOperateMonitorVisible" width="600px" title="编辑监控" @ok="handleEditOperateMonitorOk" :maskClosable="false">
+      <a-form-model ref="editMonitorForm" :rules="rules" :model="temp" :label-col="{ span: 4 }" :wrapper-col="{ span: 18 }">
+        <a-form-model-item label="监控名称" prop="name">
+          <a-input v-model="temp.name" placeholder="监控名称"/>
+        </a-form-model-item>
+        <a-form-model-item label="开启状态" prop="status">
+          <a-switch v-model="temp.status" checked-children="开" un-checked-children="关"/>
+        </a-form-model-item>
+        <a-form-model-item label="监控用户" prop="monitorUser">
+          <a-transfer
+            :data-source="monitorUserList"
+            show-search
+            :filter-option="filterOption"
+            :target-keys="monitorUserKeys"
+            :render="item => item.title"
+            @change="handleMonitorUserChange"
+          />
+        </a-form-model-item>
+        <a-form-model-item label="报警联系人" prop="notifyUser">
+          <a-transfer
+            :data-source="userList"
+            show-search
+            :filter-option="filterOption"
+            :target-keys="notifyUserKeys"
+            :render="item => item.title"
+            @change="handleNotifyUserChange"
+          />
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
   </div>
 </template>
 <script>
 import { getMonitorOperateLogList } from '../../api/monitor';
+import { getAdminUserList } from '../../api/user';
 import { parseTime } from '../../utils/time';
 export default {
   data() {
     return {
       loading: false,
       list: [],
+      userList: [],
       temp: {},
+      notifyUserKeys: [],
+      monitorUserKeys: [],
       editOperateMonitorVisible: false,
       columns: [
         {title: '名称', dataIndex: 'name', scopedSlots: {customRender: 'name'}, width: 150},
@@ -51,8 +86,19 @@ export default {
       }
     }
   },
+  computed: {
+    monitorUserList() {
+      // 深拷贝数组对象，修改属性
+      const list = JSON.parse(JSON.stringify(this.userList));
+      list.forEach(ele => {
+        ele.disabled = false;
+      });
+      return list;
+    }
+  },
   created() {
     this.loadData();
+    this.loadUserList();
   },
   methods: {
     // 加载数据
@@ -65,10 +111,44 @@ export default {
         this.loading = false;
       })
     },
+    // 加载用户列表
+    loadUserList() {
+      this.userList = [];
+      getAdminUserList().then(res => {
+        if (res.code === 200) {
+          res.data.forEach(element => {
+            this.userList.push({key: element.value, title: element.title, disabled:element.disabled});
+          });
+        }
+      })
+    },
     // 新增
     handleAdd() {
+      this.loadUserList();
       this.editOperateMonitorVisible = true;
     },
+    // 修改
+    handleEdit(record) {
+      this.loadUserList();
+      this.temp = Object.assign(record);
+      this.editOperateMonitorVisible = true;
+    },
+    // 穿梭框筛选
+    filterOption(inputValue, option) {
+      return option.title.indexOf(inputValue) > -1;
+    },
+    // 穿梭框 change
+    handleNotifyUserChange(targetKeys) {
+      this.notifyUserKeys = targetKeys;
+    },
+    // 穿梭框 change
+    handleMonitorUserChange(targetKeys) {
+      this.monitorUserKeys = targetKeys;
+    },
+    // 提交
+    handleEditOperateMonitorOk() {
+
+    }
   }
 }
 </script>
