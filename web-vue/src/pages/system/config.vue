cec07596b02eca51484a311308223318b9795978
Eleven <idiotalex@163.com>
2021-04-17 15:52:09 +0800

    fix system config css style while on small screen maybe overflow

--- web-vue/src/pages/system/config.vue
+++ web-vue/src/pages/system/config.vue
@@ -3,7 +3,7 @@
     <a-tab-pane key="1" tab="系统配置">
       <a-form-model ref="editForm" :model="temp" :label-col="{ span: 2 }" :wrapper-col="{ span: 20 }">
         <a-form-model-item label="配置内容" prop="content">
-          <a-input v-model="temp.content" type="textarea" :rows="25" style="resize: none" placeholder="请输入配置内容，参考项目的配置文件"/>
+          <a-input v-model="temp.content" type="textarea" :rows="25" class="content-config" placeholder="请输入配置内容，参考项目的配置文件"/>
         </a-form-model-item>
         <a-form-model-item :wrapper-col="{ span: 14, offset: 2 }">
           <a-button type="primary" class="btn" :disabled="submitAble" @click="onSubmit(false)">保存</a-button>
@@ -14,10 +14,10 @@
     <a-tab-pane key="2" tab="IP白名单配置">
         <a-form-model ref="editForm" :model="temp" :label-col="{ span: 2 }" :wrapper-col="{ span: 20 }">
           <a-form-model-item label="IP白名单" prop="content">
-            <a-input v-model="ipTemp.allowed" type="textarea" :rows="10" style="resize: none" placeholder="请输入IP白名单,多个使用换行"/>
+            <a-input v-model="ipTemp.allowed" type="textarea" :rows="10" class="ip-list-config" placeholder="请输入IP白名单,多个使用换行"/>
           </a-form-model-item>
           <a-form-model-item label="IP黑名单" prop="content">
-            <a-input v-model="ipTemp.prohibited" type="textarea" :rows="10" style="resize: none" placeholder="请输入IP黑名单,多个使用换行"/>
+            <a-input v-model="ipTemp.prohibited" type="textarea" :rows="10" class="ip-list-config" placeholder="请输入IP黑名单,多个使用换行"/>
           </a-form-model-item>
           <a-form-model-item :wrapper-col="{ span: 14, offset: 2 }">
             <a-button type="primary" class="btn" :disabled="submitAble">保存</a-button>
@@ -74,6 +74,19 @@ export default {
 }
 </script>
 <style scoped>
+.ant-tabs-tabpane-active {
+  height: calc(100vh - 200px);
+  overflow-y: scroll;
+}
+.content-config {
+  max-height: calc(100vh - 295px);
+}
+.ip-list-config {
+  max-height: calc((100vh - 320px) / 2);
+}
+textarea {
+  resize: none;
+}
 .btn {
   margin-left: 20px;
 }
