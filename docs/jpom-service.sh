cf2a936d55156f25ad8393c1085177d534604419
idiotalex@163.com <idiotalex@163.com>
2021-04-19 16:24:05 +0800

    add jpom-service.sh file

---
+++ docs/jpom-service.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# chkconfig: 356 10 90
+# description: Jpom-Server service
+# processname: jpom-server
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 码之科技工作室
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# make sure sh could load enviroment variables, in particular, in the init.d directory
+source /etc/profile
+
+ROOT_PATH="$1"
+
+# 启动程序
+function start() {
+    $ROOT_PATH/Server.sh start
+}
+
+# 停止程序
+function stop() {
+    $ROOT_PATH/Server.sh stop
+    status
+}
+
+# 获取程序状态
+function status() {
+    $ROOT_PATH/Server.sh status
+}
+
+# 提示使用语法
+function usage() {
+    echo "Usage: $0 {start|stop|restart|status|create}"
+    RETVAL="2"
+}
+
+# See how we were called.
+RETVAL="0"
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    status)
+        status
+        ;;
+    *)
+      usage
+      ;;
+esac
+
+exit $RETVAL
