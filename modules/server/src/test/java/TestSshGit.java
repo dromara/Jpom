fdc696b2be030aba5889a672dd2937147ed42c0e
bwcx_jzy <bwcx_jzy@163.com>
2021-07-22 10:48:24 +0800

    jgit ssh

--- modules/server/src/test/java/TestSshGit.java
+++ modules/server/src/test/java/TestSshGit.java
@@ -1,60 +1,59 @@
-//import com.jcraft.jsch.JSch;
-//import com.jcraft.jsch.JSchException;
-//import com.jcraft.jsch.Session;
-//import org.eclipse.jgit.api.Git;
-//import org.eclipse.jgit.api.TransportConfigCallback;
-//import org.eclipse.jgit.api.errors.GitAPIException;
-//import org.eclipse.jgit.errors.TransportException;
-//import org.eclipse.jgit.lib.TextProgressMonitor;
-//import org.eclipse.jgit.transport.*;
-//import org.eclipse.jgit.transport.JschConfigSessionFactory;
-//import org.eclipse.jgit.transport.OpenSshConfig;
-//import org.eclipse.jgit.util.FS;
-//import org.junit.Test;
-//
-//import java.io.File;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-///**
-// *
-// * https://qa.1r1g.com/sf/ask/875171671/
-// *
-// * @author bwcx_jzy
-// * @date 2019/7/10
-// **/
-//public class TestSshGit {
-//    public static void main(String[] args) throws GitAPIException, IOException {
-////        Git.cloneRepository()
-////                .setURI("git@gitee.com:jiangzeyin/test.git").setProgressMonitor(new TextProgressMonitor(new PrintWriter(System.out)))
-////                .setDirectory(new File("D:\\test\\gitssh"))
-////                .setTransportConfigCallback(new TransportConfigCallback() {
-////                    @Override
-////                    public void configure(Transport transport) {
-////                        SshTransport sshTransport = (SshTransport) transport;
-////                        sshTransport.setSshSessionFactory(new JschConfigSessionFactory() {
-////                            @Override
-////                            protected void configure(OpenSshConfig.Host hc, Session session) {
-////                                session.setConfig("StrictHostKeyChecking", "no");
-////                                //session.setPassword("");
-////                            }
-////
-////                            @Override
-////                            protected JSch createDefaultJSch(FS fs) throws JSchException {
-////                                JSch defaultJSch = super.createDefaultJSch(fs);
-////
-////                                defaultJSch.addIdentity("C:\\Users\\User\\.ssh\\id_rsa");
-////                                return defaultJSch;
-////                            }
-////
-////                        });
-////                    }
-////                })
-////                .call();
-//    }
-//
-//    @Test
-//    public void test2() {
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.TransportConfigCallback;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.TextProgressMonitor;
+import org.eclipse.jgit.transport.JschConfigSessionFactory;
+import org.eclipse.jgit.transport.OpenSshConfig;
+import org.eclipse.jgit.transport.SshTransport;
+import org.eclipse.jgit.transport.Transport;
+import org.eclipse.jgit.util.FS;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * https://qa.1r1g.com/sf/ask/875171671/
+ *
+ * @author bwcx_jzy
+ * @date 2019/7/10
+ **/
+public class TestSshGit {
+    public static void main(String[] args) throws GitAPIException, IOException {
+        Git.cloneRepository()
+                .setURI("git@gitee.com:jiangzeyin/test.git").setProgressMonitor(new TextProgressMonitor(new PrintWriter(System.out)))
+                .setDirectory(new File("D:\\test\\gitssh"))
+                .setTransportConfigCallback(new TransportConfigCallback() {
+                    @Override
+                    public void configure(Transport transport) {
+                        System.out.println("ssssssssssssssssssssssss");
+                        SshTransport sshTransport = (SshTransport) transport;
+                        sshTransport.setSshSessionFactory(new JschConfigSessionFactory() {
+                            @Override
+                            protected void configure(OpenSshConfig.Host hc, Session session) {
+                                session.setConfig("StrictHostKeyChecking", "no");
+                            }
+
+                            @Override
+                            protected JSch createDefaultJSch(FS fs) throws JSchException {
+                                JSch jSch = super.createDefaultJSch(fs);
+                                //添加私钥文件
+                                jSch.addIdentity("ss");
+
+                                return jSch;
+                            }
+                        });
+                    }
+                })
+                .call();
+    }
+
+    @Test
+    public void test2() {
 //        SshSessionFactory sshSessionFactory = new SshSessionFactory() {
 //            @Override
 //            public RemoteSession getSession(URIish uri, CredentialsProvider credentialsProvider, FS fs, int tms) throws TransportException {
@@ -93,5 +92,5 @@
 //                session.setUserInfo(userInfo);
 //            }
 //        };
-//    }
-//}
+    }
+}
