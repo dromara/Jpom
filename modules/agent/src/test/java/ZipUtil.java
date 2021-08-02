cb185f6e665e6249f0d82ded96adb7be9b564661
Arno <arnohand@163.com>
2021-08-02 09:49:25 +0800

    文件解压

--- modules/agent/src/test/java/ZipUtil.java
+++ modules/agent/src/test/java/ZipUtil.java
@@ -1,14 +1,13 @@
 import io.jpom.util.CompressionFileUtil;
 
 import java.io.File;
-import java.util.List;
 
 public class ZipUtil {
 
-    private static final int BUFFER_SIZE = 1024 * 100;
+	private static final int BUFFER_SIZE = 1024 * 100;
 
-    public static void main(String[] args) throws Exception {
-        List<String> list = CompressionFileUtil.unCompress(new File("C:\\Users\\Colorful\\Desktop\\Desktop.tar.bz2"), new File("C:\\Users\\Colorful\\Desktop\\Desktop.tar.gz11\\"));
-        System.out.println(list);
-    }
+	public static void main(String[] args) throws Exception {
+		CompressionFileUtil.unCompress(new File("C:\\Users\\Colorful\\Desktop\\Desktop.tar.bz2"), new File("C:\\Users\\Colorful\\Desktop\\Desktop.tar.gz11\\"));
+
+	}
 }
