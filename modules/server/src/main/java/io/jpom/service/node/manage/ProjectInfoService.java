0968217128a68550367af1e744705ded5e104246
bwcx_jzy <bwcx_jzy@163.com>
2021-08-09 17:31:05 +0800

    查询权限列表，不拉取项目状态

--- modules/server/src/main/java/io/jpom/service/node/manage/ProjectInfoService.java
+++ modules/server/src/main/java/io/jpom/service/node/manage/ProjectInfoService.java
@@ -23,42 +23,47 @@ import java.util.List;
 @Service
 public class ProjectInfoService implements BaseDynamicService {
 
-    @Resource
-    private NodeService nodeService;
+	@Resource
+	private NodeService nodeService;
 
-    @Override
-    public JSONArray listToArray(String dataId) {
-        NodeModel item = nodeService.getItem(dataId);
-        return listAll(item, null);
-    }
+	@Override
+	public JSONArray listToArray(String dataId) {
+		NodeModel item = nodeService.getItem(dataId);
+		return listAll(item, null);
+	}
 
 
-    public JSONArray listAll(NodeModel nodeModel, HttpServletRequest request) {
-        if (!nodeModel.isOpenStatus()) {
-            return null;
-        }
-        JSONArray jsonArray = NodeForward.requestData(nodeModel, NodeUrl.Manage_GetProjectInfo, request, JSONArray.class);
-        return filter(jsonArray, ClassFeature.PROJECT);
-    }
+	public JSONArray listAll(NodeModel nodeModel, HttpServletRequest request) {
+		if (!nodeModel.isOpenStatus()) {
+			return null;
+		}
+		JSONArray jsonArray;
+		if (request != null) {
+			jsonArray = NodeForward.requestData(nodeModel, NodeUrl.Manage_GetProjectInfo, request, JSONArray.class);
+		} else {
+			jsonArray = NodeForward.requestData(nodeModel, NodeUrl.Manage_GetProjectInfo, JSONArray.class, "notStatus", "true");
+		}
+		return filter(jsonArray, ClassFeature.PROJECT);
+	}
 
-    public List<String> getAllGroup(NodeModel nodeModel) {
-        return NodeForward.requestData(nodeModel, NodeUrl.Manage_GetProjectGroup, null, List.class);
-    }
+	public List<String> getAllGroup(NodeModel nodeModel) {
+		return NodeForward.requestData(nodeModel, NodeUrl.Manage_GetProjectGroup, null, List.class);
+	}
 
-    public JSONObject getItem(NodeModel nodeModel, String id) {
-        return NodeForward.requestData(nodeModel, NodeUrl.Manage_GetProjectItem, JSONObject.class, "id", id);
-    }
+	public JSONObject getItem(NodeModel nodeModel, String id) {
+		return NodeForward.requestData(nodeModel, NodeUrl.Manage_GetProjectItem, JSONObject.class, "id", id);
+	}
 
-    public JSONObject getLogSize(NodeModel nodeModel, String id, String copyId) {
-        return NodeForward.requestData(nodeModel, NodeUrl.Manage_Log_LogSize, JSONObject.class, "id", id, "copyId", copyId);
-    }
+	public JSONObject getLogSize(NodeModel nodeModel, String id, String copyId) {
+		return NodeForward.requestData(nodeModel, NodeUrl.Manage_Log_LogSize, JSONObject.class, "id", id, "copyId", copyId);
+	}
 
-    public JSONObject getItem(String nodeId, String id) {
-        NodeModel item = nodeService.getItem(id);
-        return NodeForward.requestData(item, NodeUrl.Manage_GetProjectItem, JSONObject.class, "id", id);
-    }
+	public JSONObject getItem(String nodeId, String id) {
+		NodeModel item = nodeService.getItem(id);
+		return NodeForward.requestData(item, NodeUrl.Manage_GetProjectItem, JSONObject.class, "id", id);
+	}
 
-    public JSONArray getJdkList(NodeModel nodeModel, HttpServletRequest request) {
-        return NodeForward.requestData(nodeModel, NodeUrl.Manage_jdk_list, request, JSONArray.class);
-    }
+	public JSONArray getJdkList(NodeModel nodeModel, HttpServletRequest request) {
+		return NodeForward.requestData(nodeModel, NodeUrl.Manage_jdk_list, request, JSONArray.class);
+	}
 }
