b379a96c5617263b7b1c71ab6f7dc5368764f4bb
bwcx_jzy <bwcx_jzy@163.com>
2021-08-18 09:07:07 +0800

    新增重启项目接口

--- modules/server/src/main/java/io/jpom/controller/node/manage/ProjectManageControl.java
+++ modules/server/src/main/java/io/jpom/controller/node/manage/ProjectManageControl.java
@@ -38,125 +38,120 @@ import java.util.List;
 @Feature(cls = ClassFeature.PROJECT)
 public class ProjectManageControl extends BaseServerController {
 
-    @Resource
-    private ProjectInfoService projectInfoService;
-    @Resource
-    private OutGivingServer outGivingServer;
-    @Resource
-    private MonitorService monitorService;
-    @Resource
-    private BuildService buildService;
-
-//    /**
-//     * 展示项目页面
-//     *
-//     * @return page
-//     */
-//    @RequestMapping(value = "projectInfo", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
-//    @Feature(method = MethodFeature.LIST)
-//    public String projectInfo() {
-//        List<String> hashSet = projectInfoService.getAllGroup(getNode());
-//        setAttribute("groups", hashSet);
-//        return "node/manage/projectInfo";
-//    }
+	@Resource
+	private ProjectInfoService projectInfoService;
+	@Resource
+	private OutGivingServer outGivingServer;
+	@Resource
+	private MonitorService monitorService;
+	@Resource
+	private BuildService buildService;
 
 
-//    @RequestMapping(value = "projectCopyLList.html", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
-//    @Feature(method = MethodFeature.LIST)
-//    public String projectInfoPage() {
-//        return "node/manage/javaCopyList";
-//    }
+	/**
+	 * 展示项目页面
+	 */
+	@RequestMapping(value = "project_copy_list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Feature(method = MethodFeature.LIST)
+	@ResponseBody
+	public String projectCopyList() {
+		return NodeForward.request(getNode(), getRequest(), NodeUrl.Manage_ProjectCopyList).toString();
+	}
 
+	/**
+	 * 获取正在运行的项目的端口和进程id
+	 *
+	 * @return json
+	 */
+	@RequestMapping(value = "getProjectPort", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getProjectPort() {
+		return NodeForward.request(getNode(), getRequest(), NodeUrl.Manage_GetProjectPort).toString();
+	}
 
-    /**
-     * 展示项目页面
-     */
-    @RequestMapping(value = "project_copy_list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @Feature(method = MethodFeature.LIST)
-    @ResponseBody
-    public String projectCopyList() {
-        return NodeForward.request(getNode(), getRequest(), NodeUrl.Manage_ProjectCopyList).toString();
-    }
+	/**
+	 * 获取正在运行的项目的端口和进程id
+	 *
+	 * @return json
+	 */
+	@RequestMapping(value = "getProjectCopyPort", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getProjectCopyPort() {
+		return NodeForward.request(getNode(), getRequest(), NodeUrl.Manage_GetProjectCopyPort).toString();
+	}
 
-    /**
-     * 获取正在运行的项目的端口和进程id
-     *
-     * @return json
-     */
-    @RequestMapping(value = "getProjectPort", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String getProjectPort() {
-        return NodeForward.request(getNode(), getRequest(), NodeUrl.Manage_GetProjectPort).toString();
-    }
+	/**
+	 * @return
+	 * @author Hotstrip
+	 * get project group
+	 * 获取项目的分组信息
+	 */
+	@RequestMapping(value = "project-group-list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String projectGroupList() {
+		List<String> allGroup = projectInfoService.getAllGroup(getNode());
+		return JsonMessage.getString(200, "success", allGroup);
+	}
 
-    /**
-     * 获取正在运行的项目的端口和进程id
-     *
-     * @return json
-     */
-    @RequestMapping(value = "getProjectCopyPort", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String getProjectCopyPort() {
-        return NodeForward.request(getNode(), getRequest(), NodeUrl.Manage_GetProjectCopyPort).toString();
-    }
+	/**
+	 * 查询所有项目
+	 *
+	 * @return json
+	 */
+	@RequestMapping(value = "getProjectInfo", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@Feature(method = MethodFeature.LIST)
+	public String getProjectInfo() {
+		NodeModel nodeModel = getNode();
+		JSONArray jsonArray = projectInfoService.listAll(nodeModel, getRequest());
+		return JsonMessage.getString(200, "ok", jsonArray);
+	}
 
-    /**
-     * @author Hotstrip
-     * get project group
-     * 获取项目的分组信息
-     * @return
-     */
-    @RequestMapping(value = "project-group-list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String projectGroupList() {
-        List<String> allGroup = projectInfoService.getAllGroup(getNode());
-        return JsonMessage.getString(200, "success", allGroup);
-    }
+	/**
+	 * 删除项目
+	 *
+	 * @param id id
+	 * @return json
+	 */
+	@RequestMapping(value = "deleteProject", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@OptLog(value = UserOperateLogV1.OptType.DelProject)
+	@Feature(method = MethodFeature.DEL)
+	public String deleteProject(@ValidatorItem(value = ValidatorRule.NOT_BLANK) String id, String copyId) {
+		NodeModel nodeModel = getNode();
+		if (StrUtil.isEmpty(copyId)) {
+			// 检查节点分发
+			List<OutGivingModel> outGivingModels = outGivingServer.list();
+			if (outGivingModels != null) {
+				for (OutGivingModel outGivingModel : outGivingModels) {
+					if (outGivingModel.checkContains(nodeModel.getId(), id)) {
+						return JsonMessage.getString(405, "当前项目存在节点分发，不能直接删除");
+					}
+				}
+			}
+			//
+			if (monitorService.checkProject(nodeModel.getId(), id)) {
+				return JsonMessage.getString(405, "当前项目存在监控项，不能直接删除");
+			}
+			if (buildService.checkNodeProjectId(nodeModel.getId(), id)) {
+				return JsonMessage.getString(405, "当前项目存在构建项，不能直接删除");
+			}
+		}
+		return NodeForward.request(nodeModel, getRequest(), NodeUrl.Manage_DeleteProject).toString();
+	}
 
-    /**
-     * 查询所有项目
-     *
-     * @return json
-     */
-    @RequestMapping(value = "getProjectInfo", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    @Feature(method = MethodFeature.LIST)
-    public String getProjectInfo() {
-        NodeModel nodeModel = getNode();
-        JSONArray jsonArray = projectInfoService.listAll(nodeModel, getRequest());
-        return JsonMessage.getString(200, "ok", jsonArray);
-    }
-
-    /**
-     * 删除项目
-     *
-     * @param id id
-     * @return json
-     */
-    @RequestMapping(value = "deleteProject", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    @OptLog(value = UserOperateLogV1.OptType.DelProject)
-    @Feature(method = MethodFeature.DEL)
-    public String deleteProject(@ValidatorItem(value = ValidatorRule.NOT_BLANK) String id, String copyId) {
-        NodeModel nodeModel = getNode();
-        if (StrUtil.isEmpty(copyId)) {
-            // 检查节点分发
-            List<OutGivingModel> outGivingModels = outGivingServer.list();
-            if (outGivingModels != null) {
-                for (OutGivingModel outGivingModel : outGivingModels) {
-                    if (outGivingModel.checkContains(nodeModel.getId(), id)) {
-                        return JsonMessage.getString(405, "当前项目存在节点分发，不能直接删除");
-                    }
-                }
-            }
-            //
-            if (monitorService.checkProject(nodeModel.getId(), id)) {
-                return JsonMessage.getString(405, "当前项目存在监控项，不能直接删除");
-            }
-            if (buildService.checkNodeProjectId(nodeModel.getId(), id)) {
-                return JsonMessage.getString(405, "当前项目存在构建项，不能直接删除");
-            }
-        }
-        return NodeForward.request(nodeModel, getRequest(), NodeUrl.Manage_DeleteProject).toString();
-    }
+	/**
+	 * 重启项目
+	 * <p>
+	 * nodeId,id,copyId
+	 *
+	 * @return json
+	 */
+	@RequestMapping(value = "restart", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@Feature(method = MethodFeature.EXECUTE)
+	public String restart() {
+		NodeModel nodeModel = getNode();
+		return NodeForward.request(nodeModel, getRequest(), NodeUrl.Manage_Restart).toString();
+	}
 }
