99b60f204c2ab02c5be36d5927dcb6cbb8a87c9a
bwcx_jzy <bwcx_jzy@163.com>
2021-09-10 10:48:03 +0800

    节点分发 日志存储路径修改

--- modules/server/src/main/java/io/jpom/controller/outgiving/OutGivingProjectEditController.java
+++ modules/server/src/main/java/io/jpom/controller/outgiving/OutGivingProjectEditController.java
@@ -281,16 +281,20 @@ public class OutGivingProjectEditController extends BaseServerController {
 			defData.put("javaExtDirsCp", getParameter("javaExtDirsCp"));
 		}
 		String whitelistDirectory = getParameter("whitelistDirectory");
-		String logPath = getParameter("logPath");
 		List<String> whitelistServerOutGiving = serverWhitelistServer.getOutGiving();
 		if (!AgentWhitelist.checkPath(whitelistServerOutGiving, whitelistDirectory)) {
 			return JsonMessage.getString(401, "请选择正确的项目路径,或者还没有配置白名单");
 		}
-		if (!AgentWhitelist.checkPath(whitelistServerOutGiving, logPath)) {
-			return JsonMessage.getString(401, "请选择正确的日志路径,或者还没有配置白名单");
-		}
 		defData.put("whitelistDirectory", whitelistDirectory);
-		defData.put("logPath", logPath);
+
+		String logPath = getParameter("logPath");
+		if (StrUtil.isNotEmpty(logPath)) {
+			if (!AgentWhitelist.checkPath(whitelistServerOutGiving, logPath)) {
+				return JsonMessage.getString(401, "请选择正确的日志路径,或者还没有配置白名单");
+			}
+			defData.put("logPath", logPath);
+		}
+
 		String lib = getParameter("lib");
 		defData.put("lib", lib);
 		defData.put("group", "节点分发");
