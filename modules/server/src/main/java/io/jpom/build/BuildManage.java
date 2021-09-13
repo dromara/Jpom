358b174db6ede29b0f3d801f3ffd02f5795aa9d2
bwcx_jzy <bwcx_jzy@163.com>
2021-09-13 18:41:21 +0800

    构建触发器新增延迟参数，数据库字段类型修改为 CLOB

--- modules/server/src/main/java/io/jpom/build/BuildManage.java
+++ modules/server/src/main/java/io/jpom/build/BuildManage.java
@@ -26,6 +26,7 @@ import java.io.*;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 构建管理
@@ -34,228 +35,239 @@ import java.util.concurrent.ConcurrentHashMap;
  * @date 2019/7/16
  */
 public class BuildManage extends BaseBuild implements Runnable {
-    /**
-     * 缓存构建中
-     */
-    private static final Map<String, BuildManage> BUILD_MANAGE_MAP = new ConcurrentHashMap<>();
+	/**
+	 * 缓存构建中
+	 */
+	private static final Map<String, BuildManage> BUILD_MANAGE_MAP = new ConcurrentHashMap<>();
 
-    private final BuildModel buildModel;
-    private final File gitFile;
-    private Process process;
-    private String logId;
-    private final String optUserName;
-    private final UserModel userModel;
+	private final BuildModel buildModel;
+	private final File gitFile;
+	private Process process;
+	private String logId;
+	private final String optUserName;
+	private final UserModel userModel;
+	/**
+	 * 延迟执行
+	 */
+	private Integer delay;
 
-    private BuildManage(final BuildModel buildModel, final UserModel userModel) {
-        super(BuildUtil.getLogFile(buildModel.getId(), buildModel.getBuildId()),
-                buildModel.getId());
-        this.buildModel = buildModel;
-        this.gitFile = BuildUtil.getSource(buildModel);
-        this.optUserName = UserModel.getOptUserName(userModel);
-        this.userModel = userModel;
-    }
+	private BuildManage(final BuildModel buildModel, final UserModel userModel) {
+		super(BuildUtil.getLogFile(buildModel.getId(), buildModel.getBuildId()),
+				buildModel.getId());
+		this.buildModel = buildModel;
+		this.gitFile = BuildUtil.getSource(buildModel);
+		this.optUserName = UserModel.getOptUserName(userModel);
+		this.userModel = userModel;
+	}
 
-    /**
-     * 取消构建
-     *
-     * @param id id
-     * @return bool
-     */
-    public static boolean cancel(String id) {
-        BuildManage buildManage = BUILD_MANAGE_MAP.get(id);
-        if (buildManage == null) {
-            return false;
-        }
-        if (buildManage.process != null) {
-            try {
-                buildManage.process.destroy();
-            } catch (Exception ignored) {
-            }
-        }
-        buildManage.updateStatus(BuildModel.Status.Cancel);
-        BUILD_MANAGE_MAP.remove(id);
-        return true;
-    }
+	/**
+	 * 取消构建
+	 *
+	 * @param id id
+	 * @return bool
+	 */
+	public static boolean cancel(String id) {
+		BuildManage buildManage = BUILD_MANAGE_MAP.get(id);
+		if (buildManage == null) {
+			return false;
+		}
+		if (buildManage.process != null) {
+			try {
+				buildManage.process.destroy();
+			} catch (Exception ignored) {
+			}
+		}
+		buildManage.updateStatus(BuildModel.Status.Cancel);
+		BUILD_MANAGE_MAP.remove(id);
+		return true;
+	}
 
-    /**
-     * 创建构建
-     *
-     * @param buildModel 构建项
-     * @param userModel  操作人
-     * @return this
-     */
-    public static BuildManage create(BuildModel buildModel, UserModel userModel) {
-        if (BUILD_MANAGE_MAP.containsKey(buildModel.getId())) {
-            throw new JpomRuntimeException("当前构建还在进行中");
-        }
-        BuildManage buildManage = new BuildManage(buildModel, userModel);
-        BUILD_MANAGE_MAP.put(buildModel.getId(), buildManage);
-        //
-        ThreadUtil.execute(buildManage);
-        return buildManage;
-    }
+	/**
+	 * 创建构建
+	 *
+	 * @param buildModel 构建项
+	 * @param userModel  操作人
+	 * @param delay      延迟执行时间
+	 * @return this
+	 */
+	public static BuildManage create(BuildModel buildModel, UserModel userModel, Integer delay) {
+		if (BUILD_MANAGE_MAP.containsKey(buildModel.getId())) {
+			throw new JpomRuntimeException("当前构建还在进行中");
+		}
+		BuildManage buildManage = new BuildManage(buildModel, userModel);
+		BUILD_MANAGE_MAP.put(buildModel.getId(), buildManage);
+		//
+		buildManage.delay = delay;
+		//
+		ThreadUtil.execute(buildManage);
+		return buildManage;
+	}
 
-    @Override
-    protected boolean updateStatus(BuildModel.Status status) {
-        try {
-            super.updateStatus(status);
-            if (status == BuildModel.Status.Ing) {
-                this.insertLog();
-            } else {
-                DbBuildHistoryLogService dbBuildHistoryLogService = SpringUtil.getBean(DbBuildHistoryLogService.class);
-                dbBuildHistoryLogService.updateLog(this.logId, status);
-            }
-            return true;
-        } catch (Exception e) {
-            DefaultSystemLog.getLog().error("构建状态变更失败", e);
-            return false;
-        }
-    }
+	@Override
+	protected boolean updateStatus(BuildModel.Status status) {
+		try {
+			super.updateStatus(status);
+			if (status == BuildModel.Status.Ing) {
+				this.insertLog();
+			} else {
+				DbBuildHistoryLogService dbBuildHistoryLogService = SpringUtil.getBean(DbBuildHistoryLogService.class);
+				dbBuildHistoryLogService.updateLog(this.logId, status);
+			}
+			return true;
+		} catch (Exception e) {
+			DefaultSystemLog.getLog().error("构建状态变更失败", e);
+			return false;
+		}
+	}
 
 
-    /**
-     * 插入记录
-     */
-    private void insertLog() {
-        this.logId = IdUtil.fastSimpleUUID();
-        BuildHistoryLog buildHistoryLog = new BuildHistoryLog();
+	/**
+	 * 插入记录
+	 */
+	private void insertLog() {
+		this.logId = IdUtil.fastSimpleUUID();
+		BuildHistoryLog buildHistoryLog = new BuildHistoryLog();
 
 //        buildHistoryLog.setResultDirFile(buildModel.getResultDirFile());
 //        buildHistoryLog.setReleaseMethod(this.buildModel.getReleaseMethod());
 //        buildHistoryLog.setReleaseMethodDataId(this.buildModel.getReleaseMethodDataId());
 //        buildHistoryLog.setAfterOpt(this.buildModel.getAfterOpt());
 //        buildHistoryLog.setReleaseCommand(this.buildModel.getReleaseCommand());
-        BeanUtil.copyProperties(this.buildModel, buildHistoryLog);
+		BeanUtil.copyProperties(this.buildModel, buildHistoryLog);
 
-        buildHistoryLog.setId(this.logId);
-        buildHistoryLog.setBuildDataId(buildModel.getId());
-        buildHistoryLog.setStatus(BuildModel.Status.Ing.getCode());
-        buildHistoryLog.setStartTime(System.currentTimeMillis());
-        buildHistoryLog.setBuildNumberId(buildModel.getBuildId());
-        buildHistoryLog.setBuildUser(optUserName);
+		buildHistoryLog.setId(this.logId);
+		buildHistoryLog.setBuildDataId(buildModel.getId());
+		buildHistoryLog.setStatus(BuildModel.Status.Ing.getCode());
+		buildHistoryLog.setStartTime(System.currentTimeMillis());
+		buildHistoryLog.setBuildNumberId(buildModel.getBuildId());
+		buildHistoryLog.setBuildUser(optUserName);
 
-        DbBuildHistoryLogService dbBuildHistoryLogService = SpringUtil.getBean(DbBuildHistoryLogService.class);
-        dbBuildHistoryLogService.insert(buildHistoryLog);
-    }
+		DbBuildHistoryLogService dbBuildHistoryLogService = SpringUtil.getBean(DbBuildHistoryLogService.class);
+		dbBuildHistoryLogService.insert(buildHistoryLog);
+	}
 
-    /**
-     * 打包构建产物
-     */
-    private boolean packageFile() throws InterruptedException {
-        Thread.sleep(2000);
-        File file = FileUtil.file(this.gitFile, buildModel.getResultDirFile());
-        if (!file.exists()) {
-            this.log(buildModel.getResultDirFile() + "不存在，处理构建产物失败");
-            return false;
-        }
-        File toFile = BuildUtil.getHistoryPackageFile(buildModel.getId(), buildModel.getBuildId(), buildModel.getResultDirFile());
-        FileCopier.create(file, toFile)
-                .setCopyContentIfDir(true)
-                .setOverride(true)
-                .setCopyAttributes(true)
-                .setCopyFilter(file1 -> !file1.isHidden())
-                .copy();
-        this.log(StrUtil.format("mv {} {}", buildModel.getResultDirFile(), buildModel.getBuildIdStr()));
-        return true;
-    }
+	/**
+	 * 打包构建产物
+	 */
+	private boolean packageFile() throws InterruptedException {
+		Thread.sleep(2000);
+		File file = FileUtil.file(this.gitFile, buildModel.getResultDirFile());
+		if (!file.exists()) {
+			this.log(buildModel.getResultDirFile() + "不存在，处理构建产物失败");
+			return false;
+		}
+		File toFile = BuildUtil.getHistoryPackageFile(buildModel.getId(), buildModel.getBuildId(), buildModel.getResultDirFile());
+		FileCopier.create(file, toFile)
+				.setCopyContentIfDir(true)
+				.setOverride(true)
+				.setCopyAttributes(true)
+				.setCopyFilter(file1 -> !file1.isHidden())
+				.copy();
+		this.log(StrUtil.format("mv {} {}", buildModel.getResultDirFile(), buildModel.getBuildIdStr()));
+		return true;
+	}
 
-    @Override
-    public void run() {
-        try {
-            if (!updateStatus(BuildModel.Status.Ing)) {
-                this.log("初始化构建记录失败,异常结束");
-                return;
-            }
-            try {
-                this.log("start build in file : " + FileUtil.getAbsolutePath(this.gitFile));
-                //
-                String branchName = buildModel.getBranchName();
-                this.log("repository clone pull from " + branchName);
-                String msg = "error";
-                if (buildModel.getRepoType() == BuildModel.RepoType.Git.getCode()) {
-                    // git
-                    UsernamePasswordCredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(buildModel.getUserName(), buildModel.getPassword());
-                    GitUtil.checkoutPull(buildModel.getGitUrl(), gitFile, branchName, credentialsProvider, this.getPrintWriter());
-                    // 记录最后一次提交记录
-                    msg = GitUtil.getLastCommitMsg(gitFile, branchName);
-                } else if (buildModel.getRepoType() == BuildModel.RepoType.Svn.getCode()) {
-                    // svn
-                    msg = SvnKitUtil.checkOut(buildModel.getGitUrl(), buildModel.getUserName(), buildModel.getPassword(), gitFile);
-                }
-                this.log(msg);
-            } catch (Exception e) {
-                this.log("拉取代码失败", e);
-                return;
-            }
-            String[] commands = CharSequenceUtil.splitToArray(buildModel.getScript(), StrUtil.LF);
-            if (commands == null || commands.length <= 0) {
-                this.log("没有需要执行的命令");
-                this.updateStatus(BuildModel.Status.Error);
-                return;
-            }
-            for (String item : commands) {
-                try {
-                    boolean s = runCommand(item);
-                    if (!s) {
-                        this.log("命令执行存在error");
-                    }
-                } catch (IOException e) {
-                    this.log(item + " 执行异常", e);
-                    return;
-                }
-            }
-            boolean status = packageFile();
-            if (status && buildModel.getReleaseMethod() != BuildModel.ReleaseMethod.No.getCode()) {
-                // 发布文件
-                new ReleaseManage(this.buildModel, this.userModel, this).start();
-            } else {
-                //
-                updateStatus(BuildModel.Status.Success);
-            }
-        } catch (Exception e) {
-            this.log("构建失败", e);
-        } finally {
-            BUILD_MANAGE_MAP.remove(buildModel.getId());
-        }
-    }
+	@Override
+	public void run() {
+		if (delay != null && delay > 0) {
+			// 延迟执行
+			ThreadUtil.sleep(delay, TimeUnit.SECONDS);
+		}
+		try {
+			if (!updateStatus(BuildModel.Status.Ing)) {
+				this.log("初始化构建记录失败,异常结束");
+				return;
+			}
+			try {
+				this.log("start build in file : " + FileUtil.getAbsolutePath(this.gitFile));
+				//
+				String branchName = buildModel.getBranchName();
+				this.log("repository clone pull from " + branchName);
+				String msg = "error";
+				if (buildModel.getRepoType() == BuildModel.RepoType.Git.getCode()) {
+					// git
+					UsernamePasswordCredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(buildModel.getUserName(), buildModel.getPassword());
+					GitUtil.checkoutPull(buildModel.getGitUrl(), gitFile, branchName, credentialsProvider, this.getPrintWriter());
+					// 记录最后一次提交记录
+					msg = GitUtil.getLastCommitMsg(gitFile, branchName);
+				} else if (buildModel.getRepoType() == BuildModel.RepoType.Svn.getCode()) {
+					// svn
+					msg = SvnKitUtil.checkOut(buildModel.getGitUrl(), buildModel.getUserName(), buildModel.getPassword(), gitFile);
+				}
+				this.log(msg);
+			} catch (Exception e) {
+				this.log("拉取代码失败", e);
+				return;
+			}
+			String[] commands = CharSequenceUtil.splitToArray(buildModel.getScript(), StrUtil.LF);
+			if (commands == null || commands.length <= 0) {
+				this.log("没有需要执行的命令");
+				this.updateStatus(BuildModel.Status.Error);
+				return;
+			}
+			for (String item : commands) {
+				try {
+					boolean s = runCommand(item);
+					if (!s) {
+						this.log("命令执行存在error");
+					}
+				} catch (IOException e) {
+					this.log(item + " 执行异常", e);
+					return;
+				}
+			}
+			boolean status = packageFile();
+			if (status && buildModel.getReleaseMethod() != BuildModel.ReleaseMethod.No.getCode()) {
+				// 发布文件
+				new ReleaseManage(this.buildModel, this.userModel, this).start();
+			} else {
+				//
+				updateStatus(BuildModel.Status.Success);
+			}
+		} catch (Exception e) {
+			this.log("构建失败", e);
+		} finally {
+			BUILD_MANAGE_MAP.remove(buildModel.getId());
+		}
+	}
 
-    private void log(String title, Throwable throwable) {
-        log(title, throwable, BuildModel.Status.Error);
-    }
+	private void log(String title, Throwable throwable) {
+		log(title, throwable, BuildModel.Status.Error);
+	}
 
-    /**
-     * 执行命令
-     *
-     * @param command 命令
-     * @return 是否存在错误
-     * @throws IOException IO
-     */
-    private boolean runCommand(String command) throws IOException {
-        this.log(command);
-        //
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.directory(this.gitFile);
-        List<String> commands = CommandUtil.getCommand();
-        commands.add(command);
-        processBuilder.command(commands);
-        final boolean[] status = new boolean[1];
-        process = processBuilder.start();
-        //
-        InputStream inputStream = process.getInputStream();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, JpomApplication.getCharset());
-        BufferedReader results = new BufferedReader(inputStreamReader);
-        IoUtil.readLines(results, (LineHandler) line -> {
-            log(line);
-            status[0] = true;
-        });
-        //
-        InputStream errorInputStream = process.getErrorStream();
-        InputStreamReader errorInputStreamReader = new InputStreamReader(errorInputStream, JpomApplication.getCharset());
-        BufferedReader errorResults = new BufferedReader(errorInputStreamReader);
-        IoUtil.readLines(errorResults, (LineHandler) line -> {
-            log(line);
-            status[0] = false;
-        });
-        return status[0];
-    }
+	/**
+	 * 执行命令
+	 *
+	 * @param command 命令
+	 * @return 是否存在错误
+	 * @throws IOException IO
+	 */
+	private boolean runCommand(String command) throws IOException {
+		this.log(command);
+		//
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.directory(this.gitFile);
+		List<String> commands = CommandUtil.getCommand();
+		commands.add(command);
+		processBuilder.command(commands);
+		final boolean[] status = new boolean[1];
+		process = processBuilder.start();
+		//
+		InputStream inputStream = process.getInputStream();
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream, JpomApplication.getCharset());
+		BufferedReader results = new BufferedReader(inputStreamReader);
+		IoUtil.readLines(results, (LineHandler) line -> {
+			log(line);
+			status[0] = true;
+		});
+		//
+		InputStream errorInputStream = process.getErrorStream();
+		InputStreamReader errorInputStreamReader = new InputStreamReader(errorInputStream, JpomApplication.getCharset());
+		BufferedReader errorResults = new BufferedReader(errorInputStreamReader);
+		IoUtil.readLines(errorResults, (LineHandler) line -> {
+			log(line);
+			status[0] = false;
+		});
+		return status[0];
+	}
 }
--- modules/server/src/main/java/io/jpom/controller/openapi/build/BuildTriggerApiController.java
+++ modules/server/src/main/java/io/jpom/controller/openapi/build/BuildTriggerApiController.java
@@ -1,5 +1,6 @@
 package io.jpom.controller.openapi.build;
 
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.StrUtil;
 import cn.jiangzeyin.common.JsonMessage;
 import io.jpom.common.ServerOpenApi;
@@ -25,27 +26,27 @@ import java.util.Optional;
 @NotLogin
 public class BuildTriggerApiController {
 
-    @Resource
-    private BuildService buildService;
+	@Resource
+	private BuildService buildService;
 
-    @Resource
-    private UserService userService;
+	@Resource
+	private UserService userService;
 
-    @RequestMapping(value = ServerOpenApi.BUILD_TRIGGER_BUILD, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String install(@PathVariable String id, @PathVariable String token) {
-        BuildModel item = buildService.getItem(id);
-        if (item == null) {
-            return JsonMessage.getString(404, "没有对应数据");
-        }
-        List<UserModel> list = userService.list(false);
-        Optional<UserModel> first = list.stream().filter(UserModel::isSystemUser).findFirst();
-        if (!first.isPresent()) {
-            return JsonMessage.getString(404, "没有对应数据");
-        }
-        UserModel userModel = first.get();
-        if (!StrUtil.equals(token, item.getTriggerToken())) {
-            return JsonMessage.getString(404, "触发token错误");
-        }
-        return buildService.start(userModel, id);
-    }
+	@RequestMapping(value = ServerOpenApi.BUILD_TRIGGER_BUILD, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String install(@PathVariable String id, @PathVariable String token, String delay) {
+		BuildModel item = buildService.getItem(id);
+		if (item == null) {
+			return JsonMessage.getString(404, "没有对应数据");
+		}
+		List<UserModel> list = userService.list(false);
+		Optional<UserModel> first = list.stream().filter(UserModel::isSystemUser).findFirst();
+		if (!first.isPresent()) {
+			return JsonMessage.getString(404, "没有对应数据");
+		}
+		UserModel userModel = first.get();
+		if (!StrUtil.equals(token, item.getTriggerToken())) {
+			return JsonMessage.getString(404, "触发token错误");
+		}
+		return buildService.start(userModel, id, Convert.toInt(delay, 0));
+	}
 }
