b51422f46964200df15b1e9568a2b10186d5b91a
bwcx_jzy <bwcx_jzy@163.com>
2021-09-18 16:54:37 +0800

    白名单路径说明

--- web-vue/src/pages/dispatch/white-list.vue
+++ web-vue/src/pages/dispatch/white-list.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+    <template>
+      <a-alert style="margin-bottom: 20px" message="路径需要配置绝对路径,不支持软链" type="info" />
+    </template>
     <a-form-model ref="editForm" :model="temp" :label-col="{ span: 6 }" :wrapper-col="{ span: 14 }">
       <a-form-model-item label="白名单路径" prop="id">
-        <a-input v-model="temp.outGiving" type="textarea" :rows="5" style="resize: none" placeholder="请输入白名单路径，回车支持输入多个路径，系统会自动过滤 ../ 路径、不允许输入根路径"/>
+        <a-input v-model="temp.outGiving" type="textarea" :rows="5" style="resize: none" placeholder="请输入白名单路径，回车支持输入多个路径，系统会自动过滤 ../ 路径、不允许输入根路径" />
       </a-form-model-item>
-       <a-form-model-item label="远程下载安全HOST" prop="name">
-        <a-input v-model="temp.allowRemoteDownloadHost" type="textarea" :rows="5" style="resize: none" placeholder="请输入远程下载安全HOST，回车支持输入多个路径，示例 https://www.test.com 等"/>
+      <a-form-model-item label="远程下载安全HOST" prop="name">
+        <a-input v-model="temp.allowRemoteDownloadHost" type="textarea" :rows="5" style="resize: none" placeholder="请输入远程下载安全HOST，回车支持输入多个路径，示例 https://www.test.com 等" />
       </a-form-model-item>
       <a-form-model-item :wrapper-col="{ span: 14, offset: 6 }">
         <a-button type="primary" :disabled="submitAble" @click="onSubmit">提交</a-button>
@@ -14,13 +17,13 @@
   </div>
 </template>
 <script>
-import { getDispatchWhiteList, editDispatchWhiteList } from '../../api/dispatch';
+import { getDispatchWhiteList, editDispatchWhiteList } from "../../api/dispatch";
 export default {
   data() {
     return {
       temp: {},
-      submitAble: false
-    }
+      submitAble: false,
+    };
   },
   mounted() {
     this.loadData();
@@ -28,29 +31,29 @@ export default {
   methods: {
     // load data
     loadData() {
-      getDispatchWhiteList().then(res => {
+      getDispatchWhiteList().then((res) => {
         if (res.code === 200) {
           this.temp = res.data;
         }
-      })
+      });
     },
     // submit
     onSubmit() {
       // disabled submit button
       this.submitAble = true;
-      
-      editDispatchWhiteList(this.temp).then(res => {
+
+      editDispatchWhiteList(this.temp).then((res) => {
         if (res.code === 200) {
           // 成功
           this.$notification.success({
             message: res.msg,
-            duration: 2
+            duration: 2,
           });
         }
         // button recover
         this.submitAble = false;
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
--- web-vue/src/pages/node/node-layout/system/white-list.vue
+++ web-vue/src/pages/node/node-layout/system/white-list.vue
@@ -1,20 +1,29 @@
 <template>
   <div>
+    <template>
+      <a-alert style="margin-bottom: 20px" message="路径需要配置绝对路径,不支持软链" type="info" />
+    </template>
     <a-form-model ref="editForm" :model="temp" :label-col="{ span: 6 }" :wrapper-col="{ span: 14 }">
       <a-form-model-item label="项目路径" prop="id">
-        <a-input v-model="temp.project" type="textarea" :rows="5" style="resize: none" placeholder="请输入项目存放路径白名单，回车支持输入多个路径，系统会自动过滤 ../ 路径、不允许输入根路径"/>
+        <a-input v-model="temp.project" type="textarea" :rows="5" style="resize: none" placeholder="请输入项目存放路径白名单，回车支持输入多个路径，系统会自动过滤 ../ 路径、不允许输入根路径" />
       </a-form-model-item>
       <a-form-model-item label="证书路径" prop="name">
-        <a-input v-model="temp.certificate" type="textarea" :rows="5" style="resize: none" placeholder="请输入证书存放路径白名单，回车支持输入多个路径，系统会自动过滤 ../ 路径、不允许输入根路径"/>
+        <a-input v-model="temp.certificate" type="textarea" :rows="5" style="resize: none" placeholder="请输入证书存放路径白名单，回车支持输入多个路径，系统会自动过滤 ../ 路径、不允许输入根路径" />
       </a-form-model-item>
       <a-form-model-item label="Nginx 路径" prop="name">
-        <a-input v-model="temp.nginx" type="textarea" :rows="5" style="resize: none" placeholder="请输入 nginx 存放路径白名单，回车支持输入多个路径，系统会自动过滤 ../ 路径、不允许输入根路径"/>
+        <a-input v-model="temp.nginx" type="textarea" :rows="5" style="resize: none" placeholder="请输入 nginx 存放路径白名单，回车支持输入多个路径，系统会自动过滤 ../ 路径、不允许输入根路径" />
       </a-form-model-item>
       <a-form-model-item label="远程下载安全HOST" prop="name">
-        <a-input v-model="temp.allowRemoteDownloadHost" type="textarea" :rows="5" style="resize: none" placeholder="请输入远程下载安全HOST，回车支持输入多个路径，示例 https://www.test.com 等"/>
+        <a-input v-model="temp.allowRemoteDownloadHost" type="textarea" :rows="5" style="resize: none" placeholder="请输入远程下载安全HOST，回车支持输入多个路径，示例 https://www.test.com 等" />
       </a-form-model-item>
       <a-form-model-item label="文件后缀" prop="suffix">
-        <a-input v-model="temp.allowEditSuffix" type="textarea" :rows="5" style="resize: none" placeholder="请输入允许编辑文件的后缀及文件编码，不设置编码则默认取系统编码，示例：设置编码：txt@utf-8， 不设置编码：txt"/>
+        <a-input
+          v-model="temp.allowEditSuffix"
+          type="textarea"
+          :rows="5"
+          style="resize: none"
+          placeholder="请输入允许编辑文件的后缀及文件编码，不设置编码则默认取系统编码，示例：设置编码：txt@utf-8， 不设置编码：txt"
+        />
       </a-form-model-item>
       <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
         <a-button type="primary" :disabled="submitAble" @click="onSubmit">提交</a-button>
@@ -23,18 +32,18 @@
   </div>
 </template>
 <script>
-import { getWhiteList, editWhiteList } from '../../../../api/node-system';
+import { getWhiteList, editWhiteList } from "../../../../api/node-system";
 export default {
   props: {
     node: {
-      type: Object
-    }
+      type: Object,
+    },
   },
   data() {
     return {
       temp: {},
-      submitAble: false
-    }
+      submitAble: false,
+    };
   },
   mounted() {
     this.loadData();
@@ -42,29 +51,29 @@ export default {
   methods: {
     // load data
     loadData() {
-      getWhiteList(this.node.id).then(res => {
+      getWhiteList(this.node.id).then((res) => {
         if (res.code === 200) {
           this.temp = res.data;
         }
-      })
+      });
     },
     // submit
     onSubmit() {
       // disabled submit button
       this.submitAble = true;
       this.temp.nodeId = this.node.id;
-      editWhiteList(this.temp).then(res => {
+      editWhiteList(this.temp).then((res) => {
         if (res.code === 200) {
           // 成功
           this.$notification.success({
             message: res.msg,
-            duration: 2
+            duration: 2,
           });
         }
         // button recover
         this.submitAble = false;
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
