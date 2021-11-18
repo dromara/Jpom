8611cc8250a155d1f9d6cde9689d284d941a48d0
bwcx_jzy <bwcx_jzy@163.com>
2021-11-18 16:06:42 +0800

    取消 tools.jar 依赖

--- modules/common/src/main/java/io/jpom/system/init/CheckPath.java
+++ modules/common/src/main/java/io/jpom/system/init/CheckPath.java
@@ -3,13 +3,11 @@ package io.jpom.system.init;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.ClassUtil;
 import cn.jiangzeyin.common.DefaultSystemLog;
 import cn.jiangzeyin.common.PreLoadClass;
 import cn.jiangzeyin.common.PreLoadMethod;
 import io.jpom.system.ConfigBean;
 import io.jpom.system.ExtConfigBean;
-import io.jpom.util.StringUtil;
 import org.springframework.http.HttpMethod;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,28 +25,28 @@ import java.util.Map;
  */
 @PreLoadClass(value = Integer.MIN_VALUE)
 public class CheckPath {
-	/**
-	 * 待检查的类
-	 */
-	private static final String[] CLASS_NAME = new String[]{"sun.jvmstat.monitor.MonitorException", "com.sun.tools.attach.VirtualMachine"};
-
-
-	@PreLoadMethod(1)
-	private static void checkToolsJar() {
-		try {
-			for (String item : CLASS_NAME) {
-				ClassUtil.loadClass(item, false);
-			}
-		} catch (Exception e) {
-			File file = StringUtil.getToolsJar();
-			if (file.exists() && file.isFile()) {
-				DefaultSystemLog.getLog().error("Jpom未能正常加载tools.jar,请检查当前系统环境变量是否配置：JAVA_HOME，或者检查Jpom管理命令是否正确", e);
-			} else {
-				DefaultSystemLog.getLog().error("当前JDK中没有找到tools.jar,请检查当前JDK是否安装完整，文件完整路径是：" + file.getAbsolutePath(), e);
-			}
-			System.exit(-1);
-		}
-	}
+//	/**
+//	 * 待检查的类
+//	 */
+//	private static final String[] CLASS_NAME = new String[]{"sun.jvmstat.monitor.MonitorException", "com.sun.tools.attach.VirtualMachine"};
+//
+//
+//	@PreLoadMethod(1)
+//	private static void checkToolsJar() {
+//		try {
+//			for (String item : CLASS_NAME) {
+//				ClassUtil.loadClass(item, false);
+//			}
+//		} catch (Exception e) {
+//			File file = StringUtil.getToolsJar();
+//			if (file.exists() && file.isFile()) {
+//				DefaultSystemLog.getLog().error("Jpom未能正常加载tools.jar,请检查当前系统环境变量是否配置：JAVA_HOME，或者检查Jpom管理命令是否正确", e);
+//			} else {
+//				DefaultSystemLog.getLog().error("当前JDK中没有找到tools.jar,请检查当前JDK是否安装完整，文件完整路径是：" + file.getAbsolutePath(), e);
+//			}
+//			System.exit(-1);
+//		}
+//	}
 
 	/**
 	 * 判断是否重复运行
