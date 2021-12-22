16b237d6d65ba6430a85dc204838c17d6f48cc0d
bwcx_jzy <bwcx_jzy@163.com>
2021-12-22 12:16:49 +0800

    fix

--- modules/server/src/main/java/io/jpom/service/node/command/CommandService.java
+++ modules/server/src/main/java/io/jpom/service/node/command/CommandService.java
@@ -8,7 +8,6 @@ import cn.jiangzeyin.common.DefaultSystemLog;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.Session;
 import io.jpom.model.data.CommandModel;
-import io.jpom.model.data.SshModel;
 import io.jpom.service.h2db.BaseDbService;
 import io.jpom.service.node.ssh.SshService;
 import org.springframework.stereotype.Service;
@@ -18,14 +17,19 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
+ * 命令管理
+ *
  * @author : Arno
- * @description : 命令管理
  * @since : 2021/12/6 22:11
  */
 @Service
 public class CommandService extends BaseDbService<CommandModel> {
 
-	private SshService sshService;
+	private final SshService sshService;
+
+	public CommandService(SshService sshService) {
+		this.sshService = sshService;
+	}
 
 	/**
 	 * 批量执行命令
@@ -34,22 +38,21 @@ public class CommandService extends BaseDbService<CommandModel> {
 	 * @param nodes ssh节点
 	 */
 	public void executeBatch(CommandModel model, List<String> nodes) {
-		List<SshModel> sshModels = sshService.listById(nodes);
-		for (SshModel sshModel : sshModels) {
-			this.execute(model.getCommand(), sshModel);
+		for (String sshId : nodes) {
+			this.execute(model.getCommand(), sshId);
 		}
 	}
 
 	/**
 	 * 执行命令
 	 *
-	 * @param command  命令内容
-	 * @param sshModel 节点信息
+	 * @param command 命令内容
+	 * @param sshId   ssh id
 	 * @return 执行命令结果
 	 */
-	private String execute(String command, SshModel sshModel) {
+	private String execute(String command, String sshId) {
 		try {
-			Session session = JschUtil.createSession(sshModel.getHost(), sshModel.getPort(), sshModel.getUser(), sshModel.getPassword());
+			Session session = SshService.getSession(sshId);
 			ChannelExec channel = (ChannelExec) JschUtil.createChannel(session, ChannelType.EXEC);
 			channel.setCommand(StrUtil.bytes("source /etc/profile && " + command, StandardCharsets.UTF_8));
 			channel.setInputStream(null);
@@ -58,7 +61,7 @@ public class CommandService extends BaseDbService<CommandModel> {
 			InputStream in = channel.getInputStream();
 			return IoUtil.read(in, StandardCharsets.UTF_8);
 		} catch (Exception e) {
-			DefaultSystemLog.getLog().error(sshModel.getName() + " 节点执行命令异常", e);
+			DefaultSystemLog.getLog().error(sshId + " 节点执行命令异常", e);
 		}
 		return null;
 	}
