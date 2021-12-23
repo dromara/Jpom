a10294bae2a3a569fcbb4d81ce9ddf758ef90cef
bwcx_jzy <bwcx_jzy@163.com>
2021-12-23 11:07:44 +0800

    fix search

--- web-vue/src/pages/node/ssh/command.vue
+++ web-vue/src/pages/node/ssh/command.vue
@@ -29,7 +29,7 @@
           </div>
         </a-form-model-item>
         <a-form-model-item label="SSH节点">
-          <a-select mode="multiple" v-model="chooseSsh">
+          <a-select show-search option-filter-prop="children" mode="multiple" v-model="chooseSsh">
             <a-select-option v-for="item in sshList" :key="item.id" :value="item.id">
               {{ item.name }}
             </a-select-option>
@@ -63,7 +63,7 @@
         </a-form-model-item>
 
         <a-form-model-item label="SSH节点" required>
-          <a-select mode="multiple" v-model="chooseSsh">
+          <a-select show-search option-filter-prop="children" mode="multiple" v-model="chooseSsh">
             <a-select-option v-for="item in sshList" :key="item.id" :value="item.id">
               {{ item.name }}
             </a-select-option>
