3e72c7f0a380bf0f855bcfdc3ceac5ec03bbd5f5
bwcx_jzy <bwcx_jzy@163.com>
2021-12-24 18:01:42 +0800

    script execute log auto clear

--- modules/server/src/main/java/io/jpom/service/node/script/ScriptExecuteLogServer.java
+++ modules/server/src/main/java/io/jpom/service/node/script/ScriptExecuteLogServer.java
@@ -5,10 +5,16 @@ import io.jpom.service.h2db.BaseWorkspaceService;
 import org.springframework.stereotype.Service;
 
 /**
+ * 脚本默认执行记录
+ *
  * @author bwcx_jzy
  * @since 2021/12/12
  */
 @Service
 public class ScriptExecuteLogServer extends BaseWorkspaceService<ScriptExecuteLogModel> {
 
+	@Override
+	protected String[] clearTimeColumns() {
+		return new String[]{"createTimeMillis"};
+	}
 }
