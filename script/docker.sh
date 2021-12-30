2be3cb7a74c73449c267a11c6922341cf735b4cf
bwcx_jzy <bwcx_jzy@163.com>
2021-12-30 21:46:42 +0800

    docker 发布

--- script/docker.sh
+++ script/docker.sh
@@ -15,3 +15,21 @@ docker build -f ./modules/server/Dockerfile -t jpomdocker/jpom:latest ./modules/
 docker push jpomdocker/jpom:latest
 
 
+# 服务端 - arm
+docker build -f ./modules/server/DockerfileMacARM -t jpomdocker/jpom:mac-arm-2.8.3 .
+#
+docker push jpomdocker/jpom:mac-arm-2.8.3
+
+docker build -f ./modules/server/DockerfileMacARM -t jpomdocker/jpom:mac-arm-latest .
+#
+docker push jpomdocker/jpom:mac-arm-latest
+
+
+# docker logs --tail="100" jpom-server
+# docker push jpomdocker/jpom:mac-arm-2.8.3
+# docker pull jpomdocker/jpom:mac-arm-2.8.3
+# docker run -d -p 2122:2122 --name jpom-server -v /etc/localtime:/etc/localtime:ro -v jpom-server-vol:/usr/local/jpom-server jpomdocker/jpom:mac-arm-2.8.3
+# docker stop jpom-server
+# docker rm jpom-server
+# docker exec -it jpom-server /bin/bash
+#  docker-compose up -d --build
