f5040e83ce18f1d8973e5a9ac3a4f5569a90f4e4
bwcx_jzy <bwcx_jzy@163.com>
2021-12-31 10:38:15 +0800

    docker 发布

--- script/docker.sh
+++ script/docker.sh
@@ -6,14 +6,9 @@
 # https://hub.docker.com/r/jpomdocker/jpom
 
 # 服务端
-docker build -f ./modules/server/DockerfileRelease -t jpomdocker/jpom:2.8.3 ./modules/server/target
+docker buildx build --platform linux/amd64,linux/arm64 -t jpomdocker/jpom:2.8.3 -f ./modules/server/DockerfileRelease --push .
 #
-docker push jpomdocker/jpom:2.8.3
-
-docker build -f ./modules/server/DockerfileRelease -t jpomdocker/jpom:latest ./modules/server/target
-#
-docker push jpomdocker/jpom:latest
-
+docker buildx build --platform linux/amd64,linux/arm64 -t jpomdocker/jpom:latest -f ./modules/server/DockerfileRelease --push .
 
 # docker logs --tail="100" jpom-server
 # docker push jpomdocker/jpom:mac-arm-2.8.3
@@ -23,10 +18,5 @@ docker push jpomdocker/jpom:latest
 # docker rm jpom-server
 # docker exec -it jpom-server /bin/bash
 #  docker-compose up -d --build
-
-
-> docker buildx build --platform linux/amd64,linux/arm64 -t jpomdocker/jpom:2.8.3 -f ./modules/server/DockerfileRelease --push .
-> docker buildx build --platform linux/amd64,linux/arm64 -t jpomdocker/jpom:latest -f ./modules/server/DockerfileRelease --push .
-
 # docker buildx imagetools inspect jpomdocker/jpom
 
