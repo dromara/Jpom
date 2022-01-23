ba9fa684c18fee109a8ea8f21c855c6a6a1b1260
bwcx_jzy <bwcx_jzy@163.com>
2022-01-23 18:51:59 +0800

    optim node stat

--- web-vue/src/pages/node/stat.vue
+++ web-vue/src/pages/node/stat.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="full-content">
-    <a-space direction="vertical">
+    <div>
       <a-card>
         <!-- <template slot="title">
           <a-row>
@@ -18,76 +18,157 @@
               </template>
             </a-statistic>
           </a-col>
-          <a-col :span="3">
-            <a-statistic title="启用" :value="openStatusMap['1']"> </a-statistic>
-          </a-col>
-          <a-col :span="3">
-            <a-statistic title="关闭" :value="openStatusMap['0']"> </a-statistic>
-          </a-col>
+
           <a-col :span="3"> <a-statistic-countdown format="s 秒" title="刷新倒计时" :value="deadline" @finish="onFinish" /> </a-col>
         </a-row>
       </a-card>
-
-      <div ref="filter" class="filter">
-        <a-space>
-          <a-input v-model="listQuery['%name%']" placeholder="节点名称" />
-          <a-input v-model="listQuery['%url%']" placeholder="节点地址" />
-          <a-select v-model="listQuery.status" allowClear placeholder="请选择状态" class="search-input-item">
-            <a-select-option v-for="(desc, key) in statusMap" :key="key">{{ desc }}</a-select-option>
-          </a-select>
-          <a-tooltip title="按住 Ctr 或者 Alt 键点击按钮快速回到第一页">
-            <a-button :loading="loading" type="primary" @click="loadData">搜索</a-button>
-          </a-tooltip>
-        </a-space>
-      </div>
-      <!-- 表格 :scroll="{ x: 1070, y: tableHeight -60 }" scroll 跟 expandedRowRender 不兼容，没法同时使用不然会多出一行数据-->
-      <a-table :columns="columns" :data-source="list" bordered rowKey="id" :pagination="this.listQuery.total / this.listQuery.limit > 1 ? (this, pagination) : false" @change="changePage">
-        <a-tooltip slot="tooltip" slot-scope="text" placement="topLeft" :title="text">
-          <span>{{ text }}</span>
-        </a-tooltip>
-
-        <a-tooltip slot="tooltipStatus" slot-scope="text, record" placement="topLeft" :title="text">
-          <span v-if="record.status === 0">{{ text }}</span>
-          <span v-else>-</span>
-        </a-tooltip>
-        <template slot="status" slot-scope="text, record">
-          <a-tooltip v-if="text !== 0" placement="topLeft" :title="record.failureMsg">
-            <span>{{ statusMap[text] }}</span>
-          </a-tooltip>
-          <span v-else>{{ statusMap[text] }}</span>
-        </template>
-        <a-tooltip slot="modifyTimeMillis" slot-scope="text" :title="parseTime(text)">
-          <span>{{ parseTime(text, "{m}-{d} {h}:{i}:{s}") }}</span>
-        </a-tooltip>
-
-        <template slot="networkTime" slot-scope="text, record">
-          <a-tooltip @click="handleHistory(record, 'networkTime')" v-if="record.status === 0" placement="topLeft" :title="`当前值：${text}ms 点击查看历史值`">
-            <span>{{ text }}</span>
-          </a-tooltip>
-          <span v-else>-</span>
-        </template>
-
-        <template slot="progress" slot-scope="text, record">
-          <div v-if="parseFloat(text) >= 0">
-            <a-tooltip v-if="record.status === 0" placement="topLeft" :title="`当前值：${text}% 点击查看历史值`">
-              <a-progress
-                @click="handleHistory(record, 'nodeTop')"
-                :percent="text"
-                :stroke-color="{
-                  '0%': '#87d068',
-                  '30%': '#87d068',
-                  '100%': '#108ee9',
-                }"
-                size="small"
-                status="active"
-              />
+      <a-space direction="vertical">
+        <div></div>
+        <div ref="filter" class="filter">
+          <a-space>
+            <a-input v-model="listQuery['%name%']" placeholder="节点名称" />
+            <a-input v-model="listQuery['%url%']" placeholder="节点地址" />
+            <a-select v-model="listQuery.status" allowClear placeholder="请选择状态" class="search-input-item">
+              <a-select-option v-for="(desc, key) in statusMap" :key="key">{{ desc }}</a-select-option>
+            </a-select>
+            <a-select v-model="listQuery['order_field']" allowClear placeholder="请选择排序字段" class="search-input-item">
+              <a-select-option value="networkTime">网络延迟</a-select-option>
+              <a-select-option value="occupyCpu">cpu</a-select-option>
+              <a-select-option value="occupyDisk">disk</a-select-option>
+              <a-select-option value="occupyMemoryUsed">memoryUsed</a-select-option>
+              <a-select-option value="occupyMemory">memory</a-select-option>
+            </a-select>
+            <a-tooltip title="按住 Ctr 或者 Alt 键点击按钮快速回到第一页">
+              <a-button :loading="loading" type="primary" @click="loadData">搜索</a-button>
             </a-tooltip>
-            <span v-else>-</span>
-          </div>
-          <div v-else>-</div>
-        </template>
-      </a-table>
-    </a-space>
+          </a-space>
+        </div>
+      </a-space>
+      <a-row :gutter="[16, 16]">
+        <a-col v-for="item in list" :key="item.id" :span="6">
+          <template>
+            <a-card :headStyle="{ padding: '0 6px' }">
+              <template slot="title">
+                <a-row :gutter="[4, 0]">
+                  <a-col :span="17" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap">
+                    <a-tooltip :title="`${item.name}  ${item.url}`">
+                      {{ item.name }}
+                    </a-tooltip>
+                  </a-col>
+                  <a-col :span="7" style="text-align: right">
+                    <a-tooltip :title="`当前状态：${statusMap[item.status]} ${item.status === 0 ? '' : '异常描述：' + item.failureMsg} `">
+                      <a-tag :color="item.status === 0 ? 'green' : 'pink'" style="margin-right: 0px"> {{ statusMap[item.status] }}</a-tag>
+                    </a-tooltip>
+                  </a-col>
+                </a-row>
+              </template>
+
+              <div>
+                <a-space>
+                  <a-tooltip @click="item.status === 4 ? null : handleHistory(item, 'nodeTop')" :title="`CPU 占用率：${item.occupyCpu}%`">
+                    <a-progress
+                      type="circle"
+                      :width="80"
+                      :stroke-color="{
+                        '0%': '#87d068',
+                        '30%': '#87d068',
+                        '100%': '#108ee9',
+                      }"
+                      size="small"
+                      status="active"
+                      :percent="item.occupyCpu"
+                    />
+                  </a-tooltip>
+                  <a-tooltip @click="item.status === 4 ? null : handleHistory(item, 'nodeTop')" :title="`硬盘占用率：${item.occupyDisk}%`">
+                    <a-progress
+                      type="circle"
+                      :width="80"
+                      :stroke-color="{
+                        '0%': '#87d068',
+                        '30%': '#87d068',
+                        '100%': '#108ee9',
+                      }"
+                      size="small"
+                      status="active"
+                      :percent="item.occupyDisk"
+                    />
+                  </a-tooltip>
+                  <a-tooltip @click="item.status === 4 ? null : handleHistory(item, 'nodeTop')" :title="`内存占用率：${item.occupyDisk}%`">
+                    <a-progress
+                      :width="80"
+                      type="circle"
+                      :stroke-color="{
+                        '0%': '#87d068',
+                        '30%': '#87d068',
+                        '100%': '#108ee9',
+                      }"
+                      size="small"
+                      status="active"
+                      :percent="item.occupyMemoryUsed && item.occupyMemoryUsed !== -1 ? item.occupyMemoryUsed : item.occupyMemory"
+                    />
+                  </a-tooltip>
+                </a-space>
+              </div>
+              <a-space direction="vertical"> <div></div></a-space>
+              <a-row :gutter="[16, 8]">
+                <a-col :span="8">
+                  <a-tooltip @click="item.status === 4 ? null : handleHistory(item, 'networkTime')" :title="`${item.status === 4 ? '' : '延迟' + item.networkTime + 'ms 点击查看历史趋势'}`">
+                    <a-statistic
+                      title="延迟"
+                      :value="item.networkTime"
+                      valueStyle="font-size: 14px;"
+                      :formatter="
+                        (v) => {
+                          return item.networkTime === -1 ? '-' : item.networkTime + 'ms';
+                        }
+                      "
+                    />
+                  </a-tooltip>
+                </a-col>
+                <a-col :span="16">
+                  <a-statistic
+                    title="运行时间"
+                    :value="item.networkTime"
+                    valueStyle="font-size: 14px;"
+                    :formatter="
+                      (v) => {
+                        return item.upTimeStr || '-';
+                      }
+                    "
+                  />
+                </a-col>
+              </a-row>
+            </a-card>
+          </template>
+        </a-col>
+        <!-- v-if="this.listQuery.total / this.listQuery.limit > 1" -->
+      </a-row>
+      <a-row>
+        <a-col>
+          <a-pagination
+            v-model="listQuery.page"
+            v-if="listQuery.total / listQuery.limit > 1"
+            :showTotal="
+              (total) => {
+                return PAGE_DEFAULT_SHOW_TOTAL(total, listQuery);
+              }
+            "
+            :showSizeChanger="true"
+            :pageSizeOptions="sizeOptions"
+            :pageSize="listQuery.limit"
+            :total="listQuery.total"
+            @showSizeChange="
+              (current, size) => {
+                this.listQuery.limit = size;
+                this.loadData();
+              }
+            "
+            @change="this.loadData"
+            show-less-items
+          />
+        </a-col>
+      </a-row>
+    </div>
     <!-- 历史监控 -->
     <a-modal v-model="monitorVisible" width="75%" :title="`${this.temp.name}历史监控图表`" :footer="null" :maskClosable="false">
       <node-top v-if="monitorVisible" :type="this.temp.type" :nodeId="this.temp.id"></node-top>
@@ -97,7 +178,7 @@
 <script>
 import { getStatist, status, statusStat } from "@/api/node-stat";
 import { parseTime } from "@/utils/time";
-import { PAGE_DEFAULT_LIMIT, PAGE_DEFAULT_SIZW_OPTIONS, PAGE_DEFAULT_SHOW_TOTAL, PAGE_DEFAULT_LIST_QUERY } from "@/utils/const";
+import { PAGE_DEFAULT_SHOW_TOTAL, PAGE_DEFAULT_LIST_QUERY } from "@/utils/const";
 import NodeTop from "@/pages/node/node-layout/node-top";
 
 export default {
@@ -106,55 +187,20 @@ export default {
     return {
       loading: false,
       statusMap: status,
-      listQuery: Object.assign(
-        {
-          order: "descend",
-          order_field: "networkTime",
-        },
-        PAGE_DEFAULT_LIST_QUERY
-      ),
+      listQuery: Object.assign({ order: "descend", order_field: "networkTime" }, PAGE_DEFAULT_LIST_QUERY, {
+        limit: 8,
+      }),
+      sizeOptions: ["8", "12", "16", "20", "24"],
       list: [],
       statusStatMap: {},
-      openStatusMap: {},
+      // openStatusMap: {},
       nodeCount: 0,
       monitorVisible: false,
       deadline: 0,
       temp: {},
-      columns: [
-        { title: "节点名称", dataIndex: "name", sorter: true, key: "name", ellipsis: true, scopedSlots: { customRender: "tooltip" } },
-        { title: "节点地址", dataIndex: "url", sorter: true, key: "url", ellipsis: true, scopedSlots: { customRender: "tooltip" } },
-        { title: "cpu", dataIndex: "occupyCpu", sorter: true, key: "occupyCpu", scopedSlots: { customRender: "progress" } },
-        { title: "disk", dataIndex: "occupyDisk", sorter: true, key: "occupyDisk", scopedSlots: { customRender: "progress" } },
-        { title: "memory", dataIndex: "occupyMemory", sorter: true, key: "occupyMemory", scopedSlots: { customRender: "progress" } },
-        { title: "memoryUsed", dataIndex: "occupyMemoryUsed", sorter: true, key: "occupyMemoryUsed", scopedSlots: { customRender: "progress" } },
-        { title: "延迟(ms)", width: 100, dataIndex: "networkTime", defaultSortOrder: "descend", sorter: true, key: "networkTime", ellipsis: true, scopedSlots: { customRender: "networkTime" } },
-        { title: "运行时间", dataIndex: "upTimeStr", sorter: true, key: "upTimeStr", ellipsis: true, scopedSlots: { customRender: "tooltipStatus" } },
-        { title: "状态", width: 100, dataIndex: "status", sorter: true, key: "status", ellipsis: true, scopedSlots: { customRender: "status" } },
-        {
-          title: "更新时间",
-          dataIndex: "modifyTimeMillis",
-          ellipsis: true,
-          sorter: true,
-          scopedSlots: { customRender: "modifyTimeMillis" },
-          width: 140,
-        },
-      ],
     };
   },
-  computed: {
-    pagination() {
-      return {
-        total: this.listQuery.total || 0,
-        current: this.listQuery.page || 1,
-        pageSize: this.listQuery.limit || PAGE_DEFAULT_LIMIT,
-        pageSizeOptions: PAGE_DEFAULT_SIZW_OPTIONS,
-        showSizeChanger: true,
-        showTotal: (total) => {
-          return PAGE_DEFAULT_SHOW_TOTAL(total, this.listQuery);
-        },
-      };
-    },
-  },
+  computed: {},
   watch: {},
   created() {
     this.loadData();
@@ -165,6 +211,8 @@ export default {
     }
   },
   methods: {
+    PAGE_DEFAULT_SHOW_TOTAL,
+
     // 加载数据
     loadData(pointerEvent) {
       //this.list = [];
@@ -188,20 +236,11 @@ export default {
           this.nodeCount = nodeCount2;
           this.deadline = Date.now() + res.data.heartSecond * 1000;
           //
-          this.openStatusMap = res.data.openStatus;
+          // this.openStatusMap = res.data.openStatus;
         }
       });
     },
-    // 分页、排序、筛选变化时触发
-    changePage(pagination, filters, sorter) {
-      this.listQuery.page = pagination.current;
-      this.listQuery.limit = pagination.pageSize;
-      if (sorter) {
-        this.listQuery.order = sorter.order;
-        this.listQuery.order_field = sorter.field;
-      }
-      this.loadData();
-    },
+
     onFinish() {
       this.loadData();
     },
