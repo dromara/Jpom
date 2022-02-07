66117d3caf81f3123329387bd3cde57796b6cc33
bwcx_jzy <bwcx_jzy@163.com>
2022-02-07 17:07:13 +0800

    fix

--- modules/sub-plugin/docker-cli/src/main/resources/uses/java/install.sh
+++ modules/sub-plugin/docker-cli/src/main/resources/uses/java/install.sh
@@ -31,17 +31,27 @@ fi
 cd /tmp
 download_url=""
 ARCH_O=`uname -m`
+# https://github.com/AdoptOpenJDK/openjdk-docker/blob/master/8/jdk/ubuntu/Dockerfile.hotspot.nightly.full
 case "${ARCH_O}" in
-   aarch64|arm64)
-	 ARCH='aarch64';
-	 ;;
-   amd64|x86_64)
-	 ARCH='x64';
-	 ;;
-   *)
-	 echo "Unsupported arch: ${ARCH_O}";
-	 exit 1;
-	 ;;
+	aarch64|arm64)
+		ARCH='aarch64';
+	;;
+	armhf|armv7l)
+		ARCH='arm';
+	;;
+	ppc64el|ppc64le)
+		ARCH='ppc64le';
+	;;
+	s390x)
+		ARCH='s390x';
+	;;
+	amd64|x86_64)
+	 	ARCH='x64';
+	;;
+	*)
+	 	echo "Unsupported arch: ${ARCH_O}";
+	 	exit 1;
+	;;
 esac;
 case "${JAVA_VERSION}" in
 8)
--- modules/sub-plugin/docker-cli/src/main/resources/uses/node/install.sh
+++ modules/sub-plugin/docker-cli/src/main/resources/uses/node/install.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
+
+
 if [ $NODE_VERSION ]; then
   echo "NODE_VERSION ${NODE_VERSION}"
 else
   echo "not found NODE_VERSION"
   exit 1
 fi
+# https://github.com/nodejs/docker-node/blob/main/12/buster/Dockerfile
 ARCH=`uname -m`
 case "${ARCH}" in
-   aarch64|arm64)
-	 BINARY_ARCH='arm64';
-	 ;;
-   amd64|x86_64)
-	 BINARY_ARCH='x64';
-	 ;;
-   *)
-	 echo "Unsupported arch: ${ARCH}";
-	 exit 1;
-	 ;;
+	aarch64|arm64)
+	 	BINARY_ARCH='arm64';
+ 	;;
+   	amd64|x86_64)
+	 	BINARY_ARCH='x64';
+ 	;;
+    ppc64el)
+    	BINARY_ARCH='ppc64le'
+	;;
+    s390x)
+    	BINARY_ARCH='s390x'
+    ;;
+    armhf)
+    	BINARY_ARCH='armv7l'
+    ;;
+    i386)
+   	 	BINARY_ARCH='x86'
+    ;;
+   	*)
+	 	echo "Unsupported arch: ${ARCH}";
+	 	exit 1;
+ 	;;
 esac;
 cd /tmp
 wget https://registry.npmmirror.com/-/binary/node/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-${BINARY_ARCH}.tar.gz -O node.tar.gz
