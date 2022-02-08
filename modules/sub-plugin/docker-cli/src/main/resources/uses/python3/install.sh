44404ba9f31df031666b4633743a0fe609b47006
bwcx_jzy <bwcx_jzy@163.com>
2022-02-08 22:07:10 +0800

    docker build add python3 plugin

--- modules/sub-plugin/docker-cli/src/main/resources/uses/python3/install.sh
+++ modules/sub-plugin/docker-cli/src/main/resources/uses/python3/install.sh
@@ -10,24 +10,102 @@ fi
 # https://github.com/docker-library/python/blob/master/3.10/slim-bullseye/Dockerfile
 
 pythonPath=/opt/python3/
-
-apt-get update;
-
-apt-get install -y --no-install-recommends ca-certificates netbase tzdata;
-
-rm -rf /var/lib/apt/lists/*;
-
-apt-get install -y --no-install-recommends dpkg-dev gcc  gnupg dirmngr libbluetooth-dev libbz2-dev libc6-dev libexpat1-dev libffi-dev libgdbm-dev liblzma-dev libncursesw5-dev libreadline-dev libsqlite3-dev libssl-dev make tk-dev uuid-dev wget xz-utils zlib1g-dev;
+pythonTempPath=/usr/local/python3
 
 # https://repo.huaweicloud.com/python/3.6.6/Python-3.6.6.tar.xz
 
+apt-get update; \
+	apt-get install -y --no-install-recommends \
+		ca-certificates \
+		netbase \
+		tzdata \
+	; \
+rm -rf /var/lib/apt/lists/*
+
+savedAptMark="$(apt-mark showmanual)"; \
+	apt-get update; \
+	apt-get install -y --no-install-recommends \
+		dpkg-dev \
+		gcc \
+		gnupg dirmngr \
+		libbluetooth-dev \
+		libbz2-dev \
+		libc6-dev \
+		libexpat1-dev \
+		libffi-dev \
+		libgdbm-dev \
+		liblzma-dev \
+		libncursesw5-dev \
+		libreadline-dev \
+		libsqlite3-dev \
+		libssl-dev \
+		make \
+		tk-dev \
+		uuid-dev \
+		xz-utils \
+		zlib1g-dev \
+;
+
 wget -O python.tar.xz "https://repo.huaweicloud.com/python/${PYTHON3_VERSION}/Python-${PYTHON3_VERSION}.tar.xz";
 
-tar -zxf python.tar.xz --strip-components 1 -C ${pythonPath}
+mkdir -p ${pythonTempPath} && tar -xvf python.tar.xz --strip-components 1 -C ${pythonTempPath}
 
-cd ${pythonPath};
+cd ${pythonTempPath};
 
-/configure  --build="$gnuArch"  --enable-loadable-sqlite-extensions  --enable-optimizations  --enable-option-checking=fatal  --enable-shared  --with-system-expat  --with-system-ffi  --without-ensurepip;
+gnuArch="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)"; \
+./configure \
+	--prefix=${pythonPath}
+	--build="$gnuArch" \
+	--enable-loadable-sqlite-extensions \
+	--enable-optimizations \
+	--enable-option-checking=fatal \
+	--enable-shared \
+	--with-lto \
+	--with-system-expat \
+	--with-system-ffi \
+	--without-ensurepip \
+;
 
-nproc="$(nproc)"
+nproc="$(nproc)";
+make -j "$nproc" \
+	LDFLAGS="-Wl,--strip-all" \
+# setting PROFILE_TASK makes "--enable-optimizations" reasonable: https://bugs.python.org/issue36044 / https://github.com/docker-library/python/issues/160#issuecomment-509426916
+	PROFILE_TASK='-m test.regrtest --pgo \
+		test_array \
+		test_base64 \
+		test_binascii \
+		test_binhex \
+		test_binop \
+		test_bytes \
+		test_c_locale_coercion \
+		test_class \
+		test_cmath \
+		test_codecs \
+		test_compile \
+		test_complex \
+		test_csv \
+		test_decimal \
+		test_dict \
+		test_float \
+		test_fstring \
+		test_hashlib \
+		test_io \
+		test_iter \
+		test_json \
+		test_long \
+		test_math \
+		test_memoryview \
+		test_pickle \
+		test_re \
+		test_set \
+		test_slice \
+		test_struct \
+		test_threading \
+		test_time \
+		test_traceback \
+		test_unicode \
+	' \
+;
+
+make install
 
