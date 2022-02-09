fce3f5d7e5246b69c2c4aa6158b5a002dc340d3f
sam <sam2008ext@gmail.com>
2022-02-09 08:15:29 +0800

    add comment for `repository-info` module

--- web-vue/src/api/repository.js
+++ web-vue/src/api/repository.js
@@ -1,10 +1,18 @@
 import axios from "./config";
 
 /**
- * 仓库列表
- * @param {
- *  name: 仓库名称
- * } params
+ * 分页获取仓库列表
+ *
+ * @param {Object} params               分页和查询参数
+ * @param {Number} params.limit         每页显示条数，默认 10 条
+ * @param {Number} params.page          获取第几页的数据，默认第 1 页
+ * @param {Number} params.total         总条数，默认 0
+ * @param {String} params.order         排序方式[ascend(升序，从小到大), descend(降序，从大到小)]
+ * @param {String} params.order_field   需要排序的字段名称
+ * @param {Number} params.repoType      查询的仓库类型[0(GIT), 1(SVN)]
+ * @param {String} params.name          仓库名称
+ * @param {String} params.gitUrl        仓库地址
+ * @return {axios} 请求结果 axios 对象
  */
 export function getRepositoryList(params) {
   return axios({
@@ -15,8 +23,9 @@ export function getRepositoryList(params) {
 }
 
 /**
- * 仓库列表 all
+ * 不分页获取所有仓库列表
  *
+ * @return {axios} 请求结果 axios 对象
  */
 export function getRepositoryListAll() {
   return axios({
@@ -26,18 +35,18 @@ export function getRepositoryListAll() {
 }
 
 /**
- * 编辑仓库信息，新增或者删除
- * @param {
- *  id: id
- *  name: 仓库名称
- *  gitUrl: 仓库地址
- *  repoType: 仓库类型 {0: GIT, 1: SVN}
- *  protocol: 协议 {0: HTTP(S), 1: SSH}
- *  userName: 用户名
- *  password: 密码
- *  rsaPub: 公钥信息
- * } params
- * @returns
+ * 编辑仓库信息，新增或者更新
+ *
+ * @param {Object} params               请求参数
+ * @param {String} params.id            仓库id
+ * @param {String} params.name          仓库名称
+ * @param {String} params.gitUrl        仓库地址
+ * @param {Number} params.repoType      仓库类型[0(GIT), 1(SVN)]
+ * @param {Number} params.protocol      协议[0(HTTP(S)), 1(SSH)]
+ * @param {String} params.userName      用户名
+ * @param {String} params.password      密码
+ * @param {String} params.rsaPub      公钥信息
+ * @return {axios} 请求结果 axios 对象
  */
 export function editRepository(params) {
   return axios({
@@ -48,12 +57,12 @@ export function editRepository(params) {
 }
 
 /**
- * delete by id
-@param {
-  *  id: id
-  *  isRealDel: 是否真删
-  * } params
- * @returns
+ * 根据 仓库id 删除仓库
+ *
+ * @param {Object}  params              请求参数
+ * @param {String}  params.id           仓库id
+ * @param {Boolean} params.isRealDel    是否真正删除
+ * @return {axios} 请求结果 axios 对象
  */
 export function deleteRepository(params) {
   return axios({
