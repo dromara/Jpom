53b12656f289e072e712f593eaa85219290f9c50
bwcx_jzy <bwcx_jzy@163.com>
2022-02-28 18:20:19 +0800

    fix error  controller not login

--- modules/server/src/main/java/io/jpom/controller/MyErrorController.java
+++ modules/server/src/main/java/io/jpom/controller/MyErrorController.java
@@ -24,6 +24,7 @@ package io.jpom.controller;
 
 import cn.jiangzeyin.common.DefaultSystemLog;
 import cn.jiangzeyin.common.JsonMessage;
+import io.jpom.common.interceptor.NotLogin;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
@@ -44,35 +45,36 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("${server.error.path:${error.path:/error}}")
+@NotLogin
 public class MyErrorController extends AbstractErrorController {
 
-	public MyErrorController(ErrorAttributes errorAttributes) {
-		super(errorAttributes);
-	}
+    public MyErrorController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
 
-	@RequestMapping
-	public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
-		HttpStatus status = getStatus(request);
-		if (status == HttpStatus.NO_CONTENT) {
-			return new ResponseEntity<>(status);
-		}
-		Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-		String requestUri = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
-		DefaultSystemLog.getLog().error("发生异常：" + statusCode + "  " + requestUri);
-		// 判断异常信息
-		Object attribute = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
-		Map<String, Object> body = new HashMap<>(5);
-		body.put(JsonMessage.CODE, HttpStatus.INTERNAL_SERVER_ERROR.value());
-		String msg = "啊哦，好像哪里出错了，请稍候再试试吧~";
-		if (attribute instanceof MaxUploadSizeExceededException) {
-			// 上传文件大小异常
-			msg = "上传文件太大了,请重新选择一个较小的文件上传吧";
-		} else if (status == HttpStatus.NOT_FOUND) {
-			msg = "没有找到对应的资源";
-			body.put(JsonMessage.DATA, requestUri);
-		}
-		body.put(JsonMessage.MSG, msg);
+    @RequestMapping
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        HttpStatus status = getStatus(request);
+        if (status == HttpStatus.NO_CONTENT) {
+            return new ResponseEntity<>(status);
+        }
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String requestUri = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        DefaultSystemLog.getLog().error("发生异常：" + statusCode + "  " + requestUri);
+        // 判断异常信息
+        Object attribute = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+        Map<String, Object> body = new HashMap<>(5);
+        body.put(JsonMessage.CODE, HttpStatus.INTERNAL_SERVER_ERROR.value());
+        String msg = "啊哦，好像哪里出错了，请稍候再试试吧~";
+        if (attribute instanceof MaxUploadSizeExceededException) {
+            // 上传文件大小异常
+            msg = "上传文件太大了,请重新选择一个较小的文件上传吧";
+        } else if (status == HttpStatus.NOT_FOUND) {
+            msg = "没有找到对应的资源";
+            body.put(JsonMessage.DATA, requestUri);
+        }
+        body.put(JsonMessage.MSG, msg);
 
-		return new ResponseEntity<>(body, HttpStatus.OK);
-	}
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
 }
