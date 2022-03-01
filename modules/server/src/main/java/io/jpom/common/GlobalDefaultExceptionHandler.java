b646bad23c8cf5a6a60a30af675fa4be4d4db4bb
bwcx_jzy <bwcx_jzy@163.com>
2022-03-01 13:30:16 +0800

    optimal 验证异常只是本地调试才输出日志

--- modules/server/src/main/java/io/jpom/common/GlobalDefaultExceptionHandler.java
+++ modules/server/src/main/java/io/jpom/common/GlobalDefaultExceptionHandler.java
@@ -28,10 +28,7 @@ import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.extra.servlet.ServletUtil;
 import cn.jiangzeyin.common.DefaultSystemLog;
 import cn.jiangzeyin.common.JsonMessage;
-import io.jpom.system.AgentException;
-import io.jpom.system.AuthorizeException;
-import io.jpom.system.JpomRuntimeException;
-import io.jpom.system.ServerConfigBean;
+import io.jpom.system.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConversionException;
@@ -56,98 +53,88 @@ import java.nio.file.AccessDeniedException;
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
 
-	/**
-	 * 声明要捕获的异常
-	 *
-	 * @param request  请求
-	 * @param response 响应
-	 * @param e        异常
-	 */
-	@ExceptionHandler({AuthorizeException.class, RuntimeException.class, Exception.class})
-	public void delExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e) {
-		if (e instanceof AuthorizeException) {
-			AuthorizeException authorizeException = (AuthorizeException) e;
-			ServletUtil.write(response, authorizeException.getJsonMessage().toString(), MediaType.APPLICATION_JSON_VALUE);
-		} else if (e instanceof JpomRuntimeException) {
-			DefaultSystemLog.getLog().error("global handle exception: {}", request.getRequestURI(), e.getCause());
-			ServletUtil.write(response, JsonMessage.getString(500, e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
-		} else {
-			DefaultSystemLog.getLog().error("global handle exception: {}", request.getRequestURI(), e);
-			boolean causedBy = ExceptionUtil.isCausedBy(e, AccessDeniedException.class);
-			if (causedBy) {
-				ServletUtil.write(response, JsonMessage.getString(500, "操作文件权限异常,请手动处理：" + e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
-				return;
-			}
-			ServletUtil.write(response, JsonMessage.getString(500, "服务异常：" + e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
-		}
-	}
+    /**
+     * 声明要捕获的异常
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param e        异常
+     */
+    @ExceptionHandler({AuthorizeException.class, RuntimeException.class, Exception.class})
+    public void delExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e) {
+        if (e instanceof AuthorizeException) {
+            AuthorizeException authorizeException = (AuthorizeException) e;
+            ServletUtil.write(response, authorizeException.getJsonMessage().toString(), MediaType.APPLICATION_JSON_VALUE);
+        } else if (e instanceof JpomRuntimeException) {
+            DefaultSystemLog.getLog().error("global handle exception: {}", request.getRequestURI(), e.getCause());
+            ServletUtil.write(response, JsonMessage.getString(500, e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
+        } else {
+            DefaultSystemLog.getLog().error("global handle exception: {}", request.getRequestURI(), e);
+            boolean causedBy = ExceptionUtil.isCausedBy(e, AccessDeniedException.class);
+            if (causedBy) {
+                ServletUtil.write(response, JsonMessage.getString(500, "操作文件权限异常,请手动处理：" + e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
+                return;
+            }
+            ServletUtil.write(response, JsonMessage.getString(500, "服务异常：" + e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
+        }
+    }
 
-	/**
-	 * 插件端异常
-	 * <p>
-	 * 避免重复记录堆栈
-	 *
-	 * @param request  请求
-	 * @param response 响应
-	 * @param e        异常
-	 * @author jzy
-	 * @since 2021-08-01
-	 */
-	@ExceptionHandler({AgentException.class})
-	public void agentExceptionHandler(HttpServletRequest request, HttpServletResponse response, AgentException e) {
-		Throwable cause = e.getCause();
-		if (cause != null) {
-			DefaultSystemLog.getLog().error("controller " + request.getRequestURI(), cause);
-		}
-		ServletUtil.write(response, JsonMessage.getString(405, e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
-	}
+    /**
+     * 插件端异常
+     * <p>
+     * 避免重复记录堆栈
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param e        异常
+     * @author jzy
+     * @since 2021-08-01
+     */
+    @ExceptionHandler({AgentException.class})
+    public void agentExceptionHandler(HttpServletRequest request, HttpServletResponse response, AgentException e) {
+        Throwable cause = e.getCause();
+        if (cause != null) {
+            DefaultSystemLog.getLog().error("controller " + request.getRequestURI(), cause);
+        }
+        ServletUtil.write(response, JsonMessage.getString(405, e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
+    }
 
-//	/**
-//	 * git 仓库操作相关异常
-//	 *
-//	 * @param request  请求
-//	 * @param response 响应
-//	 * @param e        异常
-//	 */
-//	@ExceptionHandler({GitAPIException.class})
-//	public void gitExceptionHandler(HttpServletRequest request, HttpServletResponse response, GitAPIException e) {
-//		DefaultSystemLog.getLog().warn("GitAPIException: " + request.getRequestURI(), e);
-//		ServletUtil.write(response, JsonMessage.getString(405, "git 仓库操作异常:" + e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
-//	}
+    /**
+     * 声明要捕获的异常 (参数，状态，验证异常)
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param e        异常
+     */
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class, ValidateException.class})
+    public void paramExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e) {
+        if (!ConfigBean.getInstance().isPro()) {
+            // 只是本地调试才输出日志
+            DefaultSystemLog.getLog().error("controller " + request.getRequestURI(), e);
+        }
+        String message = e.getMessage();
+        if (ObjectUtil.equals(message, ServerConfigBean.AUTHORIZE_TIME_OUT_CODE)) {
+            ServletUtil.write(response, JsonMessage.getString(ServerConfigBean.AUTHORIZE_TIME_OUT_CODE, "登录信息已失效,重新登录"), MediaType.APPLICATION_JSON_VALUE);
+        } else {
+            ServletUtil.write(response, JsonMessage.getString(405, message), MediaType.APPLICATION_JSON_VALUE);
+        }
+    }
 
-	/**
-	 * 声明要捕获的异常 (参数，状态，验证异常)
-	 *
-	 * @param request  请求
-	 * @param response 响应
-	 * @param e        异常
-	 */
-	@ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class, ValidateException.class})
-	public void paramExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e) {
-		DefaultSystemLog.getLog().error("controller " + request.getRequestURI(), e);
-		String message = e.getMessage();
-		if (ObjectUtil.equals(message, ServerConfigBean.AUTHORIZE_TIME_OUT_CODE)) {
-			ServletUtil.write(response, JsonMessage.getString(ServerConfigBean.AUTHORIZE_TIME_OUT_CODE, "登录信息已失效,重新登录"), MediaType.APPLICATION_JSON_VALUE);
-		} else {
-			ServletUtil.write(response, JsonMessage.getString(405, message), MediaType.APPLICATION_JSON_VALUE);
-		}
-	}
+    @ExceptionHandler({HttpMessageNotReadableException.class, HttpMessageConversionException.class})
+    @ResponseBody
+    public JsonMessage<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        DefaultSystemLog.getLog().warn("参数解析异常:{}", e.getMessage());
+        return new JsonMessage<>(HttpStatus.EXPECTATION_FAILED.value(), "传入的参数格式不正确");
+    }
 
-	@ExceptionHandler({HttpMessageNotReadableException.class, HttpMessageConversionException.class})
-	@ResponseBody
-	public JsonMessage<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-		DefaultSystemLog.getLog().warn("参数解析异常:{}", e.getMessage());
-		return new JsonMessage<>(HttpStatus.EXPECTATION_FAILED.value(), "传入的参数格式不正确");
-	}
+    @ExceptionHandler({HttpRequestMethodNotSupportedException.class, HttpMediaTypeNotSupportedException.class})
+    @ResponseBody
+    public JsonMessage<String> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        return new JsonMessage<>(HttpStatus.METHOD_NOT_ALLOWED.value(), "不被支持的请求方式", e.getMessage());
+    }
 
-	@ExceptionHandler({HttpRequestMethodNotSupportedException.class, HttpMediaTypeNotSupportedException.class})
-	@ResponseBody
-	public JsonMessage<String> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
-		return new JsonMessage<>(HttpStatus.METHOD_NOT_ALLOWED.value(), "不被支持的请求方式", e.getMessage());
-	}
-
-	@ExceptionHandler({NoHandlerFoundException.class})
-	public void handleNoHandlerFoundException(HttpServletResponse response, NoHandlerFoundException e) {
-		ServletUtil.write(response, JsonMessage.getString(HttpStatus.NOT_FOUND.value(), "没有找到对应的资源", e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
-	}
+    @ExceptionHandler({NoHandlerFoundException.class})
+    public void handleNoHandlerFoundException(HttpServletResponse response, NoHandlerFoundException e) {
+        ServletUtil.write(response, JsonMessage.getString(HttpStatus.NOT_FOUND.value(), "没有找到对应的资源", e.getMessage()), MediaType.APPLICATION_JSON_VALUE);
+    }
 }
