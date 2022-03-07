0fa69a43b30e00db895e5871417ab233c2090ae9
bwcx_jzy <bwcx_jzy@163.com>
2022-03-07 12:52:11 +0800

    optimal 自动添加本机 docker

--- modules/server/src/main/java/io/jpom/service/h2db/BaseWorkspaceService.java
+++ modules/server/src/main/java/io/jpom/service/h2db/BaseWorkspaceService.java
@@ -50,165 +50,168 @@ import java.util.function.Consumer;
  */
 public abstract class BaseWorkspaceService<T extends BaseWorkspaceModel> extends BaseDbService<T> {
 
-	/**
-	 * 根据工作空间查询
-	 *
-	 * @param request 请求
-	 * @return list
-	 */
-	public List<T> listByWorkspace(HttpServletRequest request) {
-		String workspaceId = this.getCheckUserWorkspace(request);
-		Entity entity = Entity.create();
-		entity.set("workspaceId", workspaceId);
-		List<Entity> entities = super.queryList(entity);
-		return super.entityToBeanList(entities);
-	}
+    /**
+     * 根据工作空间查询
+     *
+     * @param request 请求
+     * @return list
+     */
+    public List<T> listByWorkspace(HttpServletRequest request) {
+        String workspaceId = this.getCheckUserWorkspace(request);
+        Entity entity = Entity.create();
+        entity.set("workspaceId", workspaceId);
+        List<Entity> entities = super.queryList(entity);
+        return super.entityToBeanList(entities);
+    }
 
-	/**
-	 * 根据主键ID + 请信息查询
-	 *
-	 * @param keyValue ID
-	 * @param request  请求
-	 * @return data
-	 */
-	public T getByKey(String keyValue, HttpServletRequest request) {
-		String workspace = this.getCheckUserWorkspace(request);
-		return super.getByKey(keyValue, true, entity -> entity.set("workspaceId", workspace));
-	}
+    /**
+     * 根据主键ID + 请信息查询
+     *
+     * @param keyValue ID
+     * @param request  请求
+     * @return data
+     */
+    public T getByKey(String keyValue, HttpServletRequest request) {
+        String workspace = this.getCheckUserWorkspace(request);
+        return super.getByKey(keyValue, true, entity -> entity.set("workspaceId", workspace));
+    }
 
-	/**
-	 * 根据主键ID + 用户ID
-	 *
-	 * @param keyValue  ID
-	 * @param userModel 用户
-	 * @return data
-	 */
-	public T getByKey(String keyValue, UserModel userModel) {
-		T byKey = super.getByKey(keyValue, false);
-		if (byKey == null) {
-			return null;
-		}
-		this.checkUserWorkspace(byKey.getWorkspaceId(), userModel);
-		return byKey;
-	}
+    /**
+     * 根据主键ID + 用户ID
+     *
+     * @param keyValue  ID
+     * @param userModel 用户
+     * @return data
+     */
+    public T getByKey(String keyValue, UserModel userModel) {
+        T byKey = super.getByKey(keyValue, false);
+        if (byKey == null) {
+            return null;
+        }
+        this.checkUserWorkspace(byKey.getWorkspaceId(), userModel);
+        return byKey;
+    }
 
-	@Override
-	protected void fillInsert(T t) {
-		super.fillInsert(t);
-		if (StrUtil.isEmpty(t.getWorkspaceId())) {
-			// 自动绑定 工作空间ID
-			ServletRequestAttributes servletRequestAttributes = BaseServerController.tryGetRequestAttributes();
-			if (servletRequestAttributes != null) {
-				HttpServletRequest request = servletRequestAttributes.getRequest();
-				String workspaceId = this.getCheckUserWorkspace(request);
-				t.setWorkspaceId(workspaceId);
-			} else {
-				t.setWorkspaceId(Const.WORKSPACE_DEFAULT_ID);
-			}
-		} else {
-			// 检查权限
-			this.checkUserWorkspace(t.getWorkspaceId());
-		}
-	}
+    @Override
+    protected void fillInsert(T t) {
+        super.fillInsert(t);
+        if (StrUtil.isEmpty(t.getWorkspaceId())) {
+            // 自动绑定 工作空间ID
+            ServletRequestAttributes servletRequestAttributes = BaseServerController.tryGetRequestAttributes();
+            if (servletRequestAttributes != null) {
+                HttpServletRequest request = servletRequestAttributes.getRequest();
+                String workspaceId = this.getCheckUserWorkspace(request);
+                t.setWorkspaceId(workspaceId);
+            } else {
+                t.setWorkspaceId(Const.WORKSPACE_DEFAULT_ID);
+            }
+        } else {
+            // 检查权限
+            String modifyUser = t.getModifyUser();
+            if (!StrUtil.equals(modifyUser, UserModel.SYSTEM_ADMIN)) {
+                this.checkUserWorkspace(t.getWorkspaceId());
+            }
+        }
+    }
 
-	@Override
-	public PageResultDto<T> listPage(HttpServletRequest request) {
-		// 验证工作空间权限
-		Map<String, String> paramMap = ServletUtil.getParamMap(request);
-		String workspaceId = this.getCheckUserWorkspace(request);
-		paramMap.put("workspaceId", workspaceId);
-		return super.listPage(paramMap);
-	}
+    @Override
+    public PageResultDto<T> listPage(HttpServletRequest request) {
+        // 验证工作空间权限
+        Map<String, String> paramMap = ServletUtil.getParamMap(request);
+        String workspaceId = this.getCheckUserWorkspace(request);
+        paramMap.put("workspaceId", workspaceId);
+        return super.listPage(paramMap);
+    }
 
-	/**
-	 * 删除
-	 *
-	 * @param keyValue 主键
-	 * @param request  请求信息
-	 * @return 影响行数
-	 */
-	public int delByKey(String keyValue, HttpServletRequest request) {
-		String workspace = this.getCheckUserWorkspace(request);
-		return super.delByKey(keyValue, entity -> entity.set("workspaceId", workspace));
-	}
+    /**
+     * 删除
+     *
+     * @param keyValue 主键
+     * @param request  请求信息
+     * @return 影响行数
+     */
+    public int delByKey(String keyValue, HttpServletRequest request) {
+        String workspace = this.getCheckUserWorkspace(request);
+        return super.delByKey(keyValue, entity -> entity.set("workspaceId", workspace));
+    }
 
-	/**
-	 * 删除,根据工作空间删除
-	 *
-	 * @param consumer 回调
-	 * @param request  请求信息
-	 * @return 影响行数
-	 */
-	public int delByWorkspace(HttpServletRequest request, Consumer<Entity> consumer) {
-		String workspace = this.getCheckUserWorkspace(request);
-		return super.delByKey(null, entity -> {
-			entity.set("workspaceId", workspace);
-			consumer.accept(entity);
-			int size = entity.size();
-			Assert.state(size > 1, "没有添加任何参数");
-		});
-	}
+    /**
+     * 删除,根据工作空间删除
+     *
+     * @param consumer 回调
+     * @param request  请求信息
+     * @return 影响行数
+     */
+    public int delByWorkspace(HttpServletRequest request, Consumer<Entity> consumer) {
+        String workspace = this.getCheckUserWorkspace(request);
+        return super.delByKey(null, entity -> {
+            entity.set("workspaceId", workspace);
+            consumer.accept(entity);
+            int size = entity.size();
+            Assert.state(size > 1, "没有添加任何参数");
+        });
+    }
 
-	/**
-	 * 获取 工作空间ID 并判断是否有权限
-	 *
-	 * @param request 请求对象
-	 * @return 工作空间ID
-	 */
-	public String getCheckUserWorkspace(HttpServletRequest request) {
-		String workspaceId = ServletUtil.getHeader(request, Const.WORKSPACEID_REQ_HEADER, CharsetUtil.CHARSET_UTF_8);
-		Assert.hasText(workspaceId, "请选择工作空间");
-		//
-		this.checkUserWorkspace(workspaceId);
-		return workspaceId;
-	}
+    /**
+     * 获取 工作空间ID 并判断是否有权限
+     *
+     * @param request 请求对象
+     * @return 工作空间ID
+     */
+    public String getCheckUserWorkspace(HttpServletRequest request) {
+        String workspaceId = ServletUtil.getHeader(request, Const.WORKSPACEID_REQ_HEADER, CharsetUtil.CHARSET_UTF_8);
+        Assert.hasText(workspaceId, "请选择工作空间");
+        //
+        this.checkUserWorkspace(workspaceId);
+        return workspaceId;
+    }
 
-	/**
-	 * 判断用户是否有对应工作空间权限
-	 *
-	 * @param workspaceId 工作空间ID
-	 */
-	private void checkUserWorkspace(String workspaceId) {
-		UserModel userModel = BaseServerController.getUserByThreadLocal();
-		this.checkUserWorkspace(workspaceId, userModel);
-	}
+    /**
+     * 判断用户是否有对应工作空间权限
+     *
+     * @param workspaceId 工作空间ID
+     */
+    private void checkUserWorkspace(String workspaceId) {
+        UserModel userModel = BaseServerController.getUserByThreadLocal();
+        this.checkUserWorkspace(workspaceId, userModel);
+    }
 
-	/**
-	 * 判断用户是否有对应工作空间权限
-	 *
-	 * @param workspaceId 工作空间ID
-	 */
-	protected void checkUserWorkspace(String workspaceId, UserModel userModel) {
-		Assert.notNull(userModel, "没有对应的用户");
-		if (StrUtil.equals(userModel.getId(), UserModel.SYSTEM_ADMIN)) {
-			// 系统执行，发行检查
-			return;
-		}
-		if (userModel.isSuperSystemUser()) {
-			// 超级管理员
-			return;
-		}
-		// 查询绑定的权限
-		UserBindWorkspaceService userBindWorkspaceService = SpringUtil.getBean(UserBindWorkspaceService.class);
-		boolean exists = userBindWorkspaceService.exists(userModel.getId(), workspaceId);
-		Assert.state(exists, "没有对应的工作空间权限");
-	}
+    /**
+     * 判断用户是否有对应工作空间权限
+     *
+     * @param workspaceId 工作空间ID
+     */
+    protected void checkUserWorkspace(String workspaceId, UserModel userModel) {
+        Assert.notNull(userModel, "没有对应的用户");
+        if (StrUtil.equals(userModel.getId(), UserModel.SYSTEM_ADMIN)) {
+            // 系统执行，发行检查
+            return;
+        }
+        if (userModel.isSuperSystemUser()) {
+            // 超级管理员
+            return;
+        }
+        // 查询绑定的权限
+        UserBindWorkspaceService userBindWorkspaceService = SpringUtil.getBean(UserBindWorkspaceService.class);
+        boolean exists = userBindWorkspaceService.exists(userModel.getId(), workspaceId);
+        Assert.state(exists, "没有对应的工作空间权限");
+    }
 
 
-	public List<T> listById(Collection<String> ids, HttpServletRequest request) {
-		String workspaceId = this.getCheckUserWorkspace(request);
-		return super.listById(ids, entity -> entity.set("workspaceId", workspaceId));
-	}
+    public List<T> listById(Collection<String> ids, HttpServletRequest request) {
+        String workspaceId = this.getCheckUserWorkspace(request);
+        return super.listById(ids, entity -> entity.set("workspaceId", workspaceId));
+    }
 
-	/**
-	 * 更新数据,根据ID+工作空间ID
-	 *
-	 * @param info    更新的数据
-	 * @param request 请求
-	 * @return 影响行数
-	 */
-	public int updateById(T info, HttpServletRequest request) {
-		String workspaceId = this.getCheckUserWorkspace(request);
-		return super.updateById(info, entity -> entity.set("workspaceId", workspaceId));
-	}
+    /**
+     * 更新数据,根据ID+工作空间ID
+     *
+     * @param info    更新的数据
+     * @param request 请求
+     * @return 影响行数
+     */
+    public int updateById(T info, HttpServletRequest request) {
+        String workspaceId = this.getCheckUserWorkspace(request);
+        return super.updateById(info, entity -> entity.set("workspaceId", workspaceId));
+    }
 }
