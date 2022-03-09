53c09f9d7e59297ddb737ab68a2b16a70a5d461a
bwcx_jzy <bwcx_jzy@163.com>
2022-03-09 10:23:17 +0800

    add xml header

--- web-vue/src/pages/system/workspace.vue
+++ web-vue/src/pages/system/workspace.vue
@@ -77,8 +77,9 @@
         bordered
         :rowKey="(record, index) => index"
       >
-        <a-tooltip slot="value" slot-scope="text" placement="topLeft" :title="text">
-          <span>{{ text }}</span>
+        <a-tooltip slot="value" slot-scope="text, item" placement="topLeft" :title="item.privacy === 1 ? '隐私字段' : text">
+          <a-icon v-if="item.privacy === 1" type="eye-invisible" />
+          <span v-else>{{ text }}</span>
         </a-tooltip>
         <a-tooltip slot="name" slot-scope="text" placement="topLeft" :title="text">
           <span>{{ text }}</span>
@@ -119,7 +120,7 @@
         <a-form-model-item>
           <template slot="label">
             分发节点
-            <a-tooltip v-show="!temp.id">
+            <a-tooltip v-show="!envTemp.id">
               <template slot="title"> 分发节点是指将变量同步到对应节点，在节点脚本中也可以使用当前变量</template>
               <a-icon type="question-circle" theme="filled" />
             </a-tooltip>
