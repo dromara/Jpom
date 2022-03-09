6fe171a0eec5e944a715ea0bdcadeca9f02217d5
bwcx_jzy <bwcx_jzy@163.com>
2022-03-09 22:19:22 +0800

    add script path header

--- modules/server/script/Server.sh
+++ modules/server/script/Server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # The MIT License (MIT)
 #
 # Copyright (c) 2019 Code Technology Studio
@@ -19,7 +20,8 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# try load env
+#
+
 if [ -f /etc/profile ]; then
     . /etc/profile
 fi
