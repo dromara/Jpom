6f0cd3ddc00a0ee8ea42ee370f62734b824129b1
bwcx_jzy <bwcx_jzy@163.com>
2022-03-09 22:19:22 +0800

    add script path header

--- script/push.bat
+++ script/push.bat
@@ -1,3 +1,26 @@
+@REM
+@REM The MIT License (MIT)
+@REM
+@REM Copyright (c) 2019 Code Technology Studio
+@REM
+@REM Permission is hereby granted, free of charge, to any person obtaining a copy of
+@REM this software and associated documentation files (the "Software"), to deal in
+@REM the Software without restriction, including without limitation the rights to
+@REM use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+@REM the Software, and to permit persons to whom the Software is furnished to do so,
+@REM subject to the following conditions:
+@REM
+@REM The above copyright notice and this permission notice shall be included in all
+@REM copies or substantial portions of the Software.
+@REM
+@REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+@REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+@REM FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+@REM COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+@REM IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+@REM CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+@REM
+
 @REM Jpom Multi-branch, multi-remote merge code commands
 
 @echo off
--- script/release.bat
+++ script/release.bat
@@ -1,9 +1,32 @@
+@REM
+@REM The MIT License (MIT)
+@REM
+@REM Copyright (c) 2019 Code Technology Studio
+@REM
+@REM Permission is hereby granted, free of charge, to any person obtaining a copy of
+@REM this software and associated documentation files (the "Software"), to deal in
+@REM the Software without restriction, including without limitation the rights to
+@REM use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+@REM the Software, and to permit persons to whom the Software is furnished to do so,
+@REM subject to the following conditions:
+@REM
+@REM The above copyright notice and this permission notice shall be included in all
+@REM copies or substantial portions of the Software.
+@REM
+@REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+@REM IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+@REM FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+@REM COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+@REM IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+@REM CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+@REM
+
 @echo off
 
-@REM ¿ìËÙ´ò°üÏîÄ¿½Å±¾
+@REM ï¿½ï¿½ï¿½Ù´ï¿½ï¿½ï¿½ï¿½Ä¿ï¿½Å±ï¿½
 
-@REM ¹¹½¨Ç°¶Ë
+@REM ï¿½ï¿½ï¿½ï¿½Ç°ï¿½ï¿½
 call cd ../ && cd web-vue && npm i && npm run build
 
-@REM ¹¹½¨ Java
+@REM ï¿½ï¿½ï¿½ï¿½ Java
 call cd ../ && mvn clean package
