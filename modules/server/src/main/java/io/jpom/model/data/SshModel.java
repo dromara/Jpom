7dcceec0a5dce52a8203ecc56a258885e2c60fcc
bwcx_jzy <bwcx_jzy@163.com>
2022-03-25 19:19:22 +0800

    optimal ssh private key content connect

--- modules/server/src/main/java/io/jpom/model/data/SshModel.java
+++ modules/server/src/main/java/io/jpom/model/data/SshModel.java
@@ -30,6 +30,7 @@ import com.alibaba.fastjson.JSONArray;
 import io.jpom.model.BaseWorkspaceModel;
 import io.jpom.service.h2db.TableName;
 import io.jpom.util.StringUtil;
+import lombok.Data;
 
 import java.nio.charset.Charset;
 import java.util.List;
@@ -41,210 +42,132 @@ import java.util.List;
  * @date 2019/8/9
  */
 @TableName(value = "SSH_INFO", name = "SSH 信息")
+@Data
 public class SshModel extends BaseWorkspaceModel {
 
-	private String name;
-	private String host;
-	private Integer port;
-	private String user;
-	private String password;
-	/**
-	 * 编码格式
-	 */
-	private String charset;
+    private String name;
+    private String host;
+    private Integer port;
+    private String user;
+    private String password;
+    /**
+     * 编码格式
+     */
+    private String charset;
 
-	/**
-	 * 文件目录
-	 */
-	private String fileDirs;
+    /**
+     * 文件目录
+     */
+    private String fileDirs;
 
-	/**
-	 * ssh 私钥
-	 */
-	private String privateKey;
+    /**
+     * ssh 私钥
+     */
+    private String privateKey;
 
-	private String connectType;
+    private String connectType;
 
-	/**
-	 * 不允许执行的命令
-	 */
-	private String notAllowedCommand;
+    /**
+     * 不允许执行的命令
+     */
+    private String notAllowedCommand;
 
-	/**
-	 * 允许编辑的后缀文件
-	 */
-	private String allowEditSuffix;
+    /**
+     * 允许编辑的后缀文件
+     */
+    private String allowEditSuffix;
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public ConnectType connectType() {
+        return EnumUtil.fromString(ConnectType.class, this.connectType, ConnectType.PASS);
+    }
 
-	public String getNotAllowedCommand() {
-		return notAllowedCommand;
-	}
+    public List<String> fileDirs() {
+        return StringUtil.jsonConvertArray(this.fileDirs, String.class);
+    }
 
-	public void setNotAllowedCommand(String notAllowedCommand) {
-		this.notAllowedCommand = notAllowedCommand;
-	}
+    public void fileDirs(List<String> fileDirs) {
+        if (fileDirs != null) {
+            for (int i = fileDirs.size() - 1; i >= 0; i--) {
+                String s = fileDirs.get(i);
+                fileDirs.set(i, FileUtil.normalize(s));
+            }
+            this.fileDirs = JSONArray.toJSONString(fileDirs);
+        } else {
+            this.fileDirs = null;
+        }
+    }
 
-	public ConnectType connectType() {
-		return EnumUtil.fromString(ConnectType.class, this.connectType, ConnectType.PASS);
-	}
+    public byte[] password() {
+        byte[] pas = null;
+        if (StrUtil.isNotEmpty(this.getPassword())) {
+            pas = this.getPassword().getBytes();
+        }
+        return pas;
+    }
 
-	public String getConnectType() {
-		return connectType;
-	}
+    public Charset getCharsetT() {
+        Charset charset;
+        try {
+            charset = Charset.forName(this.getCharset());
+        } catch (Exception e) {
+            charset = CharsetUtil.CHARSET_UTF_8;
+        }
+        return charset;
+    }
 
-	public void setConnectType(String connectType) {
-		this.connectType = connectType;
-	}
+    public List<String> allowEditSuffix() {
+        return StringUtil.jsonConvertArray(this.allowEditSuffix, String.class);
+    }
 
-	public String getPrivateKey() {
-		return privateKey;
-	}
+    public void allowEditSuffix(List<String> allowEditSuffix) {
+        if (allowEditSuffix == null) {
+            this.allowEditSuffix = null;
+        } else {
+            this.allowEditSuffix = JSONArray.toJSONString(allowEditSuffix);
+        }
+    }
 
-	public void setPrivateKey(String privateKey) {
-		this.privateKey = privateKey;
-	}
+    /**
+     * 检查是否包含禁止命令
+     *
+     * @param sshItem   实体
+     * @param inputItem 输入的命令
+     * @return false 存在禁止输入的命令
+     */
+    public static boolean checkInputItem(SshModel sshItem, String inputItem) {
+        // 检查禁止执行的命令
+        String notAllowedCommand = StrUtil.emptyToDefault(sshItem.getNotAllowedCommand(), StrUtil.EMPTY).toLowerCase();
+        if (StrUtil.isEmpty(notAllowedCommand)) {
+            return true;
+        }
+        List<String> split = StrUtil.splitTrim(notAllowedCommand, StrUtil.COMMA);
+        inputItem = inputItem.toLowerCase();
+        List<String> commands = StrUtil.splitTrim(inputItem, StrUtil.CR);
+        commands.addAll(StrUtil.split(inputItem, "&"));
+        for (String s : split) {
+            //
+            boolean anyMatch = commands.stream().anyMatch(item -> StrUtil.startWithAny(item, s + StrUtil.SPACE, ("&" + s + StrUtil.SPACE), StrUtil.SPACE + s + StrUtil.SPACE));
+            if (anyMatch) {
+                return false;
+            }
+            //
+            anyMatch = commands.stream().anyMatch(item -> StrUtil.equals(item, s));
+            if (anyMatch) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public String getFileDirs() {
-		return fileDirs;
-	}
-
-	public void setFileDirs(String fileDirs) {
-		this.fileDirs = fileDirs;
-	}
-
-	public List<String> fileDirs() {
-		return StringUtil.jsonConvertArray(this.fileDirs, String.class);
-	}
-
-	public void fileDirs(List<String> fileDirs) {
-		if (fileDirs != null) {
-			for (int i = fileDirs.size() - 1; i >= 0; i--) {
-				String s = fileDirs.get(i);
-				fileDirs.set(i, FileUtil.normalize(s));
-			}
-			this.fileDirs = JSONArray.toJSONString(fileDirs);
-		} else {
-			this.fileDirs = null;
-		}
-	}
-
-	public String getHost() {
-		return host;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public Integer getPort() {
-		return port;
-	}
-
-	public void setPort(Integer port) {
-		this.port = port;
-	}
-
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getCharset() {
-		return charset;
-	}
-
-	public void setCharset(String charset) {
-		this.charset = charset;
-	}
-
-	public Charset getCharsetT() {
-		Charset charset;
-		try {
-			charset = Charset.forName(this.getCharset());
-		} catch (Exception e) {
-			charset = CharsetUtil.CHARSET_UTF_8;
-		}
-		return charset;
-	}
-
-	public List<String> allowEditSuffix() {
-		return StringUtil.jsonConvertArray(this.allowEditSuffix, String.class);
-	}
-
-	public void allowEditSuffix(List<String> allowEditSuffix) {
-		if (allowEditSuffix == null) {
-			this.allowEditSuffix = null;
-		} else {
-			this.allowEditSuffix = JSONArray.toJSONString(allowEditSuffix);
-		}
-	}
-
-	public String getAllowEditSuffix() {
-		return allowEditSuffix;
-	}
-
-	public void setAllowEditSuffix(String allowEditSuffix) {
-		this.allowEditSuffix = allowEditSuffix;
-	}
-
-	/**
-	 * 检查是否包含禁止命令
-	 *
-	 * @param sshItem   实体
-	 * @param inputItem 输入的命令
-	 * @return false 存在禁止输入的命令
-	 */
-	public static boolean checkInputItem(SshModel sshItem, String inputItem) {
-		// 检查禁止执行的命令
-		String notAllowedCommand = StrUtil.emptyToDefault(sshItem.getNotAllowedCommand(), StrUtil.EMPTY).toLowerCase();
-		if (StrUtil.isEmpty(notAllowedCommand)) {
-			return true;
-		}
-		List<String> split = StrUtil.splitTrim(notAllowedCommand, StrUtil.COMMA);
-		inputItem = inputItem.toLowerCase();
-		List<String> commands = StrUtil.splitTrim(inputItem, StrUtil.CR);
-		commands.addAll(StrUtil.split(inputItem, "&"));
-		for (String s : split) {
-			//
-			boolean anyMatch = commands.stream().anyMatch(item -> StrUtil.startWithAny(item, s + StrUtil.SPACE, ("&" + s + StrUtil.SPACE), StrUtil.SPACE + s + StrUtil.SPACE));
-			if (anyMatch) {
-				return false;
-			}
-			//
-			anyMatch = commands.stream().anyMatch(item -> StrUtil.equals(item, s));
-			if (anyMatch) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public enum ConnectType {
-		/**
-		 * 账号密码
-		 */
-		PASS,
-		/**
-		 * 密钥
-		 */
-		PUBKEY
-	}
+    public enum ConnectType {
+        /**
+         * 账号密码
+         */
+        PASS,
+        /**
+         * 密钥
+         */
+        PUBKEY
+    }
 }
--- modules/server/src/main/java/io/jpom/util/JschUtils.java
+++ modules/server/src/main/java/io/jpom/util/JschUtils.java
@@ -22,21 +22,18 @@
  */
 package io.jpom.util;
 
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.ssh.JschRuntimeException;
-import com.jcraft.jsch.Identity;
-import com.jcraft.jsch.IdentityRepository;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.KeyPair;
-import com.jcraft.jsch.Session;
+import cn.hutool.extra.ssh.JschUtil;
+import com.jcraft.jsch.*;
 
 import java.lang.reflect.Method;
-import java.nio.charset.StandardCharsets;
 
 /**
- *
  * hutool 默认封装的 SSH 工具，仅支持传入 SSL Private Key File，不支持  Private Key Content。
- *
+ * <p>
  * 本实现用于直接采用 Private Key Content 登录。
  *
  * <pre>
@@ -51,7 +48,7 @@ import java.nio.charset.StandardCharsets;
  *
  * @author zhenqin
  */
-public class JschUtil extends cn.hutool.extra.ssh.JschUtil {
+public class JschUtils {
 
 
     public final static String HEADER = "-----BEGIN RSA PRIVATE KEY-----";
@@ -60,33 +57,23 @@ public class JschUtil extends cn.hutool.extra.ssh.JschUtil {
 
     /**
      * GETKEYTYPENAME_METHOD getKeyTypeName 是私有的
+     *
+     * @see KeyPair#getKeyTypeName
      */
-    private static final Method GETKEYTYPENAME_METHOD;
-
-
-    static {
-        try {
-            GETKEYTYPENAME_METHOD = KeyPair.class.getDeclaredMethod("getKeyTypeName");
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException("get " + KeyPair.class.getName() + ".getKeyTypeName() method error!", e);
-        }
-    }
+    private static final Method GET_KEY_TYPE_NAME_METHOD = ReflectUtil.getMethod(KeyPair.class, "getKeyTypeName");
 
     static class ContentIdentity implements Identity {
 
-        private JSch jsch;
         private KeyPair kpair;
-        private String identity;
+        private final String identity;
 
 
-        static ContentIdentity newInstance(byte[] prvContent, byte[] pubContent, String username, JSch jsch) throws Exception{
+        static ContentIdentity newInstance(byte[] prvContent, byte[] pubContent, String username, JSch jsch) throws Exception {
             KeyPair kpair = KeyPair.load(jsch, prvContent, pubContent);
-            return new ContentIdentity(jsch, username, kpair);
+            return new ContentIdentity(username, kpair);
         }
 
-
-        private ContentIdentity(JSch jsch, String name, KeyPair kpair) throws JSchException {
-            this.jsch = jsch;
+        private ContentIdentity(String name, KeyPair kpair) {
             this.identity = name;
             this.kpair = kpair;
         }
@@ -94,51 +81,57 @@ public class JschUtil extends cn.hutool.extra.ssh.JschUtil {
 
         /**
          * Decrypts this identity with the specified pass-phrase.
+         *
          * @param passphrase the pass-phrase for this identity.
          * @return <tt>true</tt> if the decryption is succeeded
          * or this identity is not cyphered.
          */
-        public boolean setPassphrase(byte[] passphrase) throws JSchException{
+        @Override
+        public boolean setPassphrase(byte[] passphrase) {
             return kpair.decrypt(passphrase);
         }
 
         /**
          * Returns the public-key blob.
+         *
          * @return the public-key blob
          */
-        public byte[] getPublicKeyBlob(){
+        @Override
+        public byte[] getPublicKeyBlob() {
             return kpair.getPublicKeyBlob();
         }
 
         /**
          * Signs on data with this identity, and returns the result.
+         *
          * @param data data to be signed
          * @return the signature
          */
-        public byte[] getSignature(byte[] data){
+        @Override
+        public byte[] getSignature(byte[] data) {
             return kpair.getSignature(data);
         }
 
         /**
-         * @deprecated This method should not be invoked.
          * @see #setPassphrase(byte[] passphrase)
+         * @deprecated This method should not be invoked.
          */
-        public boolean decrypt(){
+        @Override
+        public boolean decrypt() {
             throw new RuntimeException("not implemented");
         }
 
         /**
          * Returns the name of the key algorithm.
+         *
          * @return "ssh-rsa" or "ssh-dss"
          */
-        public String getAlgName(){
+        @Override
+        public String getAlgName() {
             try {
-                GETKEYTYPENAME_METHOD.setAccessible(true);
-                //byte[] name = kpair.getKeyTypeName();
-                byte[] name = (byte[]) GETKEYTYPENAME_METHOD.invoke(kpair);
-                return new String(name, StandardCharsets.UTF_8);
-            }
-            catch (Exception e){
+                byte[] name = ReflectUtil.invoke(kpair, GET_KEY_TYPE_NAME_METHOD);
+                return new String(name, CharsetUtil.CHARSET_UTF_8);
+            } catch (Exception e) {
                 return null;
             }
         }
@@ -147,31 +140,36 @@ public class JschUtil extends cn.hutool.extra.ssh.JschUtil {
          * Returns the name of this identity.
          * It will be useful to identify this object in the {@link IdentityRepository}.
          */
-        public String getName(){
+        @Override
+        public String getName() {
             return identity;
         }
 
         /**
          * Returns <tt>true</tt> if this identity is cyphered.
+         *
          * @return <tt>true</tt> if this identity is cyphered.
          */
-        public boolean isEncrypted(){
+        @Override
+        public boolean isEncrypted() {
             return kpair.isEncrypted();
         }
 
         /**
          * Disposes internally allocated data, like byte array for the private key.
          */
-        public void clear(){
+        @Override
+        public void clear() {
             kpair.dispose();
             kpair = null;
         }
 
         /**
          * Returns an instance of {@link KeyPair} used in this {@link Identity}.
+         *
          * @return an instance of {@link KeyPair} used in this {@link Identity}.
          */
-        public KeyPair getKeyPair(){
+        public KeyPair getKeyPair() {
             return kpair;
         }
     }
@@ -179,21 +177,23 @@ public class JschUtil extends cn.hutool.extra.ssh.JschUtil {
 
     /**
      * 通过私钥获取 Session
-     * @param sshHost ssh 目标节点IP、域名、机器名
-     * @param sshPort ssh 服务端口号，一般为 22
-     * @param sshUser ssh 目标节点登录用户
+     *
+     * @param sshHost    ssh 目标节点IP、域名、机器名
+     * @param sshPort    ssh 服务端口号，一般为 22
+     * @param sshUser    ssh 目标节点登录用户
      * @param privateKey 采用的私钥，一般由 ssh-keygen 生成。必须包含完整的前后缀：-----BEGIN RSA PRIVATE KEY-----
-     * @return
+     * @param passphrase 密码
+     * @return Session
      */
-    public static Session createSession(String sshHost, int sshPort, String sshUser, byte[] privateKey) {
+    public static Session createSession(String sshHost, int sshPort, String sshUser, String privateKey, byte[] passphrase) {
         final JSch jsch = new JSch();
         try {
-            Identity identity = ContentIdentity.newInstance(privateKey, null, sshUser, jsch);
-            jsch.addIdentity(identity, null);
+            byte[] privateKeyByte = StrUtil.bytes(privateKey);
+            Identity identity = ContentIdentity.newInstance(privateKeyByte, null, sshUser, jsch);
+            jsch.addIdentity(identity, passphrase);
         } catch (Exception e) {
             throw new JschRuntimeException(e);
         }
-
-        return cn.hutool.extra.ssh.JschUtil.createSession(jsch, sshHost, sshPort, sshUser);
+        return JschUtil.createSession(jsch, sshHost, sshPort, sshUser);
     }
 }
