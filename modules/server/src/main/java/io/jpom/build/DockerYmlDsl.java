ed939326b301cb3453332417fea3eb1fda61ab12
bwcx_jzy <bwcx_jzy@163.com>
2022-03-31 18:16:15 +0800

    fix

--- modules/server/src/main/java/io/jpom/build/DockerYmlDsl.java
+++ modules/server/src/main/java/io/jpom/build/DockerYmlDsl.java
@@ -25,10 +25,7 @@ package io.jpom.build;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.http.HttpResponse;
-import cn.hutool.http.HttpStatus;
-import cn.hutool.http.HttpUtil;
-import cn.hutool.http.Method;
+import cn.hutool.http.*;
 import cn.hutool.setting.yaml.YamlUtil;
 import io.jpom.model.BaseJsonModel;
 import lombok.Data;
@@ -54,141 +51,142 @@ import java.util.Set;
 @Data
 public class DockerYmlDsl extends BaseJsonModel {
 
-	/**
-	 * 基础镜像
-	 */
-	private String runsOn;
-	/**
-	 * 使用对应到 docker tag 构建
-	 */
-	private String fromTag;
-	/**
-	 * 构建步骤
-	 */
-	private List<Map<String, Object>> steps;
+    /**
+     * 基础镜像
+     */
+    private String runsOn;
+    /**
+     * 使用对应到 docker tag 构建
+     */
+    private String fromTag;
+    /**
+     * 构建步骤
+     */
+    private List<Map<String, Object>> steps;
 
-	/**
-	 * 将本地文件复制到 容器
-	 * <p>
-	 * <host path>:<container path>:true
-	 * <p>
-	 * * If this flag is set to true, all children of the local directory will be copied to the remote without the root directory. For ex: if
-	 * * I have root/titi and root/tata and the remote path is /var/data. dirChildrenOnly = true will create /var/data/titi and /var/data/tata
-	 * * dirChildrenOnly = false will create /var/data/root/titi and /var/data/root/tata
-	 * *
-	 * * @param dirChildrenOnly
-	 * *            if root directory is ignored
-	 */
-	private List<String> copy;
-	/**
-	 * bind mounts 将宿主机上的任意位置的文件或者目录挂在到容器 （--mount type=bind,src=源目录,dst=目标目录）
-	 * /host:/container:ro
-	 */
-	private List<String> binds;
-	/**
-	 * 环境变量
-	 */
-	private Map<String, String> env;
+    /**
+     * 将本地文件复制到 容器
+     * <p>
+     * <host path>:<container path>:true
+     * <p>
+     * * If this flag is set to true, all children of the local directory will be copied to the remote without the root directory. For ex: if
+     * * I have root/titi and root/tata and the remote path is /var/data. dirChildrenOnly = true will create /var/data/titi and /var/data/tata
+     * * dirChildrenOnly = false will create /var/data/root/titi and /var/data/root/tata
+     * *
+     * * @param dirChildrenOnly
+     * *            if root directory is ignored
+     */
+    private List<String> copy;
+    /**
+     * bind mounts 将宿主机上的任意位置的文件或者目录挂在到容器 （--mount type=bind,src=源目录,dst=目标目录）
+     * /host:/container:ro
+     */
+    private List<String> binds;
+    /**
+     * 环境变量
+     */
+    private Map<String, String> env;
 
-	public void check() {
-		Assert.hasText(runsOn, "请填写runsOn。目前仅支持 ubuntu-latest.更多支持敬请期待!");
-		Assert.state(CollUtil.isNotEmpty(steps), "请填写 steps");
-		stepsCheck();
-	}
+    public void check() {
+        Assert.hasText(runsOn, "请填写runsOn。目前仅支持 ubuntu-latest.更多支持敬请期待!");
+        Assert.state(CollUtil.isNotEmpty(steps), "请填写 steps");
+        stepsCheck();
+    }
 
-	public void stepsCheck() {
-		Set<String> usesSet = new HashSet<>();
-		boolean containsRun = false;
-		for (Map<String, Object> step : steps) {
-			if (!containsRun && step.containsKey("run")) {
-				containsRun = true;
-			}
-			if (step.containsKey("env")) {
-				Assert.isInstanceOf(Map.class, step.get("env"), "env 必须是 map 类型");
-			}
-			if (step.containsKey("uses")) {
-				List<String> supportedPlugins = ListUtil.of("node", "java", "maven", "cache", "go", "python3");
-				Assert.isInstanceOf(String.class, step.get("uses"), "uses 只支持 String 类型");
-				String uses = (String) step.get("uses");
-				Assert.isTrue(supportedPlugins.contains(uses), String.format("目前仅支持的插件: %s", supportedPlugins));
-				if ("node".equals(uses)) {
-					nodePluginCheck(step);
-				} else if ("java".equals(uses)) {
-					javaPluginCheck(step);
-				} else if ("maven".equals(uses)) {
-					mavenPluginCheck(step);
-				} else if ("cache".equals(uses)) {
-					cachePluginCheck(step);
-				} else if ("go".equals(uses)) {
-					goPluginCheck(step);
-				} else if ("python3".equals(uses)) {
-					python3PluginCheck(step);
-				}
-				usesSet.add(uses);
-			}
-		}
-		if (usesSet.contains("maven") && !usesSet.contains("java")) {
-			throw new IllegalArgumentException("maven 插件依赖 java , 使用 maven 插件必须优先引入 java 插件");
-		}
-		Assert.isTrue(containsRun, "steps 中没有发现任何 run , run 用于执行命令");
-	}
+    public void stepsCheck() {
+        Set<String> usesSet = new HashSet<>();
+        boolean containsRun = false;
+        for (Map<String, Object> step : steps) {
+            if (!containsRun && step.containsKey("run")) {
+                containsRun = true;
+            }
+            if (step.containsKey("env")) {
+                Assert.isInstanceOf(Map.class, step.get("env"), "env 必须是 map 类型");
+            }
+            if (step.containsKey("uses")) {
+                List<String> supportedPlugins = ListUtil.of("node", "java", "maven", "cache", "go", "python3");
+                Assert.isInstanceOf(String.class, step.get("uses"), "uses 只支持 String 类型");
+                String uses = (String) step.get("uses");
+                Assert.isTrue(supportedPlugins.contains(uses), String.format("目前仅支持的插件: %s", supportedPlugins));
+                if ("node".equals(uses)) {
+                    nodePluginCheck(step);
+                } else if ("java".equals(uses)) {
+                    javaPluginCheck(step);
+                } else if ("maven".equals(uses)) {
+                    mavenPluginCheck(step);
+                } else if ("cache".equals(uses)) {
+                    cachePluginCheck(step);
+                } else if ("go".equals(uses)) {
+                    goPluginCheck(step);
+                } else if ("python3".equals(uses)) {
+                    python3PluginCheck(step);
+                }
+                usesSet.add(uses);
+            }
+        }
+        if (usesSet.contains("maven") && !usesSet.contains("java")) {
+            throw new IllegalArgumentException("maven 插件依赖 java , 使用 maven 插件必须优先引入 java 插件");
+        }
+        Assert.isTrue(containsRun, "steps 中没有发现任何 run , run 用于执行命令");
+    }
 
-	private void cachePluginCheck(Map<String, Object> step) {
-		Assert.notNull(step.get("path"), "cache 插件 path 不能为空");
-	}
+    private void cachePluginCheck(Map<String, Object> step) {
+        Assert.notNull(step.get("path"), "cache 插件 path 不能为空");
+    }
 
-	private void mavenPluginCheck(Map<String, Object> step) {
-		Assert.notNull(step.get("version"), "maven 插件 version 不能为空");
-		String version = String.valueOf(step.get("version"));
-		String link = String.format("https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/%s/binaries/apache-maven-%s-bin.tar.gz", version, version);
-		HttpResponse httpResponse = HttpUtil.createRequest(Method.HEAD, link).execute();
-		Assert.isTrue(httpResponse.isOk() || httpResponse.getStatus() == HttpStatus.HTTP_MOVED_TEMP, "请填入正确的 maven 版本号");
-	}
+    private void mavenPluginCheck(Map<String, Object> step) {
+        Assert.notNull(step.get("version"), "maven 插件 version 不能为空");
+        String version = String.valueOf(step.get("version"));
+        String link = String.format("https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/%s/binaries/apache-maven-%s-bin.tar.gz", version, version);
+        HttpRequest request = HttpUtil.createRequest(Method.HEAD, link);
+        HttpResponse httpResponse = request.execute();
+        Assert.isTrue(httpResponse.isOk() || httpResponse.getStatus() == HttpStatus.HTTP_MOVED_TEMP, "请填入正确的 maven 版本号");
+    }
 
-	private void javaPluginCheck(Map<String, Object> step) {
-		Assert.notNull(step.get("version"), "java 插件 version 不能为空");
-		Integer version = Integer.valueOf(String.valueOf(step.get("version")));
-		List<Integer> supportedVersions = ListUtil.of(8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
-		Assert.isTrue(supportedVersions.contains(version), String.format("目前java 插件支持的版本: %s", supportedVersions));
-	}
+    private void javaPluginCheck(Map<String, Object> step) {
+        Assert.notNull(step.get("version"), "java 插件 version 不能为空");
+        Integer version = Integer.valueOf(String.valueOf(step.get("version")));
+        List<Integer> supportedVersions = ListUtil.of(8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+        Assert.isTrue(supportedVersions.contains(version), String.format("目前java 插件支持的版本: %s", supportedVersions));
+    }
 
-	private void nodePluginCheck(Map<String, Object> step) {
-		Assert.notNull(step.get("version"), "node 插件 version 不能为空");
-		String version = String.valueOf(step.get("version"));
-		String link = String.format("https://registry.npmmirror.com/-/binary/node/v%s/node-v%s-linux-x64.tar.gz", version, version);
-		HttpResponse httpResponse = HttpUtil.createRequest(Method.HEAD, link).execute();
-		Assert.isTrue(httpResponse.isOk() || httpResponse.getStatus() == HttpStatus.HTTP_MOVED_TEMP, "请填入正确的 node 版本号");
-	}
+    private void nodePluginCheck(Map<String, Object> step) {
+        Assert.notNull(step.get("version"), "node 插件 version 不能为空");
+        String version = String.valueOf(step.get("version"));
+        String link = String.format("https://registry.npmmirror.com/-/binary/node/v%s/node-v%s-linux-x64.tar.gz", version, version);
+        HttpResponse httpResponse = HttpUtil.createRequest(Method.HEAD, link).execute();
+        Assert.isTrue(httpResponse.isOk() || httpResponse.getStatus() == HttpStatus.HTTP_MOVED_TEMP, "请填入正确的 node 版本号");
+    }
 
-	private void goPluginCheck(Map<String, Object> step) {
-		Assert.notNull(step.get("version"), "go 插件 version 不能为空");
-		String version = String.valueOf(step.get("version"));
-		String link = String.format("https://studygolang.com/dl/golang/go%s.linux-amd64.tar.gz", version);
-		HttpResponse httpResponse = HttpUtil.createRequest(Method.HEAD, link).execute();
-		Assert.isTrue(httpResponse.isOk() ||
-				httpResponse.getStatus() == HttpStatus.HTTP_MOVED_TEMP ||
-				httpResponse.getStatus() == HttpStatus.HTTP_SEE_OTHER, "请填入正确的 go 版本号");
-	}
+    private void goPluginCheck(Map<String, Object> step) {
+        Assert.notNull(step.get("version"), "go 插件 version 不能为空");
+        String version = String.valueOf(step.get("version"));
+        String link = String.format("https://studygolang.com/dl/golang/go%s.linux-amd64.tar.gz", version);
+        HttpResponse httpResponse = HttpUtil.createRequest(Method.HEAD, link).execute();
+        Assert.isTrue(httpResponse.isOk() ||
+            httpResponse.getStatus() == HttpStatus.HTTP_MOVED_TEMP ||
+            httpResponse.getStatus() == HttpStatus.HTTP_SEE_OTHER, "请填入正确的 go 版本号");
+    }
 
-	private void python3PluginCheck(Map<String, Object> step) {
-		Assert.notNull(step.get("version"), "python3 插件 version 不能为空");
-		String version = String.valueOf(step.get("version"));
-		Assert.state(StrUtil.startWith(version, "3."), "请填入正确的 python3 版本号");
-		String link = String.format("https://repo.huaweicloud.com/python/%s/Python-%s.tar.xz", version, version);
-		HttpResponse httpResponse = HttpUtil.createRequest(Method.HEAD, link).execute();
-		Assert.isTrue(httpResponse.isOk() ||
-				httpResponse.getStatus() == HttpStatus.HTTP_MOVED_TEMP, "请填入正确的 python3 版本号");
-	}
+    private void python3PluginCheck(Map<String, Object> step) {
+        Assert.notNull(step.get("version"), "python3 插件 version 不能为空");
+        String version = String.valueOf(step.get("version"));
+        Assert.state(StrUtil.startWith(version, "3."), "请填入正确的 python3 版本号");
+        String link = String.format("https://repo.huaweicloud.com/python/%s/Python-%s.tar.xz", version, version);
+        HttpResponse httpResponse = HttpUtil.createRequest(Method.HEAD, link).execute();
+        Assert.isTrue(httpResponse.isOk() ||
+            httpResponse.getStatus() == HttpStatus.HTTP_MOVED_TEMP, "请填入正确的 python3 版本号");
+    }
 
-	/**
-	 * 构建对象
-	 *
-	 * @param yml yml 内容
-	 * @return DockerYmlDsl
-	 */
-	public static DockerYmlDsl build(String yml) {
-		yml = StrUtil.replace(yml, StrUtil.TAB, StrUtil.SPACE + StrUtil.SPACE);
-		InputStream inputStream = new ByteArrayInputStream(yml.getBytes());
-		return YamlUtil.load(inputStream, DockerYmlDsl.class);
-	}
+    /**
+     * 构建对象
+     *
+     * @param yml yml 内容
+     * @return DockerYmlDsl
+     */
+    public static DockerYmlDsl build(String yml) {
+        yml = StrUtil.replace(yml, StrUtil.TAB, StrUtil.SPACE + StrUtil.SPACE);
+        InputStream inputStream = new ByteArrayInputStream(yml.getBytes());
+        return YamlUtil.load(inputStream, DockerYmlDsl.class);
+    }
 }
