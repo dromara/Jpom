52380cc9bbc830c7eb7413ec47fb2128f5cab843
bwcx_jzy <bwcx_jzy@163.com>
2022-03-31 16:22:48 +0800

    feat(add) 本地构建命令、本地命令发布、ssh 发布支持加载仓库目录 .env 文件为环境变量

--- modules/common/src/main/java/io/jpom/util/FileUtils.java
+++ modules/common/src/main/java/io/jpom/util/FileUtils.java
@@ -22,9 +22,12 @@
  */
 package io.jpom.util;
 
+import cn.hutool.core.collection.CollStreamUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.LineHandler;
+import cn.hutool.core.lang.Tuple;
 import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.system.SystemUtil;
@@ -32,6 +35,11 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * 文件工具
@@ -41,139 +49,163 @@ import java.io.File;
  */
 public class FileUtils {
 
-	private static JSONObject fileToJson(File file) {
-		JSONObject jsonObject = new JSONObject(6);
-		long sizeFile = FileUtil.size(file);
-		if (file.isDirectory()) {
-			jsonObject.put("isDirectory", true);
-			jsonObject.put("fileSize", FileUtil.readableFileSize(sizeFile));
-		} else {
-			jsonObject.put("fileSize", FileUtil.readableFileSize(sizeFile));
-		}
-		jsonObject.put("filename", file.getName());
-		long mTime = file.lastModified();
-		jsonObject.put("modifyTimeLong", mTime);
-		jsonObject.put("modifyTime", DateUtil.date(mTime).toString());
-		return jsonObject;
-	}
+    private static JSONObject fileToJson(File file) {
+        JSONObject jsonObject = new JSONObject(6);
+        long sizeFile = FileUtil.size(file);
+        if (file.isDirectory()) {
+            jsonObject.put("isDirectory", true);
+            jsonObject.put("fileSize", FileUtil.readableFileSize(sizeFile));
+        } else {
+            jsonObject.put("fileSize", FileUtil.readableFileSize(sizeFile));
+        }
+        jsonObject.put("filename", file.getName());
+        long mTime = file.lastModified();
+        jsonObject.put("modifyTimeLong", mTime);
+        jsonObject.put("modifyTime", DateUtil.date(mTime).toString());
+        return jsonObject;
+    }
 
-	/**
-	 * 对文件信息解析排序
-	 *
-	 * @param files     文件数组
-	 * @param time      是否安装时间排序
-	 * @param startPath 开始路径
-	 * @return 排序后的json
-	 */
-	public static JSONArray parseInfo(File[] files, boolean time, String startPath) {
-		if (files == null) {
-			return new JSONArray();
-		}
-		int size = files.length;
-		JSONArray arrayFile = new JSONArray(size);
-		for (File file : files) {
-			JSONObject jsonObject = FileUtils.fileToJson(file);
-			//
-			if (startPath != null) {
-				String levelName = StringUtil.delStartPath(file, startPath, false);
-				jsonObject.put("levelName", levelName);
-			}
-			//
-			arrayFile.add(jsonObject);
-		}
-		arrayFile.sort((o1, o2) -> {
-			JSONObject jsonObject1 = (JSONObject) o1;
-			JSONObject jsonObject2 = (JSONObject) o2;
-			if (time) {
-				return jsonObject2.getLong("modifyTimeLong").compareTo(jsonObject1.getLong("modifyTimeLong"));
-			}
-			return jsonObject1.getString("filename").compareTo(jsonObject2.getString("filename"));
-		});
-		final int[] i = {0};
-		arrayFile.forEach(o -> {
-			JSONObject jsonObject = (JSONObject) o;
-			jsonObject.put("index", ++i[0]);
-		});
-		return arrayFile;
-	}
+    /**
+     * 对文件信息解析排序
+     *
+     * @param files     文件数组
+     * @param time      是否安装时间排序
+     * @param startPath 开始路径
+     * @return 排序后的json
+     */
+    public static JSONArray parseInfo(File[] files, boolean time, String startPath) {
+        if (files == null) {
+            return new JSONArray();
+        }
+        int size = files.length;
+        JSONArray arrayFile = new JSONArray(size);
+        for (File file : files) {
+            JSONObject jsonObject = FileUtils.fileToJson(file);
+            //
+            if (startPath != null) {
+                String levelName = StringUtil.delStartPath(file, startPath, false);
+                jsonObject.put("levelName", levelName);
+            }
+            //
+            arrayFile.add(jsonObject);
+        }
+        arrayFile.sort((o1, o2) -> {
+            JSONObject jsonObject1 = (JSONObject) o1;
+            JSONObject jsonObject2 = (JSONObject) o2;
+            if (time) {
+                return jsonObject2.getLong("modifyTimeLong").compareTo(jsonObject1.getLong("modifyTimeLong"));
+            }
+            return jsonObject1.getString("filename").compareTo(jsonObject2.getString("filename"));
+        });
+        final int[] i = {0};
+        arrayFile.forEach(o -> {
+            JSONObject jsonObject = (JSONObject) o;
+            jsonObject.put("index", ++i[0]);
+        });
+        return arrayFile;
+    }
 
-	/**
-	 * 判断路径是否满足jdk 条件
-	 *
-	 * @param path 路径
-	 * @return 判断存在java文件
-	 */
-	public static boolean isJdkPath(String path) {
-		String fileName = getJdkJavaPath(path, false);
-		File newPath = new File(fileName);
-		return newPath.exists() && newPath.isFile();
-	}
+    /**
+     * 判断路径是否满足jdk 条件
+     *
+     * @param path 路径
+     * @return 判断存在java文件
+     */
+    public static boolean isJdkPath(String path) {
+        String fileName = getJdkJavaPath(path, false);
+        File newPath = new File(fileName);
+        return newPath.exists() && newPath.isFile();
+    }
 
-	/**
-	 * 获取java 文件路径
-	 *
-	 * @param path path
-	 * @param w    是否使用javaw
-	 * @return 完整路径
-	 */
-	public static String getJdkJavaPath(String path, boolean w) {
-		String fileName;
-		if (SystemUtil.getOsInfo().isWindows()) {
-			fileName = w ? "javaw.exe" : "java.exe";
-		} else {
-			fileName = w ? "javaw" : "java";
-		}
-		File newPath = FileUtil.file(path, "bin", fileName);
-		return FileUtil.getAbsolutePath(newPath);
-	}
+    /**
+     * 获取java 文件路径
+     *
+     * @param path path
+     * @param w    是否使用javaw
+     * @return 完整路径
+     */
+    public static String getJdkJavaPath(String path, boolean w) {
+        String fileName;
+        if (SystemUtil.getOsInfo().isWindows()) {
+            fileName = w ? "javaw.exe" : "java.exe";
+        } else {
+            fileName = w ? "javaw" : "java";
+        }
+        File newPath = FileUtil.file(path, "bin", fileName);
+        return FileUtil.getAbsolutePath(newPath);
+    }
 
-	/**
-	 * 获取jdk 版本
-	 *
-	 * @param path jdk 路径
-	 * @return 获取成功返回版本号
-	 */
-	public static String getJdkVersion(String path) {
-		String newPath = getJdkJavaPath(path, false);
-		if (path.contains(StrUtil.SPACE)) {
-			newPath = String.format("\"%s\"", newPath);
-		}
-		String command = CommandUtil.execSystemCommand(newPath + "  -version");
-		String[] split = StrUtil.splitToArray(command, StrUtil.LF);
-		if (split == null || split.length <= 0) {
-			return null;
-		}
-		String[] strings = StrUtil.splitToArray(split[0], "\"");
-		if (strings == null || strings.length <= 1) {
-			return null;
-		}
-		return strings[1];
-	}
+    /**
+     * 获取jdk 版本
+     *
+     * @param path jdk 路径
+     * @return 获取成功返回版本号
+     */
+    public static String getJdkVersion(String path) {
+        String newPath = getJdkJavaPath(path, false);
+        if (path.contains(StrUtil.SPACE)) {
+            newPath = String.format("\"%s\"", newPath);
+        }
+        String command = CommandUtil.execSystemCommand(newPath + "  -version");
+        String[] split = StrUtil.splitToArray(command, StrUtil.LF);
+        if (split == null || split.length <= 0) {
+            return null;
+        }
+        String[] strings = StrUtil.splitToArray(split[0], "\"");
+        if (strings == null || strings.length <= 1) {
+            return null;
+        }
+        return strings[1];
+    }
 
-	/**
-	 * 读取 日志文件
-	 *
-	 * @param logFile 日志文件
-	 * @param line    开始行数
-	 * @return data
-	 */
-	public static JSONObject readLogFile(File logFile, int line) {
-		JSONObject data = new JSONObject();
-		// 读取文件
-		//int linesInt = Convert.toInt(line, 1);
-		LimitQueue<String> lines = new LimitQueue<>(1000);
-		final int[] readCount = {0};
-		FileUtil.readLines(logFile, CharsetUtil.CHARSET_UTF_8, (LineHandler) line1 -> {
-			readCount[0]++;
-			if (readCount[0] < line) {
-				return;
-			}
-			lines.add(line1);
-		});
-		// 下次应该获取的行数
-		data.put("line", readCount[0] + 1);
-		data.put("getLine", line);
-		data.put("dataLines", lines);
-		return data;
-	}
+    /**
+     * 读取 日志文件
+     *
+     * @param logFile 日志文件
+     * @param line    开始行数
+     * @return data
+     */
+    public static JSONObject readLogFile(File logFile, int line) {
+        JSONObject data = new JSONObject();
+        // 读取文件
+        //int linesInt = Convert.toInt(line, 1);
+        LimitQueue<String> lines = new LimitQueue<>(1000);
+        final int[] readCount = {0};
+        FileUtil.readLines(logFile, CharsetUtil.CHARSET_UTF_8, (LineHandler) line1 -> {
+            readCount[0]++;
+            if (readCount[0] < line) {
+                return;
+            }
+            lines.add(line1);
+        });
+        // 下次应该获取的行数
+        data.put("line", readCount[0] + 1);
+        data.put("getLine", line);
+        data.put("dataLines", lines);
+        return data;
+    }
+
+    /**
+     * 读取环境变量文件
+     *
+     * @param envFile 文件
+     * @return map
+     */
+    public static Map<String, String> readEnvFile(File envFile) {
+        if (FileUtil.isFile(envFile)) {
+            List<String> list = FileUtil.readLines(envFile, CharsetUtil.CHARSET_UTF_8);
+            List<Tuple> collect = list.stream()
+                .map(StrUtil::trim)
+                .filter(s -> !StrUtil.isEmpty(s) && !StrUtil.startWith(s, "#"))
+                .map(s -> {
+                    List<String> list1 = StrUtil.splitTrim(s, "=");
+                    if (CollUtil.size(list1) != 2) {
+                        return null;
+                    }
+                    return new Tuple(list1.get(0), list1.get(1));
+                }).filter(Objects::nonNull).collect(Collectors.toList());
+            return CollStreamUtil.toMap(collect, objects -> objects.get(0), objects -> objects.get(1));
+        }
+        return new HashMap<>();
+    }
 }
--- modules/common/src/main/java/io/jpom/util/StringUtil.java
+++ modules/common/src/main/java/io/jpom/util/StringUtil.java
@@ -33,6 +33,8 @@ import com.alibaba.fastjson.JSON;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * main 方法运行参数工具
@@ -42,99 +44,99 @@ import java.util.List;
  */
 public class StringUtil {
 
-	/**
-	 * 支持的压缩包格式
-	 */
-	public static final String[] PACKAGE_EXT = new String[]{"tar.bz2", "tar.gz", "tar", "bz2", "zip", "gz"};
+    /**
+     * 支持的压缩包格式
+     */
+    public static final String[] PACKAGE_EXT = new String[]{"tar.bz2", "tar.gz", "tar", "bz2", "zip", "gz"};
 
-	/**
-	 * 获取启动参数
-	 *
-	 * @param args 所有参数
-	 * @param name 参数名
-	 * @return 值
-	 */
-	public static String getArgsValue(String[] args, String name) {
-		if (args == null) {
-			return null;
-		}
-		for (String item : args) {
-			item = StrUtil.trim(item);
-			if (item.startsWith("--" + name + "=")) {
-				return item.substring(name.length() + 3);
-			}
-		}
-		return null;
-	}
+    /**
+     * 获取启动参数
+     *
+     * @param args 所有参数
+     * @param name 参数名
+     * @return 值
+     */
+    public static String getArgsValue(String[] args, String name) {
+        if (args == null) {
+            return null;
+        }
+        for (String item : args) {
+            item = StrUtil.trim(item);
+            if (item.startsWith("--" + name + "=")) {
+                return item.substring(name.length() + 3);
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * 转换 文件内容
-	 *
-	 * @param text 字符串，可能为文件协议地址
-	 * @param def  默认值
-	 * @return 如果存在文件 则读取文件内容
-	 */
-	public static String convertFileStr(String text, String def) {
-		if (StrUtil.startWith(text, URLUtil.FILE_URL_PREFIX)) {
-			String path = StrUtil.removePrefix(text, URLUtil.FILE_URL_PREFIX);
-			if (FileUtil.isFile(path)) {
-				String fileText = FileUtil.readUtf8String(path);
-				return StrUtil.emptyToDefault(fileText, def);
-			}
-		}
-		return StrUtil.emptyToDefault(text, def);
-	}
+    /**
+     * 转换 文件内容
+     *
+     * @param text 字符串，可能为文件协议地址
+     * @param def  默认值
+     * @return 如果存在文件 则读取文件内容
+     */
+    public static String convertFileStr(String text, String def) {
+        if (StrUtil.startWith(text, URLUtil.FILE_URL_PREFIX)) {
+            String path = StrUtil.removePrefix(text, URLUtil.FILE_URL_PREFIX);
+            if (FileUtil.isFile(path)) {
+                String fileText = FileUtil.readUtf8String(path);
+                return StrUtil.emptyToDefault(fileText, def);
+            }
+        }
+        return StrUtil.emptyToDefault(text, def);
+    }
 
-	/**
-	 * 删除文件开始的路径
-	 *
-	 * @param file      要删除的文件
-	 * @param startPath 开始的路径
-	 * @param inName    是否返回文件名
-	 * @return /test/a.txt /test/  a.txt
-	 */
-	public static String delStartPath(File file, String startPath, boolean inName) {
-		String newWhitePath;
-		if (inName) {
-			newWhitePath = FileUtil.getAbsolutePath(file.getAbsolutePath());
-		} else {
-			newWhitePath = FileUtil.getAbsolutePath(file.getParentFile());
-		}
-		String itemAbsPath = FileUtil.getAbsolutePath(startPath);
-		itemAbsPath = FileUtil.normalize(itemAbsPath);
-		newWhitePath = FileUtil.normalize(newWhitePath);
-		String path = StrUtil.removePrefix(newWhitePath, itemAbsPath);
-		//newWhitePath.substring(newWhitePath.indexOf(itemAbsPath) + itemAbsPath.length());
-		path = FileUtil.normalize(path);
-		if (path.startsWith(StrUtil.SLASH)) {
-			path = path.substring(1);
-		}
-		return path;
-	}
+    /**
+     * 删除文件开始的路径
+     *
+     * @param file      要删除的文件
+     * @param startPath 开始的路径
+     * @param inName    是否返回文件名
+     * @return /test/a.txt /test/  a.txt
+     */
+    public static String delStartPath(File file, String startPath, boolean inName) {
+        String newWhitePath;
+        if (inName) {
+            newWhitePath = FileUtil.getAbsolutePath(file.getAbsolutePath());
+        } else {
+            newWhitePath = FileUtil.getAbsolutePath(file.getParentFile());
+        }
+        String itemAbsPath = FileUtil.getAbsolutePath(startPath);
+        itemAbsPath = FileUtil.normalize(itemAbsPath);
+        newWhitePath = FileUtil.normalize(newWhitePath);
+        String path = StrUtil.removePrefix(newWhitePath, itemAbsPath);
+        //newWhitePath.substring(newWhitePath.indexOf(itemAbsPath) + itemAbsPath.length());
+        path = FileUtil.normalize(path);
+        if (path.startsWith(StrUtil.SLASH)) {
+            path = path.substring(1);
+        }
+        return path;
+    }
 
-	/**
-	 * 获取jdk 中的tools jar文件路径
-	 *
-	 * @return file
-	 */
-	public static File getToolsJar() {
-		File file = new File(SystemUtil.getJavaRuntimeInfo().getHomeDir());
-		return new File(file.getParentFile(), "lib/tools.jar");
-	}
+    /**
+     * 获取jdk 中的tools jar文件路径
+     *
+     * @return file
+     */
+    public static File getToolsJar() {
+        File file = new File(SystemUtil.getJavaRuntimeInfo().getHomeDir());
+        return new File(file.getParentFile(), "lib/tools.jar");
+    }
 
-	/**
-	 * 指定时间的下一个刻度
-	 *
-	 * @return String
-	 */
-	public static String getNextScaleTime(String time, Long millis) {
-		DateTime dateTime = DateUtil.parse(time);
-		if (millis == null) {
-			millis = 30 * 1000L;
-		}
-		DateTime newTime = dateTime.offsetNew(DateField.SECOND, (int) (millis / 1000));
-		return DateUtil.formatTime(newTime);
-	}
+    /**
+     * 指定时间的下一个刻度
+     *
+     * @return String
+     */
+    public static String getNextScaleTime(String time, Long millis) {
+        DateTime dateTime = DateUtil.parse(time);
+        if (millis == null) {
+            millis = 30 * 1000L;
+        }
+        DateTime newTime = dateTime.offsetNew(DateField.SECOND, (int) (millis / 1000));
+        return DateUtil.formatTime(newTime);
+    }
 
 //	/**
 //	 * 删除 yml 文件内容注释
@@ -154,42 +156,59 @@ public class StringUtil {
 //		return CollUtil.join(split, StrUtil.LF);
 //	}
 
-	/**
-	 * json 字符串转 bean，兼容普通json和字符串包裹情况
-	 *
-	 * @param jsonStr json 字符串
-	 * @param cls     要转为bean的类
-	 * @param <T>     泛型
-	 * @return data
-	 */
-	public static <T> T jsonConvert(String jsonStr, Class<T> cls) {
-		if (StrUtil.isEmpty(jsonStr)) {
-			return null;
-		}
-		try {
-			return JSON.parseObject(jsonStr, cls);
-		} catch (Exception e) {
-			return JSON.parseObject(JSON.parse(jsonStr).toString(), cls);
-		}
-	}
+    /**
+     * json 字符串转 bean，兼容普通json和字符串包裹情况
+     *
+     * @param jsonStr json 字符串
+     * @param cls     要转为bean的类
+     * @param <T>     泛型
+     * @return data
+     */
+    public static <T> T jsonConvert(String jsonStr, Class<T> cls) {
+        if (StrUtil.isEmpty(jsonStr)) {
+            return null;
+        }
+        try {
+            return JSON.parseObject(jsonStr, cls);
+        } catch (Exception e) {
+            return JSON.parseObject(JSON.parse(jsonStr).toString(), cls);
+        }
+    }
 
-	/**
-	 * json 字符串转 bean，兼容普通json和字符串包裹情况
-	 *
-	 * @param jsonStr json 字符串
-	 * @param cls     要转为bean的类
-	 * @param <T>     泛型
-	 * @return data
-	 */
-	public static <T> List<T> jsonConvertArray(String jsonStr, Class<T> cls) {
-		try {
-			if (StrUtil.isEmpty(jsonStr)) {
-				return null;
-			}
-			return JSON.parseArray(jsonStr, cls);
-		} catch (Exception e) {
-			Object parse = JSON.parse(jsonStr);
-			return JSON.parseArray(parse.toString(), cls);
-		}
-	}
+    /**
+     * json 字符串转 bean，兼容普通json和字符串包裹情况
+     *
+     * @param jsonStr json 字符串
+     * @param cls     要转为bean的类
+     * @param <T>     泛型
+     * @return data
+     */
+    public static <T> List<T> jsonConvertArray(String jsonStr, Class<T> cls) {
+        try {
+            if (StrUtil.isEmpty(jsonStr)) {
+                return null;
+            }
+            return JSON.parseArray(jsonStr, cls);
+        } catch (Exception e) {
+            Object parse = JSON.parse(jsonStr);
+            return JSON.parseArray(parse.toString(), cls);
+        }
+    }
+
+    /**
+     * 根据 map 替换 字符串变量
+     *
+     * @param command 字符串
+     * @param evn     map
+     * @return 替换后
+     */
+    public static String formatStrByMap(String command, Map<String, String> evn) {
+        String replace = command;
+        Set<Map.Entry<String, String>> entries = evn.entrySet();
+        for (Map.Entry<String, String> entry : entries) {
+            replace = StrUtil.replace(replace, StrUtil.format("#{{}}", entry.getKey()), entry.getValue());
+            replace = StrUtil.replace(replace, StrUtil.format("${{}}", entry.getKey()), entry.getValue());
+        }
+        return replace;
+    }
 }
