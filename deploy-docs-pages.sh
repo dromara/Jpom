0bacb8a55287166a8710f186e984989dd6e60821
bwcx_jzy <bwcx_jzy@163.com>
2022-06-11 22:10:00 +0800

    update docs-pages

---
+++ deploy-docs-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR_PATH=$(cd `dirname $0`; pwd)"/"
+
+echo ${DIR_PATH}
+
+# 打包
+cd ${DIR_PATH} && cd docs && yarn run build
+
+# 删除文件
+cd ${DIR_PATH} &&  cd docs-pages && ls -lt | awk '{ print $9 }' | xargs rm -rf
+
+cd ${DIR_PATH} && cp -r ./docs/dist/* ./docs-pages
+
