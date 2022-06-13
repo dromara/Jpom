7cb57d67e10021f6ef19bef1e5349818990b9c53
bwcx_jzy <bwcx_jzy@163.com>
2022-06-13 14:07:21 +0800

    commit 2.9.0

--- script/replaceVersion.sh
+++ script/replaceVersion.sh
@@ -34,7 +34,7 @@ echo "当前路径：${pwd}"
 
 if [ -n "$1" ];then
     new_version="$1"
-    old_version=`cat ${pwd}/../docs/versions.tag`
+    old_version=`cat ${pwd}/../docs/.vuepress/public/docs/versions.tag`
     echo "$old_version 替换为新版本 $new_version"
 else
     # 参数错误，退出
@@ -43,16 +43,15 @@ else
 fi
 
 if [ ! -n "$old_version" ]; then
-    echo "ERROR: 旧版本不存在，请确认 /docs/versions.tag 中信息正确"
+    echo "ERROR: 旧版本不存在，请确认 /docs/.vuepress/public/docs/versions.tag 中信息正确"
     exit
 fi
 
-# 替换 jpom.io 主页版本号
-sed -i.bak "s/${old_version}/${new_version}/g" $pwd/../docs/js/version.js
+
 # 替换远程更新包的版本号
-sed -i.bak "s/${old_version}/${new_version}/g" $pwd/../docs/versions.json
-sed -i.bak "s/${old_version}/${new_version}/g" $pwd/../docs/release-versions.json
+sed -i.bak "s/${old_version}/${new_version}/g" $pwd/../docs/.vuepress/public/docs/versions.json
+sed -i.bak "s/${old_version}/${new_version}/g" $pwd/../docs/.vuepress/public/docs/release-versions.json
 
 
 # 保留新版本号
-echo "$new_version" > $pwd/../docs/versions.tag
+echo "$new_version" > $pwd/../docs/.vuepress/public/docs/versions.tag
