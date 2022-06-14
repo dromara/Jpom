c2c8e6e81a7a7cc1f449988f96e1b79586f4246e
bwcx_jzy <bwcx_jzy@163.com>
2022-06-15 00:07:51 +0800

    update docs-pages

--- deploy-docs-pages.sh
+++ deploy-docs-pages.sh
@@ -4,6 +4,9 @@ DIR_PATH=$(cd `dirname $0`; pwd)"/"
 
 echo ${DIR_PATH}
 
+# 提交代码 docs
+cd ${DIR_PATH} && cd docs && git add . &&  git commit -m 'update docs pages' && git push
+
 # 打包
 cd ${DIR_PATH} && cd docs && yarn run build
 
@@ -13,7 +16,7 @@ cd ${DIR_PATH} && cd docs-pages && ls -lt | awk '{ print $9 }' | xargs rm -rf
 # 复制文件
 cd ${DIR_PATH} && cp -r ./docs/dist/* ./docs-pages
 
-# 提交代码
+# 提交代码 pages
 cd ${DIR_PATH} && cd docs-pages && git add . &&  git commit -m 'update docs pages' && git push
 
 
