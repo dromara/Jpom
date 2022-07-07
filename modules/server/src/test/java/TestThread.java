2234e6f403178d72e84d0289d371419567f484e9
bwcx_jzy <bwcx_jzy@163.com>
2022-07-07 17:20:00 +0800

    fix

--- modules/server/src/test/java/TestThread.java
+++ modules/server/src/test/java/TestThread.java
@@ -20,7 +20,6 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 import cn.hutool.core.thread.ExecutorBuilder;
 import cn.hutool.core.thread.ThreadUtil;
 import org.junit.jupiter.api.Test;
