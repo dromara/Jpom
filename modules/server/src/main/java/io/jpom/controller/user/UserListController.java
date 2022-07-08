29f6d9f427f0c35a11925eed070d7747a295ae0f
bwcx_jzy <bwcx_jzy@163.com>
2022-07-08 11:01:12 +0800

    fix demo user

--- modules/server/src/main/java/io/jpom/controller/user/UserListController.java
+++ modules/server/src/main/java/io/jpom/controller/user/UserListController.java
@@ -56,180 +56,180 @@ import java.util.List;
 @SystemPermission
 public class UserListController extends BaseServerController {
 
-	private final UserService userService;
-	private final UserBindWorkspaceService userBindWorkspaceService;
+    private final UserService userService;
+    private final UserBindWorkspaceService userBindWorkspaceService;
 
-	public UserListController(UserService userService,
-							  UserBindWorkspaceService userBindWorkspaceService) {
-		this.userService = userService;
-		this.userBindWorkspaceService = userBindWorkspaceService;
-	}
+    public UserListController(UserService userService,
+                              UserBindWorkspaceService userBindWorkspaceService) {
+        this.userService = userService;
+        this.userBindWorkspaceService = userBindWorkspaceService;
+    }
 
-	/**
-	 * 查询所有用户
-	 *
-	 * @return json
-	 */
-	@RequestMapping(value = "get_user_list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	@Feature(method = MethodFeature.LIST)
-	public String getUserList() {
-		PageResultDto<UserModel> userModelPageResultDto = userService.listPage(getRequest());
-		userModelPageResultDto.each(userModel -> {
-			boolean bindMfa = userService.hasBindMfa(userModel.getId());
-			if (bindMfa) {
-				userModel.setTwoFactorAuthKey("true");
-			}
-		});
-		return JsonMessage.getString(200, "", userModelPageResultDto);
-	}
+    /**
+     * 查询所有用户
+     *
+     * @return json
+     */
+    @RequestMapping(value = "get_user_list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Feature(method = MethodFeature.LIST)
+    public String getUserList() {
+        PageResultDto<UserModel> userModelPageResultDto = userService.listPage(getRequest());
+        userModelPageResultDto.each(userModel -> {
+            boolean bindMfa = userService.hasBindMfa(userModel.getId());
+            if (bindMfa) {
+                userModel.setTwoFactorAuthKey("true");
+            }
+        });
+        return JsonMessage.getString(200, "", userModelPageResultDto);
+    }
 
-	/**
-	 * 获取所有管理员信息
-	 * get all admin user list
-	 *
-	 * @return json
-	 * @author Hotstrip
-	 */
-	@RequestMapping(value = "get_user_list_all", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	@Feature(method = MethodFeature.LIST)
-	public String getUserListAll() {
-		List<UserModel> list = userService.list();
-		return JsonMessage.getString(200, "success", list);
-	}
+    /**
+     * 获取所有管理员信息
+     * get all admin user list
+     *
+     * @return json
+     * @author Hotstrip
+     */
+    @RequestMapping(value = "get_user_list_all", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Feature(method = MethodFeature.LIST)
+    public String getUserListAll() {
+        List<UserModel> list = userService.list();
+        return JsonMessage.getString(200, "success", list);
+    }
 
-	/**
-	 * 编辑用户
-	 *
-	 * @param type 操作类型
-	 * @return String
-	 */
-	@PostMapping(value = "edit", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Feature(method = MethodFeature.EDIT)
-	public String addUser(String type) {
-		//
-		boolean create = StrUtil.equals(type, "add");
-		UserModel userModel = this.parseUser(create);
+    /**
+     * 编辑用户
+     *
+     * @param type 操作类型
+     * @return String
+     */
+    @PostMapping(value = "edit", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Feature(method = MethodFeature.EDIT)
+    public String addUser(String type) {
+        //
+        boolean create = StrUtil.equals(type, "add");
+        UserModel userModel = this.parseUser(create);
 
-		if (create) {
-			userService.insert(userModel);
-		} else {
-			UserModel model = userService.getByKey(userModel.getId());
-			Assert.notNull(model, "不存在对应的用户");
-			boolean systemUser = userModel.isSystemUser();
-			if (!systemUser) {
-				Assert.state(!model.isSuperSystemUser(), "不能取消超级管理员的权限");
-			}
-			UserModel optUser = getUser();
-			if (StrUtil.equals(model.getId(), optUser.getId())) {
-				Assert.state(optUser.isSuperSystemUser(), "不能修改自己的信息");
-			}
-			userService.update(userModel);
-		}
-		//
-		String workspace = getParameter("workspace");
-		JSONArray jsonArray = JSONArray.parseArray(workspace);
-		List<String> workspaceList = jsonArray.toJavaList(String.class);
-		userBindWorkspaceService.updateUserWorkspace(userModel.getId(), workspaceList);
-		return JsonMessage.getString(200, "操作成功");
-	}
+        if (create) {
+            userService.insert(userModel);
+        } else {
+            UserModel model = userService.getByKey(userModel.getId());
+            Assert.notNull(model, "不存在对应的用户");
+            boolean systemUser = userModel.isSystemUser();
+            if (!systemUser) {
+                Assert.state(!model.isSuperSystemUser(), "不能取消超级管理员的权限");
+            }
+            UserModel optUser = getUser();
+            if (StrUtil.equals(model.getId(), optUser.getId())) {
+                Assert.state(optUser.isSuperSystemUser(), "不能修改自己的信息");
+            }
+            userService.update(userModel);
+        }
+        //
+        String workspace = getParameter("workspace");
+        JSONArray jsonArray = JSONArray.parseArray(workspace);
+        List<String> workspaceList = jsonArray.toJavaList(String.class);
+        userBindWorkspaceService.updateUserWorkspace(userModel.getId(), workspaceList);
+        return JsonMessage.getString(200, "操作成功");
+    }
 
-	private UserModel parseUser(boolean create) {
-		String id = getParameter("id");
-		boolean email = Validator.isEmail(id);
-		if (email) {
-			int length = id.length();
-			Assert.state(length <= Const.ID_MAX_LEN && length >= UserModel.USER_NAME_MIN_LEN, "登录名如果为邮箱格式,长度必须" + UserModel.USER_NAME_MIN_LEN + "-" + Const.ID_MAX_LEN);
-		} else {
-			Validator.validateGeneral(id, UserModel.USER_NAME_MIN_LEN, Const.ID_MAX_LEN, "登录名不能为空,并且长度必须" + UserModel.USER_NAME_MIN_LEN + "-" + Const.ID_MAX_LEN);
-		}
+    private UserModel parseUser(boolean create) {
+        String id = getParameter("id");
+        boolean email = Validator.isEmail(id);
+        if (email) {
+            int length = id.length();
+            Assert.state(length <= Const.ID_MAX_LEN && length >= UserModel.USER_NAME_MIN_LEN, "登录名如果为邮箱格式,长度必须" + UserModel.USER_NAME_MIN_LEN + "-" + Const.ID_MAX_LEN);
+        } else {
+            Validator.validateGeneral(id, UserModel.USER_NAME_MIN_LEN, Const.ID_MAX_LEN, "登录名不能为空,并且长度必须" + UserModel.USER_NAME_MIN_LEN + "-" + Const.ID_MAX_LEN);
+        }
 
-		Assert.state(!StrUtil.equalsAnyIgnoreCase(id, UserModel.SYSTEM_OCCUPY_NAME, UserModel.SYSTEM_ADMIN), "当前登录名已经被系统占用");
+        Assert.state(!StrUtil.equalsAnyIgnoreCase(id, UserModel.SYSTEM_OCCUPY_NAME, UserModel.SYSTEM_ADMIN), "当前登录名已经被系统占用");
 
-		UserModel userModel = new UserModel();
-		UserModel optUser = getUser();
-		if (create) {
-			long size = userService.count();
-			Assert.state(size <= ServerExtConfigBean.getInstance().userMaxCount, "当前用户个数超过系统上限");
-			// 登录名重复
-			boolean exists = userService.exists(new UserModel(id));
-			Assert.state(!exists, "登录名已经存在");
-			userModel.setParent(optUser.getId());
-		}
-		userModel.setId(id);
-		//
-		String name = getParameter("name");
-		Assert.hasText(name, "请输入账户昵称");
-		int len = name.length();
-		Assert.state(len <= 10 && len >= 2, "昵称长度只能是2-10");
+        UserModel userModel = new UserModel();
+        UserModel optUser = getUser();
+        if (create) {
+            long size = userService.count();
+            Assert.state(size <= ServerExtConfigBean.getInstance().userMaxCount, "当前用户个数超过系统上限");
+            // 登录名重复
+            boolean exists = userService.exists(new UserModel(id));
+            Assert.state(!exists, "登录名已经存在");
+            userModel.setParent(optUser.getId());
+        }
+        userModel.setId(id);
+        //
+        String name = getParameter("name");
+        Assert.hasText(name, "请输入账户昵称");
+        int len = name.length();
+        Assert.state(len <= 10 && len >= 2, "昵称长度只能是2-10");
 
-		userModel.setName(name);
+        userModel.setName(name);
 
-		String password = getParameter("password");
-		if (create || StrUtil.isNotEmpty(password)) {
-			Assert.hasText(password, "密码不能为空");
-			// 修改用户
-			Assert.state(create || optUser.isSystemUser(), "只有系统管理员才能重置用户密码");
-			userModel.setSalt(userService.generateSalt());
-			userModel.setPassword(SecureUtil.sha1(password + userModel.getSalt()));
-		}
-		int systemUser = getParameterInt("systemUser", 0);
-		userModel.setSystemUser(systemUser);
-		return userModel;
-	}
+        String password = getParameter("password");
+        if (create || StrUtil.isNotEmpty(password)) {
+            Assert.hasText(password, "密码不能为空");
+            // 修改用户
+            Assert.state(create || optUser.isSystemUser(), "只有系统管理员才能重置用户密码");
+            userModel.setSalt(userService.generateSalt());
+            userModel.setPassword(SecureUtil.sha1(password + userModel.getSalt()));
+        }
+        int systemUser = getParameterInt("systemUser", 0);
+        userModel.setSystemUser(systemUser);
+        return userModel;
+    }
 
-	/**
-	 * 删除用户
-	 *
-	 * @param id 用户id
-	 * @return String
-	 */
-	@RequestMapping(value = "deleteUser", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	@Feature(method = MethodFeature.DEL)
-	public String deleteUser(String id) {
-		UserModel userName = getUser();
-		Assert.state(!StrUtil.equals(userName.getId(), id), "不能删除自己");
+    /**
+     * 删除用户
+     *
+     * @param id 用户id
+     * @return String
+     */
+    @RequestMapping(value = "deleteUser", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Feature(method = MethodFeature.DEL)
+    public String deleteUser(String id) {
+        UserModel userName = getUser();
+        Assert.state(!StrUtil.equals(userName.getId(), id), "不能删除自己");
 
-		UserModel userModel = userService.getByKey(id);
-		Assert.notNull(userModel, "非法访问");
-		if (userModel.isSystemUser()) {
-			// 如果是系统管理员，判断个数
-			Assert.state(userService.systemUserCount() > 1, "系统中的系统管理员账号数量必须存在一个以上");
-		}
-		// 非系统管理员不支持删除演示账号
-		Assert.state(!userModel.isDemoUser(), "演示账号不支持删除");
-		userService.delByKey(id);
-		// 删除工作空间
-		userBindWorkspaceService.deleteByUserId(id);
-		return JsonMessage.getString(200, "删除成功");
-	}
+        UserModel userModel = userService.getByKey(id);
+        Assert.notNull(userModel, "非法访问");
+        if (userModel.isSystemUser()) {
+            // 如果是系统管理员，判断个数
+            Assert.state(userService.systemUserCount() > 1, "系统中的系统管理员账号数量必须存在一个以上");
+        }
+        // 非系统管理员不支持删除演示账号
+        Assert.state(!userModel.isRealDemoUser(), "演示账号不支持删除");
+        userService.delByKey(id);
+        // 删除工作空间
+        userBindWorkspaceService.deleteByUserId(id);
+        return JsonMessage.getString(200, "删除成功");
+    }
 
-	/**
-	 * 解锁用户锁定状态
-	 *
-	 * @param id id
-	 * @return json
-	 */
-	@GetMapping(value = "unlock", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Feature(method = MethodFeature.EDIT)
-	public String unlock(@ValidatorItem String id) {
-		UserModel update = UserModel.unLock(id);
-		userService.update(update);
-		return JsonMessage.getString(200, "解锁成功");
-	}
+    /**
+     * 解锁用户锁定状态
+     *
+     * @param id id
+     * @return json
+     */
+    @GetMapping(value = "unlock", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Feature(method = MethodFeature.EDIT)
+    public String unlock(@ValidatorItem String id) {
+        UserModel update = UserModel.unLock(id);
+        userService.update(update);
+        return JsonMessage.getString(200, "解锁成功");
+    }
 
-	/**
-	 * 关闭用户 mfa
-	 *
-	 * @param id id
-	 * @return json
-	 */
-	@GetMapping(value = "close_user_mfa", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Feature(method = MethodFeature.EDIT)
-	@SystemPermission(superUser = true)
-	public String closeMfa(@ValidatorItem String id) {
-		UserModel update = new UserModel(id);
-		update.setTwoFactorAuthKey(StrUtil.EMPTY);
-		userService.update(update);
-		return JsonMessage.getString(200, "关闭成功");
-	}
+    /**
+     * 关闭用户 mfa
+     *
+     * @param id id
+     * @return json
+     */
+    @GetMapping(value = "close_user_mfa", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Feature(method = MethodFeature.EDIT)
+    @SystemPermission(superUser = true)
+    public String closeMfa(@ValidatorItem String id) {
+        UserModel update = new UserModel(id);
+        update.setTwoFactorAuthKey(StrUtil.EMPTY);
+        userService.update(update);
+        return JsonMessage.getString(200, "关闭成功");
+    }
 }
--- modules/server/src/main/java/io/jpom/model/data/UserModel.java
+++ modules/server/src/main/java/io/jpom/model/data/UserModel.java
@@ -43,89 +43,89 @@ import java.util.concurrent.TimeUnit;
 @TableName(value = "USER_INFO", name = "用户账号")
 @Data
 public class UserModel extends BaseStrikeDbModel {
-	/**
-	 * 系统管理员
-	 */
-	public final static String SYSTEM_ADMIN = "sys";
-	/**
-	 * demo 演示账号、系统预设
-	 */
-	public final static String DEMO_USER = "demo";
-	/**
-	 *
-	 */
-	public static final UserModel EMPTY = new UserModel(UserModel.SYSTEM_ADMIN);
-	/**
-	 * 系统占用名
-	 */
-	public static final String SYSTEM_OCCUPY_NAME = "系统管理员";
-	/**
-	 * 用户名限制
-	 */
-	public static final int USER_NAME_MIN_LEN = 3;
-	/**
-	 * 盐值长度
-	 */
-	public static int SALT_LEN = 8;
-	/**
-	 * 昵称
-	 */
-	private String name;
-	/**
-	 * 密码
-	 */
-	private String password;
-	/**
-	 * 密码盐值
-	 */
-	private String salt;
-	/**
-	 * 两步验证 key
-	 */
-	private String twoFactorAuthKey;
-	/**
-	 * 创建此用户的人
-	 */
-	private String parent;
-	/**
-	 * 系统管理员
-	 */
-	private Integer systemUser;
-	/**
-	 * 连续登录失败次数
-	 */
-	private Integer pwdErrorCount;
-	/**
-	 * 最后失败时间
-	 */
-	private Long lastPwdErrorTime;
-	/**
-	 * 账号被锁定的时长
-	 */
-	private Long lockTime;
-	/**
-	 * 邮箱
-	 */
-	private String email;
-	/**
-	 * 钉钉
-	 */
-	private String dingDing;
-	/**
-	 * 企业微信
-	 */
-	private String workWx;
+    /**
+     * 系统管理员
+     */
+    public final static String SYSTEM_ADMIN = "sys";
+    /**
+     * demo 演示账号、系统预设
+     */
+    public final static String DEMO_USER = "demo";
+    /**
+     *
+     */
+    public static final UserModel EMPTY = new UserModel(UserModel.SYSTEM_ADMIN);
+    /**
+     * 系统占用名
+     */
+    public static final String SYSTEM_OCCUPY_NAME = "系统管理员";
+    /**
+     * 用户名限制
+     */
+    public static final int USER_NAME_MIN_LEN = 3;
+    /**
+     * 盐值长度
+     */
+    public static int SALT_LEN = 8;
+    /**
+     * 昵称
+     */
+    private String name;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 密码盐值
+     */
+    private String salt;
+    /**
+     * 两步验证 key
+     */
+    private String twoFactorAuthKey;
+    /**
+     * 创建此用户的人
+     */
+    private String parent;
+    /**
+     * 系统管理员
+     */
+    private Integer systemUser;
+    /**
+     * 连续登录失败次数
+     */
+    private Integer pwdErrorCount;
+    /**
+     * 最后失败时间
+     */
+    private Long lastPwdErrorTime;
+    /**
+     * 账号被锁定的时长
+     */
+    private Long lockTime;
+    /**
+     * 邮箱
+     */
+    private String email;
+    /**
+     * 钉钉
+     */
+    private String dingDing;
+    /**
+     * 企业微信
+     */
+    private String workWx;
 
-	public UserModel(String id) {
-		this.setId(id);
-	}
+    public UserModel(String id) {
+        this.setId(id);
+    }
 
-	public UserModel() {
-	}
+    public UserModel() {
+    }
 
-	public void setSystemUser(Integer systemUser) {
-		this.systemUser = ObjectUtil.defaultIfNull(systemUser, 0) == 1 ? systemUser : 0;
-	}
+    public void setSystemUser(Integer systemUser) {
+        this.systemUser = ObjectUtil.defaultIfNull(systemUser, 0) == 1 ? systemUser : 0;
+    }
 
 //	/**
 //	 * 解锁
@@ -135,109 +135,118 @@ public class UserModel extends BaseStrikeDbModel {
 //		return UserModel.unLock(newModel);
 //	}
 
-	/**
-	 * 解锁
-	 */
-	public static UserModel unLock(String id) {
-		UserModel newModel = new UserModel(id);
-		newModel.setPwdErrorCount(0);
-		newModel.setLockTime(0L);
-		newModel.setLastPwdErrorTime(0L);
-		return newModel;
-	}
+    /**
+     * 解锁
+     */
+    public static UserModel unLock(String id) {
+        UserModel newModel = new UserModel(id);
+        newModel.setPwdErrorCount(0);
+        newModel.setLockTime(0L);
+        newModel.setLastPwdErrorTime(0L);
+        return newModel;
+    }
 
-	/**
-	 * 剩余解锁时间
-	 *
-	 * @return 0是未锁定
-	 */
-	public long overLockTime() {
-		if (ServerExtConfigBean.getInstance().userAlwaysLoginError <= 0) {
-			return 0;
-		}
-		// 不限制演示账号的登录
-		if (isDemoUser()) {
-			return 0;
-		}
-		// 最后一次失败时间
-		Long lastTime = getLastPwdErrorTime();
-		if (lastTime == null || lastTime <= 0) {
-			return 0;
-		}
-		// 当前锁定时间
-		Long lockTime = getLockTime();
-		if (lockTime == null || lockTime <= 0) {
-			return 0;
-		}
-		// 解锁时间
-		lastTime += lockTime;
-		long nowTime = DateUtil.currentSeconds();
-		// 剩余解锁时间
-		lastTime -= nowTime;
-		if (lastTime > 0) {
-			return lastTime;
-		}
-		return 0;
-	}
+    /**
+     * 剩余解锁时间
+     *
+     * @return 0是未锁定
+     */
+    public long overLockTime() {
+        if (ServerExtConfigBean.getInstance().userAlwaysLoginError <= 0) {
+            return 0;
+        }
+        // 不限制演示账号的登录
+        if (isDemoUser()) {
+            return 0;
+        }
+        // 最后一次失败时间
+        Long lastTime = getLastPwdErrorTime();
+        if (lastTime == null || lastTime <= 0) {
+            return 0;
+        }
+        // 当前锁定时间
+        Long lockTime = getLockTime();
+        if (lockTime == null || lockTime <= 0) {
+            return 0;
+        }
+        // 解锁时间
+        lastTime += lockTime;
+        long nowTime = DateUtil.currentSeconds();
+        // 剩余解锁时间
+        lastTime -= nowTime;
+        if (lastTime > 0) {
+            return lastTime;
+        }
+        return 0;
+    }
 
-	/**
-	 * 登录失败，重新计算锁定时间
-	 */
-	public UserModel errorLock() {
-		// 未开启锁定功能
-		int userAlwaysLoginError = ServerExtConfigBean.getInstance().userAlwaysLoginError;
-		if (userAlwaysLoginError <= 0) {
-			return null;
-		}
-		UserModel newModel = new UserModel(this.getId());
-		newModel.setPwdErrorCount(ObjectUtil.defaultIfNull(this.getPwdErrorCount(), 0) + 1);
-		int count = newModel.getPwdErrorCount();
-		// 记录错误时间
-		newModel.setLastPwdErrorTime(DateUtil.currentSeconds());
-		if (count < userAlwaysLoginError) {
-			// 还未达到锁定条件
-			return newModel;
-		}
-		int level = count / userAlwaysLoginError;
-		switch (level) {
-			case 1:
-				// 在错误倍数 为1 锁定 30分钟
-				newModel.setLockTime(TimeUnit.MINUTES.toSeconds(30));
-				break;
-			case 2:
-				// 在错误倍数 为2 锁定 1小时
-				newModel.setLockTime(TimeUnit.HOURS.toSeconds(1));
-				break;
-			default:
-				// 其他情况 10小时
-				newModel.setLockTime(TimeUnit.HOURS.toSeconds(10));
-				break;
-		}
-		return newModel;
-	}
+    /**
+     * 登录失败，重新计算锁定时间
+     */
+    public UserModel errorLock() {
+        // 未开启锁定功能
+        int userAlwaysLoginError = ServerExtConfigBean.getInstance().userAlwaysLoginError;
+        if (userAlwaysLoginError <= 0) {
+            return null;
+        }
+        UserModel newModel = new UserModel(this.getId());
+        newModel.setPwdErrorCount(ObjectUtil.defaultIfNull(this.getPwdErrorCount(), 0) + 1);
+        int count = newModel.getPwdErrorCount();
+        // 记录错误时间
+        newModel.setLastPwdErrorTime(DateUtil.currentSeconds());
+        if (count < userAlwaysLoginError) {
+            // 还未达到锁定条件
+            return newModel;
+        }
+        int level = count / userAlwaysLoginError;
+        switch (level) {
+            case 1:
+                // 在错误倍数 为1 锁定 30分钟
+                newModel.setLockTime(TimeUnit.MINUTES.toSeconds(30));
+                break;
+            case 2:
+                // 在错误倍数 为2 锁定 1小时
+                newModel.setLockTime(TimeUnit.HOURS.toSeconds(1));
+                break;
+            default:
+                // 其他情况 10小时
+                newModel.setLockTime(TimeUnit.HOURS.toSeconds(10));
+                break;
+        }
+        return newModel;
+    }
 
-	public boolean isSystemUser() {
-		return systemUser != null && systemUser == 1;
-	}
+    public boolean isSystemUser() {
+        return systemUser != null && systemUser == 1;
+    }
 
-	/**
-	 * 是否为超级管理员
-	 *
-	 * @return true 是
-	 */
-	public boolean isSuperSystemUser() {
-		return StrUtil.equals(getParent(), SYSTEM_ADMIN);
-	}
+    /**
+     * 是否为超级管理员
+     *
+     * @return true 是
+     */
+    public boolean isSuperSystemUser() {
+        return StrUtil.equals(getParent(), SYSTEM_ADMIN);
+    }
 
-	/**
-	 * demo 登录名默认为系统验证账号
-	 *
-	 * @return true
-	 */
-	public boolean isDemoUser() {
-		// demo 账号 和他创建的账号都是 demo
-		return UserModel.DEMO_USER.equals(getId()) || UserModel.DEMO_USER.equals(getParent());
-	}
+    /**
+     * demo 登录名默认为系统演示账号
+     *
+     * @return true
+     */
+    public boolean isDemoUser() {
+        // demo 账号 和他创建的账号都是 demo
+        return isRealDemoUser() || UserModel.DEMO_USER.equals(getParent());
+    }
+
+    /**
+     * demo 登录名默认为系统演示账号
+     *
+     * @return true
+     */
+    public boolean isRealDemoUser() {
+        return UserModel.DEMO_USER.equals(getId());
+    }
 
 //	/**
 //	 * 隐藏系统管理的真实id
