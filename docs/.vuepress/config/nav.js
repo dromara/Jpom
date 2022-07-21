c5f65fea2e3ea2f7392ba44ab3fb910f3ec95489
bwcx_jzy <bwcx_jzy@163.com>
2022-07-21 22:12:45 +0800

    update docs pages

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,16 +1,14 @@
 module.exports = [
     {
-        text: '首页', link: '/', items: [
-            {text: '演示站点', link: 'https://jpom.keepbx.cn?from=site-nav'},
-        ]
+        text: '首页', link: '/'
     },
+    {text: '演示站点', link: 'https://jpom.keepbx.cn?from=site-nav'},
     {
         text: '文档', link: '/pages/install/', items: [
             {text: 'v2.8.X迁移到v2.9.X指南', link: '/pages/upgrade/2.8.x-to-2.9.x/'},
-            {text: '实践案例', link: '/pages/practice/'},
-
         ]
     },
+    {text: '实践案例', link: '/pages/practice/'},
     {
         text: '常见问题', link: '/pages/FQA/', items: [
             {text: '名词解释', link: '/pages/FQA/proper-noun/'},
