2a854ab153a138255f41f17f02d0b1ec2a777211
魏宏斌 <1602586227@qq.com>
2022-07-25 08:50:45 +0800

    feat(docker-cli): 添加 构建配置

--- web-vue/src/router/route-menu.js
+++ web-vue/src/router/route-menu.js
@@ -36,6 +36,7 @@ const routeMenuMap = {
   logManage: "/system/log",
   update: "/system/upgrade",
   sysConfig: "/system/config",
+  buildConfig: "/system/build-config",
   projectSearch: "/node/search",
   // 数据库备份
   backup: "/system/backup",
