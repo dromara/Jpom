47debd11139902a0f77657a4bbdcd46c6bca3842
bwcx_jzy <bwcx_jzy@163.com>
2022-07-26 12:05:44 +0800

    update

---
+++ baiduPush.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 百度链接推送
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://jpom-docs.keepbx.cn&token=nsCvuh521afD4tyA"
+
+rm -rf urls.txt # 删除文件
