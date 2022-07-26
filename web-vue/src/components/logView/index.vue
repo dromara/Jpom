09a47c8ee83ac8cb2de542845ab5aadf641601a1
bwcx_jzy <bwcx_jzy@163.com>
2022-07-26 18:42:39 +0800

    fix 修复日志弹窗在部分屏幕下按钮重叠问题 #I5IZ9H

--- web-vue/src/components/logView/index.vue
+++ web-vue/src/components/logView/index.vue
@@ -2,68 +2,81 @@
   <div :style="`margin-top:${this.marginTop}`">
     <div class="log-filter">
       <template>
-        <a-row type="flex">
-          <a-col :span="18" style="width: 650px;">
-            <a-space>
-              <slot name="before"></slot>
-              <a-tooltip title="关键词高亮,支持正则(正则可能影响性能请酌情使用)">
-                <a-input addonBefore="正则:/" placeholder="关键词高亮,支持正则" v-model="temp.searchValue" :style="`width: ${searchWidth}`" @pressEnter="onSearch">
-                  <a-select
-                    :getPopupContainer="
-                      (triggerNode) => {
-                        return triggerNode.parentNode || document.body;
-                      }
-                    "
-                    slot="addonAfter"
-                    :value="'/' + this.regModifier"
-                    style="width: 80px"
-                  >
-                    <div @mousedown="(e) => e.preventDefault()" slot="dropdownRender" style="width: 300px; padding: 10px; cursor: pointer; background-color: #fff; border-radius: 5px">
-                      <a-checkbox-group @change="regModifierChange" :value="regModifiers" :options="modifiers"> </a-checkbox-group>
-                    </div>
-                  </a-select>
-                </a-input>
-              </a-tooltip>
-
-              <a-tooltip title="为避免显示内容太多而造成浏览器卡顿,限制只显示最后多少行日志。修改后需要回车才能生效">
-                <a-input addonBefore="行数" :min="1" style="width: 200px" :disabled="!this.temp.logScroll" v-model="temp.tempLogShowLine" placeholder="显示行数" @pressEnter="onSearch">
-                  <template slot="addonAfter">
-                    <a-icon type="swap" @click="onSearch" />
-                  </template>
-                </a-input>
-              </a-tooltip>
-            </a-space>
+        <a-row type="flex" align="middle">
+          <a-col>
+            <slot name="before"></slot>
           </a-col>
-          <a-col v-if="this.extendBar" :span="6" style="width: 295px;text-align: right">
+          <a-col :span="8" style="padding-left: 10px">
+            <a-tooltip title="关键词高亮,支持正则(正则可能影响性能请酌情使用)">
+              <a-input addonBefore="正则:/" placeholder="关键词高亮,支持正则" v-model="temp.searchValue" @pressEnter="onSearch">
+                <a-select
+                  :getPopupContainer="
+                    (triggerNode) => {
+                      return triggerNode.parentNode || document.body;
+                    }
+                  "
+                  slot="addonAfter"
+                  :value="'/' + this.regModifier"
+                >
+                  <div @mousedown="(e) => e.preventDefault()" slot="dropdownRender" style="width: 200px; max-width: 300px; padding: 10px; cursor: pointer; background-color: #fff; border-radius: 5px">
+                    <a-checkbox-group @change="regModifierChange" :value="regModifiers" :options="modifiers"> </a-checkbox-group>
+                  </div>
+                </a-select>
+              </a-input>
+            </a-tooltip>
+          </a-col>
+          <a-col :span="4" style="padding-left: 10px">
+            <a-tooltip title="为避免显示内容太多而造成浏览器卡顿,限制只显示最后多少行日志。修改后需要回车才能生效">
+              <a-input addonBefore="行数" :min="1" :disabled="!this.temp.logScroll" v-model="temp.tempLogShowLine" placeholder="显示行数" @pressEnter="onSearch">
+                <template slot="addonAfter">
+                  <a-icon type="swap" @click="onSearch" />
+                </template>
+              </a-input>
+            </a-tooltip>
+          </a-col>
+          <a-col v-if="this.extendBar" style="padding-left: 10px">
             <a-space>
-              <a-tooltip title="内容超过边界自动换行">
-                <a-switch v-model="temp.wordBreak" checked-children="自动换行" un-checked-children="不换行" />
-              </a-tooltip>
-              <a-tooltip title="有新内容后是否自动滚动到底部">
-                <a-switch v-model="temp.logScroll" checked-children="自动滚动" un-checked-children="不滚动" />
-              </a-tooltip>
               <a-tooltip title="清空当前缓冲区内容">
                 <a-button type="link" style="padding: 0" @click="clearLogCache" icon="delete"><span style="margin-left: 2px">清空</span></a-button>
               </a-tooltip>
-              <a-popover title="正则语法参考">
-                <template slot="content">
-                  <ul>
-                    <li><b>.</b> - 除换行符以外的所有字符。</li>
-                    <li><b>^</b> - 字符串开头。</li>
-                    <li><b>$</b> - 字符串结尾。</li>
-                    <li><b>\d,\w,\s</b> - 匹配数字、字符、空格。</li>
-                    <li><b>\D,\W,\S</b> - 匹配非数字、非字符、非空格。</li>
-                    <li><b>[abc]</b> - 匹配 a、b 或 c 中的一个字母。</li>
-                    <li><b>[a-z]</b> - 匹配 a 到 z 中的一个字母。</li>
-                    <li><b>[^abc]</b> - 匹配除了 a、b 或 c 中的其他字母。</li>
-                    <li><b>aa|bb</b> - 匹配 aa 或 bb。</li>
-                    <li><b>[^%&',;=?$\x22]+</b> - 含有^%&',;=?$\"等字符。</li>
-                    <li><b>[\u4E00-\u9FA5A-Za-z0-9_]</b> - 中文、英文、数字包括下划线。</li>
-                    <li><b>[\u4e00-\u9fa5]</b> - 汉字。</li>
-                  </ul>
-                </template>
-                <a-button type="link" style="padding: 0" icon="unordered-list"><span style="margin-left: 2px">语法参考</span></a-button>
-              </a-popover>
+              <a-dropdown>
+                <a-button type="link" style="padding: 0" icon="setting"> 设置 <a-icon type="down" /></a-button>
+                <a-menu slot="overlay">
+                  <a-menu-item key="0">
+                    <a-tooltip title="内容超过边界自动换行">
+                      <a-switch v-model="temp.wordBreak" checked-children="自动换行" un-checked-children="不换行" />
+                    </a-tooltip>
+                  </a-menu-item>
+                  <a-menu-divider />
+                  <a-menu-item key="3">
+                    <a-tooltip title="有新内容后是否自动滚动到底部">
+                      <a-switch v-model="temp.logScroll" checked-children="自动滚动" un-checked-children="不滚动" />
+                    </a-tooltip>
+                  </a-menu-item>
+                  <a-menu-divider />
+                  <a-menu-item key="2">
+                    <a-popover title="正则语法参考" placement="leftBottom">
+                      <template slot="content">
+                        <ul>
+                          <li><b>.</b> - 除换行符以外的所有字符。</li>
+                          <li><b>^</b> - 字符串开头。</li>
+                          <li><b>$</b> - 字符串结尾。</li>
+                          <li><b>\d,\w,\s</b> - 匹配数字、字符、空格。</li>
+                          <li><b>\D,\W,\S</b> - 匹配非数字、非字符、非空格。</li>
+                          <li><b>[abc]</b> - 匹配 a、b 或 c 中的一个字母。</li>
+                          <li><b>[a-z]</b> - 匹配 a 到 z 中的一个字母。</li>
+                          <li><b>[^abc]</b> - 匹配除了 a、b 或 c 中的其他字母。</li>
+                          <li><b>aa|bb</b> - 匹配 aa 或 bb。</li>
+                          <li><b>[^%&',;=?$\x22]+</b> - 含有^%&',;=?$\"等字符。</li>
+                          <li><b>[\u4E00-\u9FA5A-Za-z0-9_]</b> - 中文、英文、数字包括下划线。</li>
+                          <li><b>[\u4e00-\u9fa5]</b> - 汉字。</li>
+                        </ul>
+                      </template>
+                      <a-button type="link" style="padding: 0" icon="unordered-list"><span style="margin-left: 2px">正则语法参考</span></a-button>
+                    </a-popover>
+                  </a-menu-item>
+                </a-menu>
+              </a-dropdown>
             </a-space>
           </a-col>
         </a-row>
@@ -99,10 +112,6 @@ export default {
     marginTop: {
       String,
       default: "0px",
-    },
-    searchWidth: {
-      String,
-      default: "360px",
     },
     extendBar: {
       Boolean,
@@ -193,5 +202,10 @@ export default {
   padding-top: 0;
   padding-bottom: 10px;
   line-height: 0;
+}
+
+/deep/ .ant-checkbox-group-item {
+  display: flex;
+  align-items: center;
 }
 </style>
