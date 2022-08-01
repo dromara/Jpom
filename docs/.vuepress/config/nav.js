71ce98c2259b6df848bab8455d15ec0ec4f7bd1e
bwcx_jzy <bwcx_jzy@163.com>
2022-08-01 19:04:33 +0800

    update docs pages

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,27 +1,31 @@
 module.exports = [
-    {
-        text: '首页', link: '/'
-    },
-    {text: '演示站点', link: 'https://demo.jpom.top?from=site-nav'},
-    {
-        text: '文档', link: '/pages/install/', items: [
-            {text: 'v2.8.X迁移到v2.9.X指南', link: '/pages/upgrade/2.8.x-to-2.9.x/'},
-        ]
-    },
-    {text: '🔥实践案例', link: '/pages/practice/'},
-    {
-        text: '常见问题', link: '/pages/FQA/', items: [
-            {text: '名词解释', link: '/pages/FQA/proper-noun/'},
-        ]
-    },
-    {
-        text: '💖支持', link: '/pages/praise/', items: [
-            {text: '加入社区讨论', link: '/pages/praise/join/'},
-        ]
-    },
+  {
+    text: '首页', link: '/'
+  },
+  {text: '演示站点', link: 'https://demo.jpom.top?from=site-nav'},
+  {
+    text: '文档', link: '/pages/install/', items: [
+      {text: 'v2.8.X迁移到v2.9.X指南', link: '/pages/upgrade/2.8.x-to-2.9.x/'},
+    ]
+  },
+  {text: '🔥实践案例', link: '/pages/practice/'},
+  {
+    text: '常见问题', link: '/pages/FQA/', items: [
+      {text: '名词解释', link: '/pages/FQA/proper-noun/'},
+    ]
+  },
+  {
+    text: '💖支持', link: '/pages/praise/', items: [
+      {text: '加入社区讨论', link: '/pages/praise/join/'},
+    ]
+  },
 
-    {text: '更新记录', link: '/pages/changelog/new/'},
+  {
+    text: '更新记录', link: '/pages/changelog/new/', items: [
+      {text: '下载中心', link: '/pages/all-downloads/'},
+    ]
+  },
 
-    {text: 'Gitee', link: 'https://gitee.com/dromara/Jpom'},
-    {text: 'Github', link: 'https://github.com/dromara/Jpom'},
+  {text: 'Gitee', link: 'https://gitee.com/dromara/Jpom'},
+  {text: 'Github', link: 'https://github.com/dromara/Jpom'},
 ];
