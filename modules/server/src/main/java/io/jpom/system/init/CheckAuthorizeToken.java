305ebb755bac2f63df60f168546b3e47f0ccac27
bwcx_jzy <bwcx_jzy@163.com>
2022-08-14 15:33:24 +0800

    fix checkpassword

--- modules/server/src/main/java/io/jpom/system/init/CheckAuthorizeToken.java
+++ modules/server/src/main/java/io/jpom/system/init/CheckAuthorizeToken.java
@@ -22,13 +22,17 @@
  */
 package io.jpom.system.init;
 
+import cn.hutool.core.text.PasswdStrength;
 import cn.hutool.core.util.StrUtil;
 import cn.jiangzeyin.common.PreLoadClass;
 import cn.jiangzeyin.common.PreLoadMethod;
+import io.jpom.JpomApplication;
 import io.jpom.system.JpomRuntimeException;
 import io.jpom.system.ServerExtConfigBean;
-import io.jpom.util.CheckPassword;
 import lombok.extern.slf4j.Slf4j;
+
+import static cn.hutool.core.text.PasswdStrength.PASSWD_LEVEL.EASY;
+import static cn.hutool.core.text.PasswdStrength.PASSWD_LEVEL.MIDIUM;
 
 /**
  * 验证token 合法性
@@ -40,20 +44,22 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CheckAuthorizeToken {
 
-	@PreLoadMethod
-	private static void check() {
-		String authorizeToken = ServerExtConfigBean.getInstance().getAuthorizeToken();
-		if (StrUtil.isEmpty(authorizeToken)) {
-			return;
-		}
-		if (authorizeToken.length() < 6) {
-			log.error("配置的授权token长度小于六位不生效", new JpomRuntimeException("配置的授权token长度小于六位不生效"));
-			System.exit(-1);
-		}
-		int strength = CheckPassword.checkPasswordStrength(authorizeToken);
-		if (strength != 2) {
-			log.error("配置的授权token 需要包含数字，字母，符号的组合", new JpomRuntimeException("配置的授权token 需要包含数字，字母，符号的组合"));
-			System.exit(-1);
-		}
-	}
+    @PreLoadMethod
+    private static void check() {
+        String authorizeToken = ServerExtConfigBean.getInstance().getAuthorizeToken();
+        if (StrUtil.isEmpty(authorizeToken)) {
+            return;
+        }
+        if (authorizeToken.length() < 6) {
+            log.error("配置的授权token长度小于六位不生效", new JpomRuntimeException("配置的授权token长度小于六位不生效"));
+            System.exit(-1);
+        }
+        PasswdStrength.PASSWD_LEVEL level = PasswdStrength.getLevel(authorizeToken);
+
+        if (level == EASY || level == MIDIUM) {
+            JpomApplication.consoleExit(-1, "配置的授权token 需要包含数字，字母，符号的组合");
+            //log.error("配置的授权token 需要包含数字，字母，符号的组合", new JpomRuntimeException("配置的授权token 需要包含数字，字母，符号的组合"));
+            //System.exit(-1);
+        }
+    }
 }
