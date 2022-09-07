db9b68f2adee020fab8808538db19199ba7c886f
bwcx_jzy <bwcx_jzy@163.com>
2022-09-07 10:09:47 +0800

    commit 2.9.10

--- script/deploy-release-oss.sh
+++ script/deploy-release-oss.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-DIR_PATH=$(cd `dirname $0`; pwd)"/../"
+current_path=$(pwd)
+case "$(uname)" in
+    Linux)
+		bin_abs_path=$(readlink -f $(dirname "$0"))
+		;;
+	*)
+		bin_abs_path=`cd $(dirname $0) || exit; pwd`
+		;;
+esac
+DIR_PATH=${bin_abs_path}/../
 
-echo ${DIR_PATH}
+now_version="$(cat "$DIR_PATH"/jpom-parent/docs/version.txt)"
 
-now_version=`cat ${DIR_PATH}/jpom-parent/docs/version.txt`
+echo "当前路径：${current_path} 脚本路径：${DIR_PATH} 当前版本：${now_version}"
 
 if [ ! -n "$now_version" ]; then
     echo "ERROR: 版本不存在，请确认 /jpom-parent/docs/version.txt 中信息正确"
