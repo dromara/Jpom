c211223142133dd60dd7ab3c0d0f554b97e520f4
bwcx_jzy <bwcx_jzy@163.com>
2022-09-09 14:21:14 +0800

    commit 2.9.11

--- script/deploy-docs-pages.sh
+++ script/deploy-docs-pages.sh
@@ -5,7 +5,8 @@ DIR_PATH=$(cd `dirname $0`; pwd)"/../"
 echo ${DIR_PATH}
 
 # 提交代码 docs
-cd ${DIR_PATH} && cd docs && git add . &&  git commit -m 'update docs pages' && git push
+cd ${DIR_PATH} && cd docs && git add . &&  git commit -m 'update docs pages'
+cd ${DIR_PATH} && git push
 
 # 打包
 cd ${DIR_PATH} && cd docs && yarn && yarn run build
