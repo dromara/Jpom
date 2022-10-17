f7917e28df1a08febce4693e540253c0b695dcfd
bwcx_jzy <bwcx_jzy@163.com>
2022-10-17 17:49:19 +0800

    update docs pages

--- docs/.vuepress/config/htmlModules.js
+++ docs/.vuepress/config/htmlModules.js
@@ -41,6 +41,31 @@ module.exports = {
       <img class="no-zoom" height="60" width="224" src="/images/friends/ad/eoapi.gif">
     </a>
   `,
+  // 万维广告
+  pageT: `
+  <div class="wwads-cn wwads-horizontal page-wwads" data-id="188"></div>
+  <style>
+      .page-wwads{
+        width:100%!important;
+        min-height: 0;
+        margin: 0;
+      }
+      .page-wwads .wwads-img img{
+        width:80px!important;
+      }
+      .page-wwads .wwads-poweredby{
+        width: 40px;
+        position: absolute;
+        right: 25px;
+        bottom: 3px;
+      }
+      .wwads-content .wwads-text, .page-wwads .wwads-text{
+        height: 100%;
+        padding-top: 5px;
+        display: block;
+      }
+  </style>
+  `,
   //
   pageB: `
 
