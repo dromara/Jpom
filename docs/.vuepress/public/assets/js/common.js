1841a42616140e0f432221646b991a60a3e24974
bwcx_jzy <bwcx_jzy@163.com>
2022-11-10 13:38:40 +0800

    update

--- docs/.vuepress/public/assets/js/common.js
+++ docs/.vuepress/public/assets/js/common.js
@@ -1,18 +1,5 @@
 (function () {
   var bp = document.createElement('script');
-  var curProtocol = window.location.protocol.split(':')[0];
-  if (curProtocol === 'https') {
-    bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-  } else {
-    bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-  }
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(bp, s);
-})();
-
-
-(function () {
-  var bp = document.createElement('script');
   bp.type = 'text/javascript';
   bp.async = true;
   bp.src = 'https://www.googletagmanager.com/gtag/js?id=G-FYSG66S4HQ';
