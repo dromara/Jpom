d44b30154b1caaf25b91e70104579c832e7dc314
bwcx_jzy <bwcx_jzy@163.com>
2022-11-24 22:28:21 +0800

    fix 优化节点分发结果添加执行耗时、上传文件大小信息

--- modules/server/src/main/java/io/jpom/outgiving/OutGivingItemRun.java
+++ modules/server/src/main/java/io/jpom/outgiving/OutGivingItemRun.java
@@ -23,8 +23,10 @@
 package io.jpom.outgiving;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.BetweenFormatter;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.SystemClock;
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.ObjectUtil;
@@ -32,6 +34,7 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpStatus;
 import cn.jiangzeyin.common.JsonMessage;
 import cn.jiangzeyin.common.spring.SpringUtil;
+import com.alibaba.fastjson.JSONObject;
 import io.jpom.JpomApplication;
 import io.jpom.model.AfterOpt;
 import io.jpom.model.data.NodeModel;
@@ -57,148 +60,154 @@ import java.util.stream.Collectors;
 @Slf4j
 public class OutGivingItemRun implements Callable<OutGivingNodeProject.Status> {
 
-	private final String outGivingId;
-	private final OutGivingNodeProject outGivingNodeProject;
-	private final NodeModel nodeModel;
-	private final File file;
-	private final AfterOpt afterOpt;
-	private final UserModel userModel;
-	private final boolean unzip;
-	private final boolean clearOld;
-	/**
-	 * 数据库记录id
-	 */
-	private final String logId;
+    private final String outGivingId;
+    private final OutGivingNodeProject outGivingNodeProject;
+    private final NodeModel nodeModel;
+    private final File file;
+    private final AfterOpt afterOpt;
+    private final UserModel userModel;
+    private final boolean unzip;
+    private final boolean clearOld;
+    /**
+     * 数据库记录id
+     */
+    private final String logId;
 
-	public OutGivingItemRun(OutGivingModel item,
-							OutGivingNodeProject outGivingNodeProject,
-							File file,
-							UserModel userModel,
-							boolean unzip) {
-		this.outGivingId = item.getId();
-		this.unzip = unzip;
-		this.clearOld = item.clearOld();
-		this.outGivingNodeProject = outGivingNodeProject;
-		this.file = file;
-		this.afterOpt = ObjectUtil.defaultIfNull(EnumUtil.likeValueOf(AfterOpt.class, item.getAfterOpt()), AfterOpt.No);
-		//
-		NodeService nodeService = SpringUtil.getBean(NodeService.class);
-		this.nodeModel = nodeService.getByKey(outGivingNodeProject.getNodeId());
-		//
-		this.userModel = userModel;
-		this.logId = IdUtil.fastSimpleUUID();
-	}
+    public OutGivingItemRun(OutGivingModel item,
+                            OutGivingNodeProject outGivingNodeProject,
+                            File file,
+                            UserModel userModel,
+                            boolean unzip) {
+        this.outGivingId = item.getId();
+        this.unzip = unzip;
+        this.clearOld = item.clearOld();
+        this.outGivingNodeProject = outGivingNodeProject;
+        this.file = file;
+        this.afterOpt = ObjectUtil.defaultIfNull(EnumUtil.likeValueOf(AfterOpt.class, item.getAfterOpt()), AfterOpt.No);
+        //
+        NodeService nodeService = SpringUtil.getBean(NodeService.class);
+        this.nodeModel = nodeService.getByKey(outGivingNodeProject.getNodeId());
+        //
+        this.userModel = userModel;
+        this.logId = IdUtil.fastSimpleUUID();
+    }
 
-	@Override
-	public OutGivingNodeProject.Status call() {
-		OutGivingNodeProject.Status result;
-		try {
-			this.updateStatus(this.outGivingId, this.outGivingNodeProject,
-					OutGivingNodeProject.Status.Ing, "开始分发");
-			//
-			JsonMessage<String> jsonMessage = OutGivingRun.fileUpload(file, null,
-					this.outGivingNodeProject.getProjectId(),
-					unzip,
-					afterOpt,
-					this.nodeModel, this.userModel, this.clearOld);
-			if (jsonMessage.getCode() == HttpStatus.HTTP_OK) {
-				result = OutGivingNodeProject.Status.Ok;
-			} else {
-				result = OutGivingNodeProject.Status.Fail;
-			}
-			this.updateStatus(this.outGivingId, this.outGivingNodeProject, result, jsonMessage.toString());
-		} catch (Exception e) {
-			log.error(this.outGivingNodeProject.getNodeId() + " " + this.outGivingNodeProject.getProjectId() + " " + "分发异常保存", e);
-			result = OutGivingNodeProject.Status.Fail;
-			this.updateStatus(this.outGivingId, this.outGivingNodeProject, result, "error:" + e.getMessage());
-		}
-		return result;
-	}
+    @Override
+    public OutGivingNodeProject.Status call() {
+        OutGivingNodeProject.Status result;
+        long time = SystemClock.now();
+        String fileSize = FileUtil.readableFileSize(file);
+        try {
+            this.updateStatus(this.outGivingId, this.outGivingNodeProject,
+                OutGivingNodeProject.Status.Ing, "开始分发");
+            //
+            JsonMessage<String> jsonMessage = OutGivingRun.fileUpload(file, null,
+                this.outGivingNodeProject.getProjectId(),
+                unzip,
+                afterOpt,
+                this.nodeModel, this.userModel, this.clearOld);
+            if (jsonMessage.getCode() == HttpStatus.HTTP_OK) {
+                result = OutGivingNodeProject.Status.Ok;
+            } else {
+                result = OutGivingNodeProject.Status.Fail;
+            }
+            JSONObject jsonObject = jsonMessage.toJson();
+            jsonObject.put("upload_duration", new BetweenFormatter(SystemClock.now() - time, BetweenFormatter.Level.MILLISECOND, 2).format());
+            jsonObject.put("upload_file_size", fileSize);
+            this.updateStatus(this.outGivingId, this.outGivingNodeProject, result, jsonObject.toString());
+        } catch (Exception e) {
+            log.error("{} {} 分发异常保存", this.outGivingNodeProject.getNodeId(), this.outGivingNodeProject.getProjectId(), e);
+            result = OutGivingNodeProject.Status.Fail;
+            this.updateStatus(this.outGivingId, this.outGivingNodeProject, result, StrUtil.format("error: {} upload_duration: {} upload_file_size: {}",
+                e.getMessage(), new BetweenFormatter(SystemClock.now() - time, BetweenFormatter.Level.MILLISECOND, 2).format(), fileSize));
+        }
+        return result;
+    }
 
-	/**
-	 * 更新状态
-	 *
-	 * @param outGivingId              分发id
-	 * @param outGivingNodeProjectItem 分发项
-	 * @param status                   状态
-	 * @param msg                      消息描述
-	 */
-	public void updateStatus(String outGivingId,
-							 OutGivingNodeProject outGivingNodeProjectItem,
-							 OutGivingNodeProject.Status status,
-							 String msg) {
-		String userId = this.userModel == null ? JpomApplication.SYSTEM_ID : this.userModel.getId();
-		updateStatus(this.logId, outGivingId, outGivingNodeProjectItem, status, msg, userId);
-	}
+    /**
+     * 更新状态
+     *
+     * @param outGivingId              分发id
+     * @param outGivingNodeProjectItem 分发项
+     * @param status                   状态
+     * @param msg                      消息描述
+     */
+    public void updateStatus(String outGivingId,
+                             OutGivingNodeProject outGivingNodeProjectItem,
+                             OutGivingNodeProject.Status status,
+                             String msg) {
+        String userId = this.userModel == null ? JpomApplication.SYSTEM_ID : this.userModel.getId();
+        updateStatus(this.logId, outGivingId, outGivingNodeProjectItem, status, msg, userId);
+    }
 
-	/**
-	 * 更新状态
-	 *
-	 * @param logId                    日志ID
-	 * @param outGivingId              分发id
-	 * @param outGivingNodeProjectItem 分发项
-	 * @param status                   状态
-	 * @param msg                      消息描述
-	 */
-	public static void updateStatus(String logId,
-									String outGivingId,
-									OutGivingNodeProject outGivingNodeProjectItem,
-									OutGivingNodeProject.Status status,
-									String msg,
-									String userId) {
-		synchronized (outGivingId.intern()) {
-			OutGivingServer outGivingServer = SpringUtil.getBean(OutGivingServer.class);
-			OutGivingModel outGivingModel = outGivingServer.getByKey(outGivingId);
+    /**
+     * 更新状态
+     *
+     * @param logId                    日志ID
+     * @param outGivingId              分发id
+     * @param outGivingNodeProjectItem 分发项
+     * @param status                   状态
+     * @param msg                      消息描述
+     */
+    public static void updateStatus(String logId,
+                                    String outGivingId,
+                                    OutGivingNodeProject outGivingNodeProjectItem,
+                                    OutGivingNodeProject.Status status,
+                                    String msg,
+                                    String userId) {
+        synchronized (outGivingId.intern()) {
+            OutGivingServer outGivingServer = SpringUtil.getBean(OutGivingServer.class);
+            OutGivingModel outGivingModel = outGivingServer.getByKey(outGivingId);
 
-			List<OutGivingNodeProject> outGivingNodeProjects = outGivingModel.outGivingNodeProjectList();
-			Assert.notEmpty(outGivingNodeProjects, "没有分发项目");
-			OutGivingNodeProject finOutGivingNodeProject = null;
-			//
-			for (OutGivingNodeProject outGivingNodeProject : outGivingNodeProjects) {
-				if (!outGivingNodeProject.getProjectId().equalsIgnoreCase(outGivingNodeProjectItem.getProjectId()) ||
-						!outGivingNodeProject.getNodeId().equalsIgnoreCase(outGivingNodeProjectItem.getNodeId())) {
-					continue;
-				}
-				outGivingNodeProject.setStatus(status.getCode());
-				outGivingNodeProject.setResult(msg);
-				outGivingNodeProject.setLastOutGivingTime(DateUtil.now());
-				//
-				finOutGivingNodeProject = outGivingNodeProject;
-			}
-			{
-				List<OutGivingNodeProject.Status> collect = outGivingNodeProjects
-						.stream()
-						.map(outGivingNodeProject -> EnumUtil.likeValueOf(OutGivingNodeProject.Status.class, outGivingNodeProject.getStatus()))
-						.collect(Collectors.toList());
-				OutGivingModel.Status outGivingStatus = CollUtil.contains(collect, OutGivingNodeProject.Status.Ing)
-						? OutGivingModel.Status.ING : OutGivingModel.Status.DONE;
-				// 更新分发数据
-				OutGivingModel outGivingModel1 = new OutGivingModel();
-				outGivingModel1.setId(outGivingId);
-				outGivingModel1.setStatus(outGivingStatus.getCode());
-				outGivingModel1.outGivingNodeProjectList(outGivingNodeProjects);
-				outGivingServer.update(outGivingModel1);
-			}
-			// 更新日志数据
-			OutGivingLog outGivingLog = new OutGivingLog();
-			outGivingLog.setId(StrUtil.emptyToDefault(logId, IdUtil.fastSimpleUUID()));
+            List<OutGivingNodeProject> outGivingNodeProjects = outGivingModel.outGivingNodeProjectList();
+            Assert.notEmpty(outGivingNodeProjects, "没有分发项目");
+            OutGivingNodeProject finOutGivingNodeProject = null;
+            //
+            for (OutGivingNodeProject outGivingNodeProject : outGivingNodeProjects) {
+                if (!outGivingNodeProject.getProjectId().equalsIgnoreCase(outGivingNodeProjectItem.getProjectId()) ||
+                    !outGivingNodeProject.getNodeId().equalsIgnoreCase(outGivingNodeProjectItem.getNodeId())) {
+                    continue;
+                }
+                outGivingNodeProject.setStatus(status.getCode());
+                outGivingNodeProject.setResult(msg);
+                outGivingNodeProject.setLastOutGivingTime(DateUtil.now());
+                //
+                finOutGivingNodeProject = outGivingNodeProject;
+            }
+            {
+                List<OutGivingNodeProject.Status> collect = outGivingNodeProjects
+                    .stream()
+                    .map(outGivingNodeProject -> EnumUtil.likeValueOf(OutGivingNodeProject.Status.class, outGivingNodeProject.getStatus()))
+                    .collect(Collectors.toList());
+                OutGivingModel.Status outGivingStatus = CollUtil.contains(collect, OutGivingNodeProject.Status.Ing)
+                    ? OutGivingModel.Status.ING : OutGivingModel.Status.DONE;
+                // 更新分发数据
+                OutGivingModel outGivingModel1 = new OutGivingModel();
+                outGivingModel1.setId(outGivingId);
+                outGivingModel1.setStatus(outGivingStatus.getCode());
+                outGivingModel1.outGivingNodeProjectList(outGivingNodeProjects);
+                outGivingServer.update(outGivingModel1);
+            }
+            // 更新日志数据
+            OutGivingLog outGivingLog = new OutGivingLog();
+            outGivingLog.setId(StrUtil.emptyToDefault(logId, IdUtil.fastSimpleUUID()));
 
-			if (finOutGivingNodeProject != null) {
-				outGivingLog.setNodeId(finOutGivingNodeProject.getNodeId());
-				outGivingLog.setProjectId(finOutGivingNodeProject.getProjectId());
-			}
-			outGivingLog.setModifyUser(userId);
-			outGivingLog.setOutGivingId(outGivingId);
-			outGivingLog.setResult(msg);
-			outGivingLog.setStatus(status.getCode());
-			DbOutGivingLogService dbOutGivingLogService = SpringUtil.getBean(DbOutGivingLogService.class);
-			if (status == OutGivingNodeProject.Status.Ing || status == OutGivingNodeProject.Status.Cancel) {
-				// 开始或者 取消都还没有记录
-				dbOutGivingLogService.insert(outGivingLog);
-			} else {
-				outGivingLog.setEndTime(SystemClock.now());
-				dbOutGivingLogService.update(outGivingLog);
-			}
-		}
-	}
+            if (finOutGivingNodeProject != null) {
+                outGivingLog.setNodeId(finOutGivingNodeProject.getNodeId());
+                outGivingLog.setProjectId(finOutGivingNodeProject.getProjectId());
+            }
+            outGivingLog.setModifyUser(userId);
+            outGivingLog.setOutGivingId(outGivingId);
+            outGivingLog.setResult(msg);
+            outGivingLog.setStatus(status.getCode());
+            DbOutGivingLogService dbOutGivingLogService = SpringUtil.getBean(DbOutGivingLogService.class);
+            if (status == OutGivingNodeProject.Status.Ing || status == OutGivingNodeProject.Status.Cancel) {
+                // 开始或者 取消都还没有记录
+                dbOutGivingLogService.insert(outGivingLog);
+            } else {
+                outGivingLog.setEndTime(SystemClock.now());
+                dbOutGivingLogService.update(outGivingLog);
+            }
+        }
+    }
 }
