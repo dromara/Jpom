ac108838e6ab42b7026c71307e4295275cc6ce2a
bwcx_jzy <bwcx_jzy@163.com>
2022-12-21 18:01:18 +0800

    commit

--- script/deploy-release-oss.sh
+++ script/deploy-release-oss.sh
@@ -9,9 +9,9 @@ case "$(uname)" in
 		bin_abs_path=`cd $(dirname $0) || exit; pwd`
 		;;
 esac
-DIR_PATH=${bin_abs_path}/../
+DIR_PATH=${bin_abs_path}/../jpom-parent/
 
-now_version="$(cat "${DIR_PATH}/jpom-parent/docs/version.txt")"
+now_version="$(cat "${DIR_PATH}/docs/version.txt")"
 
 #
 now_version="$(echo $now_version |tr -d '\n\r')"
@@ -40,20 +40,20 @@ esac
 echo  "${OS} ${ossUtil} 开始上传"
 
 # 同步到 agent oss 中
-${ossUtil} cp ${DIR_PATH}/jpom-parent/modules/agent/target/agent-${now_version}-release.tar.gz oss://jpom-releases/release/${now_version}/ -f
-${ossUtil} cp ${DIR_PATH}/jpom-parent/modules/agent/target/agent-${now_version}-release.tar.gz.sha1 oss://jpom-releases/release/${now_version}/ -f
-${ossUtil} cp ${DIR_PATH}/jpom-parent/modules/agent/target/agent-${now_version}-release.zip oss://jpom-releases/release/${now_version}/ -f
-${ossUtil} cp ${DIR_PATH}/jpom-parent/modules/agent/target/agent-${now_version}-release.zip.sha1 oss://jpom-releases/release/${now_version}/ -f
+${ossUtil} cp ${DIR_PATH}/modules/agent/target/agent-${now_version}-release.tar.gz oss://jpom-releases/release/${now_version}/ -f
+${ossUtil} cp ${DIR_PATH}/modules/agent/target/agent-${now_version}-release.tar.gz.sha1 oss://jpom-releases/release/${now_version}/ -f
+${ossUtil} cp ${DIR_PATH}/modules/agent/target/agent-${now_version}-release.zip oss://jpom-releases/release/${now_version}/ -f
+${ossUtil} cp ${DIR_PATH}/modules/agent/target/agent-${now_version}-release.zip.sha1 oss://jpom-releases/release/${now_version}/ -f
 
 # 同步到 server oss 中
-${ossUtil} cp ${DIR_PATH}/jpom-parent/modules/server/target/server-${now_version}-release.tar.gz oss://jpom-releases/release/${now_version}/ -f
-${ossUtil} cp ${DIR_PATH}/jpom-parent/modules/server/target/server-${now_version}-release.tar.gz.sha1 oss://jpom-releases/release/${now_version}/ -f
-${ossUtil} cp ${DIR_PATH}/jpom-parent/modules/server/target/server-${now_version}-release.zip oss://jpom-releases/release/${now_version}/ -f
-${ossUtil} cp ${DIR_PATH}/jpom-parent/modules/server/target/server-${now_version}-release.zip.sha1 oss://jpom-releases/release/${now_version}/ -f
+${ossUtil} cp ${DIR_PATH}/modules/server/target/server-${now_version}-release.tar.gz oss://jpom-releases/release/${now_version}/ -f
+${ossUtil} cp ${DIR_PATH}/modules/server/target/server-${now_version}-release.tar.gz.sha1 oss://jpom-releases/release/${now_version}/ -f
+${ossUtil} cp ${DIR_PATH}/modules/server/target/server-${now_version}-release.zip oss://jpom-releases/release/${now_version}/ -f
+${ossUtil} cp ${DIR_PATH}/modules/server/target/server-${now_version}-release.zip.sha1 oss://jpom-releases/release/${now_version}/ -f
 
 rm -f ${DIR_PATH}/jpom-${now_version}.zip
 
-zip -rj ${DIR_PATH}/jpom-${now_version}.zip ${DIR_PATH}/jpom-parent/modules/server/target/server-${now_version}-release.zip ${DIR_PATH}/jpom-parent/modules/agent/target/agent-${now_version}-release.zip
+zip -rj ${DIR_PATH}/jpom-${now_version}.zip ${DIR_PATH}/modules/server/target/server-${now_version}-release.zip ${DIR_PATH}/modules/agent/target/agent-${now_version}-release.zip
 
 # 完整的压缩包
 ${ossUtil} cp ${DIR_PATH}/jpom-${now_version}.zip oss://jpom-releases/release/${now_version}/ -f
