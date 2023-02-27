c55e0067c32ee36afa317750a99588a8e76b480b
bwcx_jzy <bwcx_jzy@163.com>
2023-02-27 08:42:24 +0800

    feat temp ssh 资产管理

---
+++ web-vue/src/api/system/assets-ssh.js
@@ -0,0 +1,66 @@
+import axios from "@/api/config";
+
+// ssh 列表
+export function machineSshListData(params) {
+  return axios({
+    url: "/system/assets/ssh/list-data",
+    method: "post",
+    params: params,
+  });
+}
+
+export function machineSshListGroup(params) {
+  return axios({
+    url: "/system/assets/ssh/list-group",
+    method: "get",
+    params: params,
+  });
+}
+
+// 编辑ssh
+export function machineSshEdit(params) {
+  return axios({
+    url: "/system/assets/ssh/edit",
+    method: "post",
+    params: params,
+  });
+}
+
+// 检查 agent
+export function machineSshCheckAgent(params) {
+  return axios({
+    url: "/system/assets/ssh/check-agent",
+    method: "get",
+    params: params,
+    timeout: 0,
+    headers: {
+      loading: "no",
+    },
+  });
+}
+
+// 删除 ssh
+export function machineSshDelete(params) {
+  return axios({
+    url: "/system/assets/ssh/delete",
+    method: "post",
+    params: params,
+  });
+}
+
+// ssh 操作日志列表
+export function getMachineSshOperationLogList(params) {
+  return axios({
+    url: "/system/assets/ssh/log-list-data",
+    method: "post",
+    data: params,
+  });
+}
+
+export function machineListGroupWorkspaceSsh(params) {
+  return axios({
+    url: "/system/assets/ssh/list-workspace-ssh",
+    method: "get",
+    params: params,
+  });
+}
