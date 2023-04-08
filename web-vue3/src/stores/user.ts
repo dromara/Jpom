721bb61139fac36d564e4356589af438f98450fa
bwcx_jzy <bwcx_jzy@163.com>
2023-04-08 15:55:42 +0800

    fix

--- web-vue3/src/stores/user.ts
+++ web-vue3/src/stores/user.ts
@@ -27,96 +27,96 @@
  * 存储该用户对应的菜单列表，用于渲染侧边栏菜单
  * 同样提供与之对应的更新功能
  */
-import { TOKEN_KEY, USER_INFO_KEY, MENU_KEY, LONG_TERM_TOKEN } from "@/utils/const";
+import { TOKEN_KEY, USER_INFO_KEY, MENU_KEY, LONG_TERM_TOKEN } from '@/utils/const'
 
-import { getUserInfo, loginOut } from "@/api/user/user";
+import { getUserInfo, loginOut } from '@/api/user/user'
 
 const user = {
-	state: {
-		token: localStorage.getItem(TOKEN_KEY),
-		longTermToken: localStorage.getItem(LONG_TERM_TOKEN),
-		userInfo: JSON.parse(localStorage.getItem(USER_INFO_KEY)!) || {},
-		reloadUserInfo: false,
-	},
-	// mutations: {
-	// 	setToken(state, data) {
-	// 		state.token = data.token || "";
-	// 		state.longTermToken = data.longTermToken || "";
-	// 		if (state.token) {
-	// 			localStorage.setItem(TOKEN_KEY, data.token);
-	// 		} else {
-	// 			localStorage.removeItem(TOKEN_KEY);
-	// 		}
-	// 		if (state.longTermToken) {
-	// 			localStorage.setItem(LONG_TERM_TOKEN, data.longTermToken);
-	// 		} else {
-	// 			localStorage.removeItem(LONG_TERM_TOKEN);
-	// 		}
-	// 	},
-	// 	setUserInfo(state, userInfo) {
-	// 		state.userInfo = userInfo;
-	// 	},
-	// },
-	actions: {
-		refreshUserInfo() {
-			// 加载用户信息
-			return new Promise((resolve) => {
-				getUserInfo().then((res) => {
-					if (res.code === 200) {
-						 this.userInfo= res.data;
-						localStorage.setItem(USER_INFO_KEY, JSON.stringify(res.data));
-						resolve(true);
-					}
-				});
-			});
-		},
-		// 页面刷新 加载用户信息
-		pageReloadRefreshUserInfo({ dispatch, state }) {
-			if (state.reloadUserInfo) {
-				return;
-			}
-			dispatch("refreshUserInfo").then(() => {
-				state.reloadUserInfo = true;
-			});
-		},
-		// 登录 data = {token: 'xxx', userName: 'name'}
-		login({ dispatch, commit }, data:any) {
-			return new Promise((resolve) => {
-				commit("setToken", data);
-				//commit('setUserName', data.userName);
-				dispatch("refreshUserInfo");
-				// 加载系统菜单 这里需要等待 action 执行完毕返回 promise, 否则第一次登录可能会从 store 里面获取不到 menus 数据而报错
-				dispatch("loadSystemMenus").then(() => {
-					resolve(true);
-				});
-			});
-		},
-		// 退出登录 移除对应的 store
-		logOut({ dispatch, commit }) {
-			return new Promise((resolve) => {
-				commit("setToken", {});
-				commit("setMenus", "");
+  state: {
+    token: localStorage.getItem(TOKEN_KEY),
+    longTermToken: localStorage.getItem(LONG_TERM_TOKEN),
+    userInfo: JSON.parse(localStorage.getItem(USER_INFO_KEY)!) || {},
+    reloadUserInfo: false,
+  },
+  // mutations: {
+  // 	setToken(state, data) {
+  // 		state.token = data.token || "";
+  // 		state.longTermToken = data.longTermToken || "";
+  // 		if (state.token) {
+  // 			localStorage.setItem(TOKEN_KEY, data.token);
+  // 		} else {
+  // 			localStorage.removeItem(TOKEN_KEY);
+  // 		}
+  // 		if (state.longTermToken) {
+  // 			localStorage.setItem(LONG_TERM_TOKEN, data.longTermToken);
+  // 		} else {
+  // 			localStorage.removeItem(LONG_TERM_TOKEN);
+  // 		}
+  // 	},
+  // 	setUserInfo(state, userInfo) {
+  // 		state.userInfo = userInfo;
+  // 	},
+  // },
+  actions: {
+    refreshUserInfo() {
+      // 加载用户信息
+      return new Promise((resolve) => {
+        getUserInfo().then((res) => {
+          if (res.code === 200) {
+            this.userInfo = res.data
+            localStorage.setItem(USER_INFO_KEY, JSON.stringify(res.data))
+            resolve(true)
+          }
+        })
+      })
+    },
+    // 页面刷新 加载用户信息
+    pageReloadRefreshUserInfo({ dispatch, state }) {
+      if (state.reloadUserInfo) {
+        return
+      }
+      dispatch('refreshUserInfo').then(() => {
+        state.reloadUserInfo = true
+      })
+    },
+    // 登录 data = {token: 'xxx', userName: 'name'}
+    login({ dispatch, commit }, data: any) {
+      return new Promise((resolve) => {
+        commit('setToken', data)
+        //commit('setUserName', data.userName);
+        dispatch('refreshUserInfo')
+        // 加载系统菜单 这里需要等待 action 执行完毕返回 promise, 否则第一次登录可能会从 store 里面获取不到 menus 数据而报错
+        dispatch('loadSystemMenus').then(() => {
+          resolve(true)
+        })
+      })
+    },
+    // 退出登录 移除对应的 store
+    logOut({ dispatch, commit }) {
+      return new Promise((resolve) => {
+        commit('setToken', {})
+        commit('setMenus', '')
 
-				localStorage.removeItem(MENU_KEY);
-				// 调用其他 action
-				dispatch("clearTabs", { key: "all" });
-				loginOut({}).then(() => {
-					resolve(true);
-				});
-			});
-		},
-	},
-	getters: {
-		getToken(state) {
-			return state.token;
-		},
-		getLongTermToken(state) {
-			return state.longTermToken;
-		},
-		getUserInfo(state) {
-			return state.userInfo;
-		},
-	},
-};
+        localStorage.removeItem(MENU_KEY)
+        // 调用其他 action
+        dispatch('clearTabs', { key: 'all' })
+        loginOut({}).then(() => {
+          resolve(true)
+        })
+      })
+    },
+  },
+  getters: {
+    getToken(state) {
+      return state.token
+    },
+    getLongTermToken(state) {
+      return state.longTermToken
+    },
+    getUserInfo(state) {
+      return state.userInfo
+    },
+  },
+}
 
-export default user;
+export default user
