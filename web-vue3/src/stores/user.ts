830c61d13bc2e21628fc634dd04a9961406df50a
bwcx_jzy <bwcx_jzy@163.com>
2023-04-08 15:21:44 +0800

    js rename ts

--- web-vue3/src/stores/user.ts
+++ web-vue3/src/stores/user.ts
@@ -35,37 +35,37 @@ const user = {
 	state: {
 		token: localStorage.getItem(TOKEN_KEY),
 		longTermToken: localStorage.getItem(LONG_TERM_TOKEN),
-		userInfo: JSON.parse(localStorage.getItem(USER_INFO_KEY)) || {},
+		userInfo: JSON.parse(localStorage.getItem(USER_INFO_KEY)!) || {},
 		reloadUserInfo: false,
 	},
-	mutations: {
-		setToken(state, data) {
-			state.token = data.token || "";
-			state.longTermToken = data.longTermToken || "";
-			if (state.token) {
-				localStorage.setItem(TOKEN_KEY, data.token);
-			} else {
-				localStorage.removeItem(TOKEN_KEY);
-			}
-			if (state.longTermToken) {
-				localStorage.setItem(LONG_TERM_TOKEN, data.longTermToken);
-			} else {
-				localStorage.removeItem(LONG_TERM_TOKEN);
-			}
-		},
-		setUserInfo(state, userInfo) {
-			state.userInfo = userInfo;
-		},
-	},
+	// mutations: {
+	// 	setToken(state, data) {
+	// 		state.token = data.token || "";
+	// 		state.longTermToken = data.longTermToken || "";
+	// 		if (state.token) {
+	// 			localStorage.setItem(TOKEN_KEY, data.token);
+	// 		} else {
+	// 			localStorage.removeItem(TOKEN_KEY);
+	// 		}
+	// 		if (state.longTermToken) {
+	// 			localStorage.setItem(LONG_TERM_TOKEN, data.longTermToken);
+	// 		} else {
+	// 			localStorage.removeItem(LONG_TERM_TOKEN);
+	// 		}
+	// 	},
+	// 	setUserInfo(state, userInfo) {
+	// 		state.userInfo = userInfo;
+	// 	},
+	// },
 	actions: {
-		refreshUserInfo({ commit }) {
+		refreshUserInfo() {
 			// 加载用户信息
 			return new Promise((resolve) => {
 				getUserInfo().then((res) => {
 					if (res.code === 200) {
-						commit("setUserInfo", res.data);
+						 this.userInfo= res.data;
 						localStorage.setItem(USER_INFO_KEY, JSON.stringify(res.data));
-						resolve();
+						resolve(true);
 					}
 				});
 			});
@@ -80,14 +80,14 @@ const user = {
 			});
 		},
 		// 登录 data = {token: 'xxx', userName: 'name'}
-		login({ dispatch, commit }, data) {
+		login({ dispatch, commit }, data:any) {
 			return new Promise((resolve) => {
 				commit("setToken", data);
 				//commit('setUserName', data.userName);
 				dispatch("refreshUserInfo");
 				// 加载系统菜单 这里需要等待 action 执行完毕返回 promise, 否则第一次登录可能会从 store 里面获取不到 menus 数据而报错
 				dispatch("loadSystemMenus").then(() => {
-					resolve();
+					resolve(true);
 				});
 			});
 		},
@@ -101,7 +101,7 @@ const user = {
 				// 调用其他 action
 				dispatch("clearTabs", { key: "all" });
 				loginOut({}).then(() => {
-					resolve();
+					resolve(true);
 				});
 			});
 		},
