ff93dc3471fc407e3161c0e96f8f1f86be0a884d
bwcx_jzy <bwcx_jzy@163.com>
2023-04-11 19:08:29 +0800

    fix

--- modules/sub-plugin/git-clone/src/main/java/org/dromara/jpom/plugin/GitProcessFactory.java
+++ modules/sub-plugin/git-clone/src/main/java/org/dromara/jpom/plugin/GitProcessFactory.java
@@ -1,5 +1,11 @@
 package org.dromara.jpom.plugin;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.system.OsInfo;
+import cn.hutool.system.SystemUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.jpom.util.CommandUtil;
+
 import java.util.Map;
 
 /**
@@ -9,17 +15,61 @@ import java.util.Map;
  *
  * @author Hong
  **/
+@Slf4j
 public class GitProcessFactory {
+
+    private static final String WIN_EXISTS_GIT = "where git";
+    private static final String LINUX_EXISTS_GIT = "which git";
+
+    private static Boolean result;
 
     private static final String DEFAULT_GIT_PROCESS = "JGit";
     private static final String SYSTEM_GIT_PROCESS = "SystemGit";
 
     public static GitProcess get(Map<String, Object> parameter, IWorkspaceEnvPlugin workspaceEnvPlugin) {
         String processType = (String) parameter.getOrDefault("gitProcessType", DEFAULT_GIT_PROCESS);
-        if (SYSTEM_GIT_PROCESS.equalsIgnoreCase(processType) && GitEnv.existsSystemGit()) {
+        if (SYSTEM_GIT_PROCESS.equalsIgnoreCase(processType) && GitProcessFactory.existsSystemGit()) {
             return new SystemGitProcess(workspaceEnvPlugin, parameter);
         } else {
             return new JGitProcess(workspaceEnvPlugin, parameter);
         }
     }
+
+
+    /**
+     * 操作系统是否有GIT环境
+     */
+    public static boolean existsSystemGit() {
+        if (result == null) {
+            result = existsSystemGit2();
+        }
+        return result;
+    }
+
+    /**
+     * 操作系统是否有GIT环境
+     */
+    private static boolean existsSystemGit2() {
+        String result;
+        OsInfo osInfo = SystemUtil.getOsInfo();
+        if (osInfo.isWindows()) {
+            result = CommandUtil.execSystemCommand(WIN_EXISTS_GIT);
+            if (StrUtil.contains(result, ".exe")) {
+                log.info("git安装位置：{}", result);
+                return true;
+            }
+        } else if (osInfo.isLinux() || osInfo.isMac()) {
+            result = CommandUtil.execSystemCommand(LINUX_EXISTS_GIT);
+            if (StrUtil.containsAny(result, "no git", "not found")) {
+                return false;
+            }
+            log.info("git安装位置：{}", result);
+            return true;
+        } else {
+            log.warn("不支持的系统类型：{}", osInfo.getName());
+            return false;
+        }
+        return false;
+    }
+
 }
