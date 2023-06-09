cb6503511a492d671749a222271e374a7621e16d
bwcx_jzy <bwcx_jzy@163.com>
2023-06-09 10:35:27 +0800

    fix 项目列表可以查看项目日志（避免控制台卡顿无法操作下载日志）

--- web-vue/src/pages/node/node-layout/project/project-console.vue
+++ web-vue/src/pages/node/node-layout/project/project-console.vue
@@ -25,62 +25,50 @@
 
           <a-dropdown>
             <!-- <a type="link" class="ant-dropdown-link"> 更多<a-icon type="down" /> </a> -->
-            <a class="ant-dropdown-link" @click="(e) => e.preventDefault()">
+            <a
+              class="ant-dropdown-link"
+              @click="
+                (e) => {
+                  e.preventDefault();
+                  handleLogBack();
+                }
+              "
+            >
               <a-tag>
                 文件大小: {{ project.logSize || "-" }}
                 <!-- 更多 -->
-                <a-icon type="down" />
+                <a-icon type="fullscreen" />
               </a-tag>
             </a>
-            <a-menu slot="overlay">
+            <!-- <a-menu slot="overlay">
               <a-menu-item>
                 <a-button type="primary" size="small" :disabled="!project.logSize" @click="handleDownload">导出日志</a-button>
               </a-menu-item>
               <a-menu-item>
                 <a-button type="primary" size="small" @click="handleLogBack">备份列表</a-button>
               </a-menu-item>
-            </a-menu>
+            </a-menu> -->
           </a-dropdown>
         </a-space>
       </template>
     </log-view>
     <!-- 日志备份 -->
     <a-modal destroyOnClose v-model="lobbackVisible" title="日志备份列表" width="850px" :footer="null" :maskClosable="false">
-      <div ref="model-filter" class="filter">
-        <a-space direction="vertical">
-          <a-tag>控制台日志只是启动项目输出的日志信息,并非项目日志。可以关闭控制台日志备份功能：<b>jpom.project.log.auto-backup-to-file: false</b></a-tag>
-
-          <a-tag color="orange">控制台日志路径: {{ project.log }}</a-tag>
-          <a-tag color="orange">控制台日志备份路径: {{ project.logBack }}</a-tag>
-        </a-space>
-      </div>
-      <!-- 数据表格 -->
-      <a-table :data-source="logBackList" :loading="loading" :columns="columns" :scroll="{ y: 400 }" :pagination="false" bordered :rowKey="(record, index) => index">
-        <a-tooltip slot="filename" slot-scope="text" placement="topLeft" :title="text">
-          <span>{{ text }}</span>
-        </a-tooltip>
-        <a-tooltip slot="fileSize" slot-scope="text" placement="topLeft" :title="text">
-          <span>{{ text }}</span>
-        </a-tooltip>
-        <template slot="operation" slot-scope="text, record">
-          <a-space>
-            <a-button type="primary" @click="handleDownloadLogback(record)">下载</a-button>
-            <a-button type="danger" @click="handleDelete(record)">删除</a-button>
-          </a-space>
-        </template>
-      </a-table>
+      <ProjectLog v-if="lobbackVisible" :nodeId="this.nodeId" :copyId="this.copyId" :projectId="this.projectId"></ProjectLog>
     </a-modal>
   </div>
 </template>
 <script>
-import { deleteProjectLogBackFile, downloadProjectLogBackFile, downloadProjectLogFile, getLogBackList, getProjectData, getProjectLogSize } from "@/api/node-project";
+import { getProjectData, getProjectLogSize } from "@/api/node-project";
 import { mapGetters } from "vuex";
 import { getWebSocketUrl } from "@/utils/const";
 import LogView from "@/components/logView";
+import ProjectLog from "./project-log.vue";
 
 export default {
   components: {
     LogView,
+    ProjectLog,
   },
   props: {
     nodeId: {
@@ -104,13 +92,7 @@ export default {
       socket: null,
       logExist: false,
       lobbackVisible: false,
-      logBackList: [],
-      columns: [
-        { title: "文件名称", dataIndex: "filename", width: 150, ellipsis: true, scopedSlots: { customRender: "filename" } },
-        { title: "修改时间", dataIndex: "modifyTime", width: 150, ellipsis: true, scopedSlots: { customRender: "modifyTime" } },
-        { title: "文件大小", dataIndex: "fileSize", width: 100, ellipsis: true, scopedSlots: { customRender: "fileSize" } },
-        { title: "操作", dataIndex: "operation", scopedSlots: { customRender: "operation" }, width: 130 },
-      ],
+
       heart: null,
     };
   },
@@ -281,100 +263,17 @@ export default {
         },
       });
     },
-    // 下载日志文件
-    handleDownload() {
-      this.$notification.info({
-        message: "正在下载，请稍等...",
-      });
-      // 请求参数
-      const params = {
-        nodeId: this.nodeId,
-        id: this.projectId,
-        copyId: this.copyId,
-      };
-      // 请求接口拿到 blob
-      window.open(downloadProjectLogFile(params), "_blank");
-    },
+
     // 日志备份列表
     handleLogBack() {
-      this.loading = true;
       // 设置显示的数据
-      this.detailData = [];
+      // this.detailData = [];
       this.lobbackVisible = true;
-      const params = {
-        nodeId: this.nodeId,
-        id: this.projectId,
-        copyId: this.copyId,
-      };
-      getLogBackList(params).then((res) => {
-        if (res.code === 200) {
-          this.logBackList = res.data.array;
-        }
-        this.loading = false;
-      });
     },
-    // 下载日志备份文件
-    handleDownloadLogback(record) {
-      this.$notification.info({
-        message: "正在下载，请稍等...",
-      });
-      // 请求参数
-      const params = {
-        nodeId: this.nodeId,
-        id: this.projectId,
-        copyId: this.copyId,
-        key: record.filename,
-      };
-      // 请求接口拿到 blob
-      window.open(downloadProjectLogBackFile(params), "_blank");
-    },
-    // 删除日志备份文件
-    handleDelete(record) {
-      this.$confirm({
-        title: "系统提示",
-        content: "真的要删除文件么？",
-        okText: "确认",
-        cancelText: "取消",
-        onOk: () => {
-          // 请求参数
-          const params = {
-            nodeId: this.nodeId,
-            id: this.projectId,
-            copyId: this.copyId,
-            name: record.filename,
-          };
-          // 删除
-          deleteProjectLogBackFile(params).then((res) => {
-            if (res.code === 200) {
-              this.$notification.success({
-                message: res.msg,
-              });
-              this.handleLogBack();
-            }
-          });
-        },
-      });
-    },
+
     goFile() {
       this.$emit("goFile");
     },
   },
 };
 </script>
-<style scoped>
-/* .filter {
-  margin: 0 0 10px;
-} */
-/*
-.console {
-  padding: 5px;
-  color: #fff;
-  font-size: 14px;
-  background-color: black;
-  width: 100%;
-  height: calc(100vh - 120px);
-  overflow-y: auto;
-  border: 1px solid #e2e2e2;
-  border-radius: 5px 5px;
-} */
-</style>
