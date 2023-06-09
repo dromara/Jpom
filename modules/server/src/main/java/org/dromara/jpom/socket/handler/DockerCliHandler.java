11f21ea9e1ac6b5f7e9eb1fbcbdca36803566950
bwcx_jzy <bwcx_jzy@163.com>
2023-06-09 09:04:01 +0800

    fix docker 终端页面缓冲区大小自动适应

--- modules/server/src/main/java/org/dromara/jpom/socket/handler/DockerCliHandler.java
+++ modules/server/src/main/java/org/dromara/jpom/socket/handler/DockerCliHandler.java
@@ -22,6 +22,7 @@
  */
 package org.dromara.jpom.socket.handler;
 
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.CharsetUtil;
@@ -47,6 +48,7 @@ import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
@@ -107,31 +109,26 @@ public class DockerCliHandler extends BaseTerminalHandler {
             }
             if (StrUtil.equals(data, "resize")) {
                 // 缓存区大小
-                //handlerItem.resize(jsonObject);
+                handlerItem.resize(jsonObject);
                 return;
             }
         }
         try {
-            this.sendCommand(handlerItem, payload);
+            handlerItem.sendCommand(payload);
         } catch (Exception e) {
             sendBinary(session, "Failure:" + e.getMessage());
             log.error("执行命令异常", e);
         }
     }
 
-    private void sendCommand(HandlerItem handlerItem, String data) throws Exception {
-        handlerItem.outputStream.write(data.getBytes());
-        handlerItem.outputStream.flush();
-    }
-
-
     private class HandlerItem implements Runnable, AutoCloseable {
         private final WebSocketSession session;
         private final MachineDockerModel dockerInfoModel;
         private final Map<String, Object> map;
-        private final PipedInputStream inputStream = new PipedInputStream();
-        private final PipedOutputStream outputStream = new PipedOutputStream(inputStream);
-        private final String containerId;
+        private PipedInputStream inputStream = new PipedInputStream();
+        private PipedOutputStream outputStream = new PipedOutputStream(inputStream);
+        private String containerId;
+        private Thread thread;
 
         HandlerItem(WebSocketSession session, MachineDockerModel dockerInfoModel, Map<String, Object> map, String containerId) throws IOException {
             this.session = session;
@@ -144,14 +141,45 @@ public class DockerCliHandler extends BaseTerminalHandler {
             ThreadUtil.execute(this);
         }
 
+        private void sendCommand(String data) throws Exception {
+            if (this.outputStream == null) {
+                return;
+            }
+            this.outputStream.write(data.getBytes());
+            this.outputStream.flush();
+        }
+
+        /**
+         * 调整 缓存区大小
+         *
+         * @param jsonObject 参数
+         */
+        private void resize(JSONObject jsonObject) {
+            Integer rows = Convert.toInt(jsonObject.getString("rows"), 10);
+            Integer cols = Convert.toInt(jsonObject.getString("cols"), 10);
+            Integer wp = Convert.toInt(jsonObject.getString("wp"), 10);
+            Integer hp = Convert.toInt(jsonObject.getString("hp"), 10);
+            map.put("sizeHeight", rows);
+            map.put("sizeWidth", cols);
+            IPlugin plugin = PluginFactory.getPlugin(DockerInfoService.DOCKER_PLUGIN_NAME);
+            try {
+                plugin.execute("resizeExec", map);
+            } catch (Exception e) {
+                log.error("执行容器命令异常", e);
+                sendBinary(session, "执行异常:" + e.getMessage());
+            }
+        }
+
         @Override
         public void run() {
             map.put("containerId", containerId);
+            thread = Thread.currentThread();
             Consumer<String> logConsumer = s -> {
                 if (StrUtil.startWith(s, "CALLBACK_EXECID:")) {
                     // 终端id
                     String execId = StrUtil.removePrefix(s, "CALLBACK_EXECID:");
                     session.getAttributes().put("execId", execId);
+                    map.put("execId", execId);
                     return;
                 }
                 sendBinary(session, s);
@@ -174,34 +202,62 @@ public class DockerCliHandler extends BaseTerminalHandler {
                 log.error("执行容器命令异常", e);
                 sendBinary(session, "执行异常:" + e.getMessage());
             }
+            log.debug("[{}] docker exec 终端进程结束", dockerInfoModel.getName());
+            // 标记自动结束
+            this.containerId = null;
+        }
+
+        private void tryExit() throws Exception {
+            if (this.containerId == null) {
+                // 如果线程已经结束，不再尝试发送消息
+                return;
+            }
+            // ctrl + c
+            this.sendCommand(String.valueOf((char) 3));
+            ThreadUtil.sleep(100);
+            // ctrl + c
+            this.sendCommand(String.valueOf((char) 3));
+            ThreadUtil.sleep(100);
+            // quit
+            this.sendCommand("quit");
+            this.sendCommand(String.valueOf((char) 13));
+            ThreadUtil.sleep(100);
+            // exit
+            this.sendCommand("exit");
+            this.sendCommand(String.valueOf((char) 13));
+            ThreadUtil.sleep(100);
         }
 
         @Override
-        public void close() throws Exception {
-            Object execId = session.getAttributes().get("execId");
-            if (execId == null) {
+        public void close() {
+            if (this.inputStream == null) {
+                // 避免多次调用
                 return;
             }
-            IPlugin plugin = PluginFactory.getPlugin(DockerInfoService.DOCKER_PLUGIN_NAME);
-            map.put("execId", execId.toString());
+            Object execId = session.getAttributes().get("execId");
             try {
-                plugin.execute("inspectExec", map);
+                // 多次尝试退出，可能终端内部进入交互命令行
+                for (int i = 0; i < 3; i++) {
+                    this.tryExit();
+                }
+                //
+                Optional.ofNullable(this.thread).ifPresent(Thread::interrupt);
             } catch (Exception e) {
                 log.error("执行容器命令异常", e);
             }
+            log.debug("关闭[{}] docker exec 终端：{}", dockerInfoModel.getName(), execId);
+            IoUtil.close(this.inputStream);
+            IoUtil.close(this.outputStream);
+            this.inputStream = null;
+            this.outputStream = null;
         }
     }
 
     @Override
     public void destroy(WebSocketSession session) {
-        HandlerItem handlerItem = HANDLER_ITEM_CONCURRENT_HASH_MAP.get(session.getId());
-        if (handlerItem != null) {
-            IoUtil.close(handlerItem.inputStream);
-            IoUtil.close(handlerItem.outputStream);
-        }
+        HandlerItem handlerItem = HANDLER_ITEM_CONCURRENT_HASH_MAP.remove(session.getId());
         IoUtil.close(handlerItem);
         IoUtil.close(session);
-        HANDLER_ITEM_CONCURRENT_HASH_MAP.remove(session.getId());
         SocketSessionUtil.close(session);
     }
 }
