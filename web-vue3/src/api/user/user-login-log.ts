d8de3d42f297a5d0090b92d732a0baf4549ac50f
miracle <1772176802@qq.com>
2023-07-10 12:24:28 +0800

    修改用户日志

--- web-vue3/src/api/user/user-login-log.ts
+++ web-vue3/src/api/user/user-login-log.ts
@@ -1,6 +1,6 @@
 import axios from "../config";
 
-export function userLoginLgin(params:any) {
+export function userLoginLgin(params: any) {
   return axios({
     url: "/user/login-log/list-data",
     method: "post",
@@ -8,12 +8,4 @@ export function userLoginLgin(params:any) {
   });
 }
 
-export const operateCodeMap = {
-  0: "正常登录",
-  1: "密码错误",
-  2: "账号被锁定",
-  3: "自动续期",
-  4: "账号被禁用",
-  5: "登录成功,需要验证 MFA",
-  6: "oauth2",
-};
+export const operateCodeMap = ["正常登录", "密码错误", "账号被锁定", "自动续期", "账号被禁用", "登录成功,需要验证 MFA", "oauth2"];
