b9246cf5254a2ec0f918d6907770e31da38b7a38
黄纲 <Gang.Huang@cisdi.com.cn>
2023-08-27 23:25:06 +0800

    feat: 1、git-clone插件添加git submodules项目支持 2、发布管理-构建环境新增构建产物目录变量，用于发布阶段使用

--- modules/sub-plugin/git-clone/src/main/java/org/dromara/jpom/plugin/SystemGitProcess.java
+++ modules/sub-plugin/git-clone/src/main/java/org/dromara/jpom/plugin/SystemGitProcess.java
@@ -160,10 +160,16 @@ public class SystemGitProcess extends AbstractGitProcess {
         File saveFile = getSaveFile();
         {
             // 更新
+            printWriter.println("git pull");
             CommandUtil.exec(saveFile, null, line -> {
                 printWriter.println(line);
                 printWriter.flush();
             }, "git", "pull");
+            printWriter.println("git submodule update --init --remote -f --recursive");
+			CommandUtil.exec(saveFile, null, line -> {
+				printWriter.println(line);
+				printWriter.flush();
+			}, "git", "submodule", "update", "--init", "--remote", "-f", "--recursive");
         }
         // 获取提交日志
         String[] command = {"git", "log", "-1", branchOrTag};
@@ -181,7 +187,7 @@ public class SystemGitProcess extends AbstractGitProcess {
     }
 
     private void reClone(PrintWriter printWriter, String branchOrTag) throws IOException {
-        printWriter.println("Automatically re-clones repositories");
+        printWriter.println("SystemGit: Automatically re-clones repositories");
         // 先删除本地目录
         File savePath = getSaveFile();
         if (!FileUtil.clean(savePath)) {
@@ -205,7 +211,7 @@ public class SystemGitProcess extends AbstractGitProcess {
                 return null;
             }).ifPresent(integer -> env.put("GIT_HTTP_TIMEOUT", String.valueOf(integer)));
         //
-        String[] command = new String[]{"git", "clone", depthStr, "-b", branchOrTag, this.getCovertUrl(), savePath.getAbsolutePath()};
+        String[] command = new String[]{"git", "clone", "--recursive", depthStr, "-b", branchOrTag, this.getCovertUrl(), savePath.getAbsolutePath()};
         FileUtil.mkdir(savePath);
         CommandUtil.exec(savePath, env, line -> {
             printWriter.println(line);
