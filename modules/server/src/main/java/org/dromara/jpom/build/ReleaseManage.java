5fd8127e72ee40221e006b17d8b03bdfab8f9f62
bwcx_jzy <bwcx_jzy@163.com>
2023-09-15 20:25:04 +0800

    fix 容器构建判断构建异常（严格模式异常中断构建）

--- modules/server/src/main/java/org/dromara/jpom/build/ReleaseManage.java
+++ modules/server/src/main/java/org/dromara/jpom/build/ReleaseManage.java
@@ -210,34 +210,36 @@ public class ReleaseManage {
             return dockerTag;
         }
         List<String> list = StrUtil.splitTrim(dockerTag, StrUtil.COMMA);
-        return list.stream().map(s -> {
-            List<String> tag = StrUtil.splitTrim(s, StrUtil.COLON);
-            String version = CollUtil.getLast(tag);
-            List<String> versionList = StrUtil.splitTrim(version, StrUtil.DOT);
-            int tagSize = CollUtil.size(tag);
-            if (tagSize <= 1 || CollUtil.size(versionList) <= 1) {
-                logRecorder.systemWarning("version number incrementing error, no match for . or :");
-                return s;
-            }
-            boolean match = false;
-            for (int i = versionList.size() - 1; i >= 0; i--) {
-                String versionParting = versionList.get(i);
-                int versionPartingInt = Convert.toInt(versionParting, Integer.MIN_VALUE);
-                if (versionPartingInt != Integer.MIN_VALUE) {
-                    versionList.set(i, this.buildNumberId + StrUtil.EMPTY);
-                    match = true;
-                    break;
+        return list.stream()
+            .map(s -> {
+                List<String> tag = StrUtil.splitTrim(s, StrUtil.COLON);
+                String version = CollUtil.getLast(tag);
+                List<String> versionList = StrUtil.splitTrim(version, StrUtil.DOT);
+                int tagSize = CollUtil.size(tag);
+                if (tagSize <= 1 || CollUtil.size(versionList) <= 1) {
+                    logRecorder.systemWarning("version number incrementing error, no match for . or :");
+                    return s;
                 }
-            }
-            tag.set(tagSize - 1, CollUtil.join(versionList, StrUtil.DOT));
-            String newVersion = CollUtil.join(tag, StrUtil.COLON);
-            if (match) {
-                logRecorder.system("dockerTag version number incrementing {} -> {}", s, newVersion);
-            } else {
-                logRecorder.systemWarning("version number incrementing error,No numeric version number {} ", s);
-            }
-            return newVersion;
-        }).collect(Collectors.joining(StrUtil.COMMA));
+                boolean match = false;
+                for (int i = versionList.size() - 1; i >= 0; i--) {
+                    String versionParting = versionList.get(i);
+                    int versionPartingInt = Convert.toInt(versionParting, Integer.MIN_VALUE);
+                    if (versionPartingInt != Integer.MIN_VALUE) {
+                        versionList.set(i, this.buildNumberId + StrUtil.EMPTY);
+                        match = true;
+                        break;
+                    }
+                }
+                tag.set(tagSize - 1, CollUtil.join(versionList, StrUtil.DOT));
+                String newVersion = CollUtil.join(tag, StrUtil.COLON);
+                if (match) {
+                    logRecorder.system("dockerTag version number incrementing {} -> {}", s, newVersion);
+                } else {
+                    logRecorder.systemWarning("version number incrementing error,No numeric version number {} ", s);
+                }
+                return newVersion;
+            })
+            .collect(Collectors.joining(StrUtil.COMMA));
     }
 
     private String doDockerImage() {
@@ -279,7 +281,13 @@ public class ReleaseManage {
             }
             //String dockerBuildArgs = this.buildExtraModule.getDockerBuildArgs();
             for (DockerInfoModel infoModel : dockerInfoModels) {
-                this.doDockerImage(infoModel, envMap, dockerfile, baseDir, dockerTag, this.buildExtraModule);
+                boolean done = this.doDockerImage(infoModel, envMap, dockerfile, baseDir, dockerTag, this.buildExtraModule);
+                if (!done) {
+                    logRecorder.systemWarning("容器构建异常：{} -> {}", infoModel.getName(), dockerTag);
+                    if (buildExtraModule.strictlyEnforce()) {
+                        return "严格模式下镜像构建失败,终止任务";
+                    }
+                }
             }
             // 推送 - 只选择一个 docker 服务来推送到远程仓库
             Boolean pushToRepository = this.buildExtraModule.getPushToRepository();
@@ -326,7 +334,7 @@ public class ReleaseManage {
         }
     }
 
-    private void doDockerImage(DockerInfoModel dockerInfoModel, Map<String, String> envMap, File dockerfile, File baseDir, String dockerTag, BuildExtraModule extraModule) {
+    private boolean doDockerImage(DockerInfoModel dockerInfoModel, Map<String, String> envMap, File dockerfile, File baseDir, String dockerTag, BuildExtraModule extraModule) {
         logRecorder.system("{} start build image {}{}", dockerInfoModel.getName(), dockerTag, System.lineSeparator());
         Map<String, Object> map = machineDockerServer.dockerParameter(dockerInfoModel);
         //.toParameter();
@@ -343,9 +351,10 @@ public class ReleaseManage {
         map.put("logConsumer", logConsumer);
         IPlugin plugin = PluginFactory.getPlugin(DockerInfoService.DOCKER_PLUGIN_NAME);
         try {
-            plugin.execute("buildImage", map);
+            return (boolean) plugin.execute("buildImage", map);
         } catch (Exception e) {
-            logRecorder.error("构建镜像调用容器异常", e);
+            logRecorder.systemError("构建镜像调用容器异常", e);
+            return false;
         }
     }
 
