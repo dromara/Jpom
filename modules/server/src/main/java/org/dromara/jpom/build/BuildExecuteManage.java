62084cae4d7d81253ec6c18c8da951d92227855c
bwcx_jzy <bwcx_jzy@163.com>
2023-09-18 15:38:59 +0800

    fix SSH 独立管理面板支持快捷使用文件管理

--- modules/server/src/main/java/org/dromara/jpom/build/BuildExecuteManage.java
+++ modules/server/src/main/java/org/dromara/jpom/build/BuildExecuteManage.java
@@ -818,6 +818,7 @@ public class BuildExecuteManage implements Runnable {
         }
 
         try {
+            boolean stop = false;
             for (Map.Entry<String, IProcessItem> stringSupplierEntry : processItemMap.entrySet()) {
                 processName = stringSupplierEntry.getKey();
                 IProcessItem processItem = stringSupplierEntry.getValue();
@@ -830,6 +831,7 @@ public class BuildExecuteManage implements Runnable {
                     logRecorder.system("执行中断 {} 流程,原因事件脚本中断", processItem.name());
                     this.asyncWebHooks("stop", "process", processName, "statusMsg", interruptMsg);
                     buildExecuteService.updateStatus(buildInfoModel.getId(), this.logId, buildInfoModel.getBuildId(), BuildStatus.Interrupt, interruptMsg);
+                    stop = true;
                     break;
                 }
                 String errorMsg = processItem.execute();
@@ -838,11 +840,14 @@ public class BuildExecuteManage implements Runnable {
                     logRecorder.systemError("执行异常[{}]流程：{}", processItem.name(), errorMsg);
                     this.asyncWebHooks("stop", "process", processName, "statusMsg", errorMsg);
                     buildExecuteService.updateStatus(buildInfoModel.getId(), this.logId, buildInfoModel.getBuildId(), BuildStatus.Error, errorMsg);
+                    stop = true;
                     break;
                 }
                 logRecorder.system("执行结束 {}流程,耗时：{}", processItem.name(), DateUtil.formatBetween(SystemClock.now() - processItemStartTime));
             }
-            this.asyncWebHooks("success");
+            if (!stop) { // 没有执行 stop
+                this.asyncWebHooks("success");
+            }
         } catch (DiyInterruptException diyInterruptException) {
             // 主动中断
             this.asyncWebHooks("stop", "process", processName);
