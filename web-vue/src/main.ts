ac69202620403fde4d346513e2a2100628375e9d
bwcx_jzy <bwcx_jzy@163.com>
2024-01-16 17:18:26 +0800

    fix

--- web-vue/src/main.ts
+++ web-vue/src/main.ts
@@ -1,7 +1,6 @@
 // import '@/assets/style.css'
 // import '@/assets/reset.less'
 import App from './App.vue'
-import Antd from 'ant-design-vue'
 import router from './router'
 // import 'ant-design-vue/dist/antd.css'
 import '@/router/auth'
@@ -9,6 +8,6 @@ const pinia = createPinia()
 
 const app = createApp(App)
 
-app.use(Antd).use(router).use(pinia)
+app.use(router).use(pinia)
 
 app.mount('#app')
