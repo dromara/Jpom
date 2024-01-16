3ac37b227b2704674aa91adc9b0e8b35a8f3f382
bwcx_jzy <bwcx_jzy@163.com>
2024-01-16 09:10:08 +0800

    fix

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -195,6 +195,7 @@ export default ({
           10,
           function () {
             ABDetected();
+            ABDetectedHeader();
             changeAdHideEvent();
           }
         );
@@ -227,6 +228,53 @@ export default ({
     }, 900);
   }
 
+  function ABDetectedHeader() {
+    // 万维广告“禁止”广告拦截
+    // function called if wwads is blocked
+    // https://github.com/bytegravity/whitelist-wwads
+
+    if (
+      location.hostname === "127.0.0.1" ||
+      location.hostname === "localhost" ||
+      location.hostname.indexOf("192.168.") > -1
+    ) {
+      // 本地环境不显示
+      return;
+    }
+    var adDivDom = document.querySelector(".adBlockDetected_div");
+    if (adDivDom) {
+      // console.log("存在");
+      return;
+    }
+    // console.log(adDivDom);
+    // console.error("_AdBlockInit");
+    var adBlockDetected_div = document.createElement("div");
+    adBlockDetected_div.className = "adBlockDetected_div";
+    document.body.appendChild(adBlockDetected_div);
+    // console.log(document.querySelector(".adBlockDetected_div"));
+    var navbar = document.querySelector(".navbar");
+    //var sidebar = document.querySelector(".sidebar");
+    navbar && (navbar.style.cssText = "transition:top 300ms;top:33px");
+    //sidebar && (sidebar.style.cssText = "transition:top 300ms;top:33px");
+    adBlockDetected_div.style.cssText =
+      "position: fixed; top: 0; left: 0; width: 100%; background: #E01E5A; color: #fff; z-index: 9999999999; font-size: 14px; text-align: center; line-height: 1.5; font-weight: bold; padding-top: 6px; padding-bottom: 6px;";
+    adBlockDetected_div.innerHTML =
+      "我们的广告服务商 <a style='color:#fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/end-user-privacy'>并不跟踪您的隐私</a>，为了支持本站的长期运营，请将我们的网站 <a style='color: #fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/whitelist-wwads'>加入广告拦截器的白名单</a>。";
+    document.getElementsByTagName("body")[0].appendChild(adBlockDetected_div);
+    // add a close button to the right side of the div
+    var adBlockDetected_close = document.createElement("div");
+    adBlockDetected_close.style.cssText =
+      "position: absolute; top: 0; right: 10px; width: 30px; height: 30px; background: #E01E5A; color: #fff; z-index: 9999999999; line-height: 30px; cursor: pointer;";
+    adBlockDetected_close.innerHTML = "×";
+    adBlockDetected_div.appendChild(adBlockDetected_close);
+    // add a click event to the close button
+    adBlockDetected_close.onclick = function () {
+      this.parentNode.parentNode.removeChild(this.parentNode);
+      navbar.style.cssText = "transition:top 300ms;top:0";
+    };
+  }
+
+  // 显示最新版本号
   function titleShowVersion() {
     var mainTitle = $("#main-title");
     if (mainTitle.length && window.JPOM_RELEASE_VERSION) {
@@ -257,6 +305,7 @@ export default ({
     }
   }
 
+  // 图片添加悬停提示
   function imgAddLayerTip() {
     loopExecute(function () {
       titleShowVersion();
--- docs/.vuepress/public/assets/js/common.js
+++ docs/.vuepress/public/assets/js/common.js
@@ -77,64 +77,6 @@ $(function () {
   }, 20);
 });
 
-(function () {
-  // 万维广告“禁止”广告拦截
-  // function called if wwads is blocked
-  // https://github.com/bytegravity/whitelist-wwads
-  function ABDetected() {
-    if (
-      location.hostname === "127.0.0.1" ||
-      location.hostname === "localhost" ||
-      location.hostname.indexOf("192.168.") > -1
-    ) {
-      // 本地环境不显示
-      return;
-    }
-    // console.error("_AdBlockInit");
-    var adBlockDetected_div = document.createElement("div");
-    document.body.appendChild(adBlockDetected_div);
-    var navbar = document.querySelector(".navbar");
-    //var sidebar = document.querySelector(".sidebar");
-    navbar && (navbar.style.cssText = "transition:top 300ms;top:33px");
-    //sidebar && (sidebar.style.cssText = "transition:top 300ms;top:33px");
-    adBlockDetected_div.style.cssText =
-      "position: fixed; top: 0; left: 0; width: 100%; background: #E01E5A; color: #fff; z-index: 9999999999; font-size: 14px; text-align: center; line-height: 1.5; font-weight: bold; padding-top: 6px; padding-bottom: 6px;";
-    adBlockDetected_div.innerHTML =
-      "我们的广告服务商 <a style='color:#fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/end-user-privacy'>并不跟踪您的隐私</a>，为了支持本站的长期运营，请将我们的网站 <a style='color: #fff;text-decoration:underline' target='_blank' href='https://wwads.cn/page/whitelist-wwads'>加入广告拦截器的白名单</a>。";
-    document.getElementsByTagName("body")[0].appendChild(adBlockDetected_div);
-    // add a close button to the right side of the div
-    var adBlockDetected_close = document.createElement("div");
-    adBlockDetected_close.style.cssText =
-      "position: absolute; top: 0; right: 10px; width: 30px; height: 30px; background: #E01E5A; color: #fff; z-index: 9999999999; line-height: 30px; cursor: pointer;";
-    adBlockDetected_close.innerHTML = "×";
-    adBlockDetected_div.appendChild(adBlockDetected_close);
-    // add a click event to the close button
-    adBlockDetected_close.onclick = function () {
-      this.parentNode.parentNode.removeChild(this.parentNode);
-      navbar.style.cssText = "transition:top 300ms;top:0";
-    };
-  }
-
-  //check if wwads' fire function was blocked after document is ready with 3s timeout (waiting the ad loading)
-  docReady(function () {
-    loopExecute(
-      function () {
-        if (
-          window._AdBlockInit === undefined ||
-          $(".wwads-cn").children().length === 0
-        ) {
-          return false;
-        }
-        return true;
-      },
-      10,
-      function () {
-        ABDetected();
-      }
-    );
-  });
-})();
-
 function docReady(t) {
   "complete" === document.readyState || "interactive" === document.readyState
     ? setTimeout(t, 1)
