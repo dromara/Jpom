9ef5692401d861af22e68e9a6e67a728a164ee48
bwcx_jzy <bwcx_jzy@163.com>
2024-01-17 21:14:49 +0800

    fix

--- web-vue/src/pages/system/oauth-config.vue
+++ web-vue/src/pages/system/oauth-config.vue
@@ -2,36 +2,6 @@
   <div>
     <a-alert message="账号如果开启 MFA(两步验证)，使用 Oauth2 登录不会验证 MFA(两步验证)" type="info" show-icon />
     <a-tabs>
-      <a-tab-pane key="gitee" tab="Gitee Oauth2">
-        <a-form ref="editForm" :model="gitee" :rules="rules" :label-col="{ span: 4 }" :wrapper-col="{ span: 16 }">
-          <a-form-item label="是否开启" name="enabled">
-            <a-switch v-model:checked="gitee.enabled" checked-children="启用" un-checked-children="停用" />
-          </a-form-item>
-          <a-form-item label="客户端ID" name="clientId">
-            <a-input v-model:value="gitee.clientId" type="text" placeholder="请输入客户端ID [clientId]" />
-          </a-form-item>
-          <a-form-item label="客户端密钥" name="clientSecret">
-            <a-input-password v-model:value="gitee.clientSecret" placeholder="请输入客户端密钥 [clientSecret]" />
-          </a-form-item>
-          <a-form-item label="回调 url" name="redirectUri">
-            <template v-slot:help>参考地址：{{ `${this.host}/oauth2-gitee` }}</template>
-            <a-input v-model:value="gitee.redirectUri" type="text" placeholder="请输入回调重定向 url [redirectUri]" />
-          </a-form-item>
-          <!-- <a-form-item label="登录url">
-              <a-input :value="`${this.host}/oauth2-render-gitee`" type="text" />
-            </a-form-item> -->
-          <a-form-item label="自动创建用户" name="autoCreteUser">
-            <a-switch v-model:checked="gitee.autoCreteUser" checked-children="启用" un-checked-children="停用" />
-          </a-form-item>
-          <a-form-item label="忽略校验 state" name="ignoreCheckState">
-            <a-switch v-model:checked="gitee.ignoreCheckState" checked-children="忽略" un-checked-children="校验" />
-          </a-form-item>
-
-          <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
-            <a-button type="primary" class="btn" @click="onSubmit('gitee')">提交</a-button>
-          </a-form-item>
-        </a-form>
-      </a-tab-pane>
       <a-tab-pane key="maxkey" tab="MaxKey Oauth2">
         <a-form ref="editForm" :model="maxkey" :rules="rules" :label-col="{ span: 4 }" :wrapper-col="{ span: 16 }">
           <a-form-item label="是否开启" name="enabled">
@@ -75,6 +45,37 @@
           </a-form-item>
         </a-form>
       </a-tab-pane>
+      <a-tab-pane key="gitee" tab="Gitee Oauth2">
+        <a-form ref="editForm" :model="gitee" :rules="rules" :label-col="{ span: 4 }" :wrapper-col="{ span: 16 }">
+          <a-form-item label="是否开启" name="enabled">
+            <a-switch v-model:checked="gitee.enabled" checked-children="启用" un-checked-children="停用" />
+          </a-form-item>
+          <a-form-item label="客户端ID" name="clientId">
+            <a-input v-model:value="gitee.clientId" type="text" placeholder="请输入客户端ID [clientId]" />
+          </a-form-item>
+          <a-form-item label="客户端密钥" name="clientSecret">
+            <a-input-password v-model:value="gitee.clientSecret" placeholder="请输入客户端密钥 [clientSecret]" />
+          </a-form-item>
+          <a-form-item label="回调 url" name="redirectUri">
+            <template v-slot:help>参考地址：{{ `${this.host}/oauth2-gitee` }}</template>
+            <a-input v-model:value="gitee.redirectUri" type="text" placeholder="请输入回调重定向 url [redirectUri]" />
+          </a-form-item>
+          <!-- <a-form-item label="登录url">
+              <a-input :value="`${this.host}/oauth2-render-gitee`" type="text" />
+            </a-form-item> -->
+          <a-form-item label="自动创建用户" name="autoCreteUser">
+            <a-switch v-model:checked="gitee.autoCreteUser" checked-children="启用" un-checked-children="停用" />
+          </a-form-item>
+          <a-form-item label="忽略校验 state" name="ignoreCheckState">
+            <a-switch v-model:checked="gitee.ignoreCheckState" checked-children="忽略" un-checked-children="校验" />
+          </a-form-item>
+
+          <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+            <a-button type="primary" class="btn" @click="onSubmit('gitee')">提交</a-button>
+          </a-form-item>
+        </a-form>
+      </a-tab-pane>
+
       <a-tab-pane key="github" tab="Github Oauth2">
         <a-form ref="editForm" :model="github" :rules="rules" :label-col="{ span: 4 }" :wrapper-col="{ span: 16 }">
           <a-form-item label="是否开启" name="enabled">
