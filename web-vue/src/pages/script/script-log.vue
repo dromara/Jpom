cd0d48719d668907747da8b46473dad76eb894da
bwcx_jzy <bwcx_jzy@163.com>
2024-01-30 08:54:08 +0800

    fix 脚本日志时间筛选不生效

--- web-vue/src/pages/script/script-log.vue
+++ web-vue/src/pages/script/script-log.vue
@@ -61,7 +61,15 @@
             <a-select-option v-for="(val, key) in statusMap" :key="key">{{ val }}</a-select-option>
           </a-select>
           <a-range-picker
-            v-model:value="listQuery['createTimeMillis']"
+            @change="
+              (value, dateString) => {
+                if (!dateString[0] || !dateString[1]) {
+                  this.listQuery.createTimeMillis = ''
+                } else {
+                  this.listQuery.createTimeMillis = `${dateString[0]} ~ ${dateString[1]}`
+                }
+              }
+            "
             allowClear
             inputReadOnly
             :show-time="{ format: 'HH:mm:ss' }"
