c59d09995d5241ba95e3a394e1dfdd26f9d0ff83
bwcx_jzy <bwcx_jzy@163.com>
2024-02-22 21:32:05 +0800

    fix 前端请求编码准备 #I8Z1VJ

--- modules/common/src/main/java/org/dromara/jpom/common/transport/BodyRewritingRequestWrapper.java
+++ modules/common/src/main/java/org/dromara/jpom/common/transport/BodyRewritingRequestWrapper.java
@@ -20,7 +20,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package org.dromara.jpom.common.interceptor;
+package org.dromara.jpom.common.transport;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
