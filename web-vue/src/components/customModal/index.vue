be82eb511efba8814ca992d7174cbda98085a904
a20070322 <1031839775@qq.com>
2024-03-28 10:25:09 +0800

    优化操作日志弹窗

--- web-vue/src/components/customModal/index.vue
+++ web-vue/src/components/customModal/index.vue
@@ -11,23 +11,22 @@ import { CustomSlotsType } from 'ant-design-vue/es/_util/type'
 import { CSSProperties, defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'CustomTable',
+  name: 'CustomModal',
   props: initDefaultProps(modalProps(), {
     width: 520,
     confirmLoading: false,
     okType: 'primary'
   }),
   slots: Object as CustomSlotsType<{ default: any }>,
-  setup(props, { slots }) {
+  setup(props, { emit }) {
     const bodyStyle: CSSProperties = {
       maxHeight: 'calc(100vh - 196px )',
       overflowY: 'auto'
     }
-    console.log(slots, props)
     return {
       props,
-      slots,
-      bodyStyle
+      bodyStyle,
+      emit
     }
   }
 })
