fd93263e4379b25c58d1afabbd6eeedbde43efb8
bwcx_jzy <bwcx_jzy@163.com>
2024-03-30 15:04:20 +0800

    fix

--- script/mac-stop-docker.sh
+++ script/mac-stop-docker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#
+# Copyright (c) 2019 Of Him Code Technology Studio
+# Jpom is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+# 			http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+#
+
 
 task_list=`launchctl list | grep docker | grep -v 'com.docker.helper' | awk '{print $3}'`
 echo "docker task：${task_list}"
