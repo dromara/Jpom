c869916fff0a7e6b5bab61adee16669d79b38668
a20070322 <1031839775@qq.com>
2024-04-09 17:09:49 +0800

    i18n: pages.build.item 优化

--- web-vue/src/i18n/locales/en_US/pages/build/item.ts
+++ web-vue/src/i18n/locales/en_US/pages/build/item.ts
@@ -1,17 +1,17 @@
 export default {
   c: {},
   p: {
-    verificationCode: '构建信息',
-    editBuild: '编辑构建',
-    trigger: '触发器',
-    addBuild: '新增构建',
-    cancel: '取消',
+    buildInfo: 'Build information',
+    editBuild: 'Edit build',
+    trigger: 'Trigger',
+    addBuild: 'Add build',
+    cancel: 'Cancel',
     refreshWarning:
-      '如果当前构建信息已经在其他页面更新过，需要点击刷新按钮来获取最新的信息，点击刷新后未保存的数据也将丢失',
-    refresh: '刷新',
-    previousStep: '上一步',
-    nextStep: '下一步',
-    save: '保存',
-    saveAndBuild: '保存并构建'
+      'If the current build information has been updated on other pages, you need to click the refresh button to get the latest information. Unsaved data will be lost after clicking refresh.',
+    refresh: 'Refresh',
+    previousStep: 'Previous step',
+    nextStep: 'Next step',
+    save: 'Save',
+    saveAndBuild: 'Save and build'
   }
 }
