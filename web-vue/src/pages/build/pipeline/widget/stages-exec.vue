61922e686de5cbfdc1bc7f4066a160728cb5863c
bwcx_jzy <bwcx_jzy@163.com>
2024-04-10 11:06:57 +0800

    ⏸ segment: 流水线代码暂存

--- web-vue/src/pages/build/pipeline/widget/stages-exec.vue
+++ web-vue/src/pages/build/pipeline/widget/stages-exec.vue
@@ -20,25 +20,37 @@
         <template #help> </template>
 
         <a-space direction="vertical" style="width: 100%">
-          <a-row v-for="(item, index) in envList" :key="index">
-            <a-col :span="22">
-              <a-space direction="vertical" style="width: 100%">
-                <a-input v-model:value="item.key" :addon-before="`变量${index + 1}key`" placeholder="变量key" />
-                <a-input v-model:value="item.value" :addon-before="`变量${index + 1}值`" placeholder="变量值"
-              /></a-space>
-            </a-col>
-            <a-col :span="2">
-              <a-row type="flex" justify="center" align="middle">
-                <a-col>
-                  <MinusCircleOutlined style="color: #ff0000" @click="() => envList.splice(index, 1)" />
-                </a-col>
-              </a-row>
-            </a-col>
-          </a-row>
-          <a-divider style="margin: 5px 0" />
-        </a-space>
+          <a-space direction="vertical" style="width: 100%">
+            <template #split>
+              <a-divider type="horizontal" style="margin: 0" />
+            </template>
+            <a-row v-for="(item, index) in envList" :key="index">
+              <a-col :span="22">
+                <a-space direction="vertical" style="width: 100%">
+                  <a-input
+                    :value="item.key"
+                    :addon-before="`变量${index + 1}key`"
+                    placeholder="变量key"
+                    @change="(e) => envInputChange(index, 'key', e.target.value as string)" />
+                  <a-input
+                    :value="item.value"
+                    :addon-before="`变量${index + 1}值`"
+                    placeholder="变量值"
+                    @change="(e) => envInputChange(index, 'value', e.target.value as string)"
+                /></a-space>
+              </a-col>
+              <a-col :span="2">
+                <a-row type="flex" justify="center" align="middle">
+                  <a-col>
+                    <MinusCircleOutlined style="color: #ff0000" @click="() => envList.splice(index, 1)" />
+                  </a-col>
+                </a-row>
+              </a-col>
+            </a-row>
 
-        <a-button type="primary" size="small" @click="() => envList.push({})">新增参数</a-button>
+            <a-button type="primary" size="small" @click="() => envList.push({})">新增参数</a-button>
+          </a-space>
+        </a-space>
       </a-form-item>
       <a-form-item label="超时时间" name="timeout">
         <a-input-number v-model:value="useData.timeout" style="width: 100%" placeholder="执行脚本超时时间" />
@@ -74,37 +86,53 @@ watch(
     immediate: false
   }
 )
-const envObject2List = (obj: any) => {
-  const list: Array<{ key: string; value: string }> = []
-  for (let key in obj) {
-    list.push({
-      key: key,
-      value: obj[key]
-    })
-  }
-  return list
-}
-
-const envList = ref<Array<{ key?: string; value?: string }>>(envObject2List(props.data.env))
+// 暂存变量列表
+const envList = ref<Array<{ key?: string; value?: string }>>([])
 
 watch(
   () => props.data,
   (val) => {
     useData.value = val
-    const newEnv = envObject2List(val.env)
-    // 避免组建循环调用
-    if (JSON.stringify(newEnv) != JSON.stringify(envList.value)) {
-      envList.value = newEnv
+    const propEnv = props.data.env
+    const putArray = []
+    for (let key in propEnv) {
+      const find = envList.value
+        .map((item) => {
+          if (item.key === key) {
+            item.value = propEnv[key]
+          }
+          return item
+        })
+        .filter((item) => item.key === key).length
+      if (!find) {
+        putArray.push({
+          key: key,
+          value: propEnv[key]
+        })
+      }
     }
+    // console.log(putArray)
+    envList.value.push(...putArray)
+
+    // putArray.forEach((item) => envList.value.push(item))
   },
   {
     deep: true,
-    immediate: false
+    immediate: true
   }
 )
+
+const envInputChange = (index: number, key: any, value: string) => {
+  // if (isKey) {
+  envList.value[index][key] = value
+  // } else {
+  // envList.value[index].value = value
+  // }
+}
+
 // 监听环境变量变化
 watch(
-  () => envList.value.map((item: any) => item.key + ':' + item.value),
+  () => envList.value.map((item: any, index) => index + '=' + item.key + ':' + item.value),
   () => {
     const env: any = {}
     envList.value.forEach((element) => {
@@ -113,17 +141,11 @@ watch(
       }
     })
     useData.value = { ...useData.value, env: env }
-    // console.log(env)
+    //console.log(env, val)
     //emit('update:data', useData.value)
   },
   {
     immediate: false
   }
 )
-
-// const envList = computed(() => {
-//   if (!useData || !useData.env) {
-//     return []
-//   }
-// })
 </script>
