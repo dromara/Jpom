882ff4131baae70690733dd6417fbc8196853746
a20070322 <1031839775@qq.com>
2024-04-10 09:24:44 +0800

    i18n: pages.build.log

---
+++ web-vue/src/i18n/locales/zh-CN/pages/build/log.ts
@@ -0,0 +1,12 @@
+export default {
+  c: {
+    currentStatus: '当前状态：'
+  },
+  p: {
+    buildLog: '构建日志',
+    statusMsg: '状态消息：',
+    unknownStatus: '未知状态',
+    buildId: '构建ID：',
+    download: '下载'
+  }
+}
