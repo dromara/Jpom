384a73189520fac45e8fd686a54ac4ac11b6b3f1
a20070322 <1031839775@qq.com>
2024-04-28 13:11:15 +0800

    暂存

--- web-vue/src/i18n/locales/en_US/pages/dispatch/index.ts
+++ web-vue/src/i18n/locales/en_US/pages/dispatch/index.ts
@@ -3,11 +3,12 @@ import logRead from './logRead'
 import start from './start'
 import status from './status'
 import whiteList from './white-list'
-
+import list from './list'
 export default {
   log,
   logRead,
   start,
   status,
-  whiteList
+  whiteList,
+  list
 }
--- web-vue/src/i18n/locales/en_US/pages/dispatch/log.ts
+++ web-vue/src/i18n/locales/en_US/pages/dispatch/log.ts
@@ -1,29 +1,29 @@
 export default {
   c: {
-    content1: 'Unknown',
-    content2: 'Distribution Result'
+    unknown: 'Unknown',
+    result: 'Distribution result'
   },
   p: {
-    content1: 'No distribution logs',
-    content2: 'Select a node',
-    content3: 'Distribute item',
-    content4: 'Select a status',
-    content5: 'Click the button with Ctrl or Alt/Option key to quickly return to the first page',
-    content6: 'Search',
-    content7: 'Related data:',
-    content8: 'Details',
-    content9: 'Detail information',
-    content10: 'Distribution item ID',
-    content11: 'Node name',
-    content12: 'Project ID',
-    content13: 'Distribution method',
-    content14: 'Distribution duration',
-    content15: 'File size',
-    content16: 'Start time',
-    content17: 'End time',
-    content18: 'Distribution status message',
-    content19: 'Operator',
-    content20: 'Status',
-    content21: 'Operation'
+    noLogs: 'No distribution logs available',
+    selectNode: 'Please select a node',
+    distributeProject: 'Distribute project',
+    selectStatus: 'Please select a status',
+    goToFirstPage: 'Hold Ctrl or Alt/Option key and click the button to quickly go to the first page',
+    search: 'Search',
+    relatedData: 'Related data:',
+    details: 'Details',
+    info: 'Detailed information',
+    projectId: 'Distribution project ID',
+    nodeName: 'Node name',
+    distributeId: 'Project ID',
+    method: 'Distribution method',
+    duration: 'Distribution duration',
+    fileSize: 'File size',
+    startTime: 'Start time',
+    endTime: 'End time',
+    statusMessage: 'Distribution status message',
+    operator: 'Operator',
+    status: 'Status',
+    action: 'Action'
   }
 }
