141a7e490ab0ea75854526bbff5c0127bf476d3d
bwcx_jzy <bwcx_jzy@163.com>
2024-06-12 10:35:32 +0800

    ⏸ segment(all): pre i18n

---
+++ modules/agent-transport/agent-transport-common/src/main/java/org/dromara/jpom/transport/i18n/IMessageUtil.java
@@ -0,0 +1,10 @@
+package org.dromara.jpom.transport.i18n;
+
+/**
+ * @author bwcx_jzy1
+ * @since 2024/6/11
+ */
+public interface IMessageUtil {
+
+    String get(String key);
+}
---
+++ modules/agent-transport/agent-transport-common/src/main/java/org/dromara/jpom/transport/i18n/MessageUtil.java
@@ -0,0 +1,36 @@
+package org.dromara.jpom.transport.i18n;
+
+import cn.hutool.core.lang.Singleton;
+import cn.hutool.core.util.ServiceLoaderUtil;
+
+/**
+ * @author bwcx_jzy1
+ * @since 2024/6/11
+ */
+public class MessageUtil {
+
+    /**
+     * 获得单例的 TransportServer
+     *
+     * @return 单例的 TransportServer
+     */
+    public static String get(String key) {
+        return Singleton.get(IMessageUtil.class.getName(), MessageUtil::doCreate).get(key);
+    }
+
+
+    /**
+     * 根据用户引入的拼音引擎jar，自动创建对应的拼音引擎对象<br>
+     * 推荐创建的引擎单例使用，此方法每次调用会返回新的引擎
+     *
+     * @return {@code EngineFactory}
+     */
+    private static IMessageUtil doCreate() {
+        final IMessageUtil engine = ServiceLoaderUtil.loadFirstAvailable(IMessageUtil.class);
+        if (null != engine) {
+            return engine;
+        }
+
+        throw new RuntimeException("No jpom IMessageUtil jar found ! Please add one of it to your project !");
+    }
+}
