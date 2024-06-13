da8af06f25b9660c52382636909080b0b2a80be4
小吾立 <fyjc_wlj@163.com>
2024-06-13 16:36:40 +0800

    ⏸ segment(all): pre i18n

--- modules/agent/src/test/java/TestJps.java
+++ modules/agent/src/test/java/TestJps.java
@@ -7,7 +7,6 @@
  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
  * See the Mulan PSL v2 for more details.
  */
-
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.lang.PatternPool;
 import cn.hutool.core.text.StrSplitter;
