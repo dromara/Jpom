28bc307aec7fc5cbb8067d3f8ed12f1ba208566d
a20070322 <1031839775@qq.com>
2024-06-18 14:14:17 +0800

    chore i18n 动态加载

--- web-vue/src/i18n/index.ts
+++ web-vue/src/i18n/index.ts
@@ -9,63 +9,47 @@
 ///
 
 import { createI18n } from 'vue-i18n'
-// @ts-ignore 兼容i8n_tools插件
-import { zhCN, enUS } from './locales'
 
-import antdZhCN from 'ant-design-vue/es/locale/zh_CN'
-import antdEnUS from 'ant-design-vue/es/locale/en_US'
-
-/** 以中文为主,定义语言类型 */
-export type I18nLocaleType = typeof zhCN
-
-// 深层递归合并，防止出现其他语言未覆盖问题
-const deepMerge = (...objects: Record<string, any>[]) => {
-  const result: Record<string, any> = {}
-  for (const obj of objects) {
-    for (const key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        if (typeof obj[key] === 'object' && obj[key] !== null) {
-          result[key] = deepMerge(result[key] || {}, obj[key])
-        } else {
-          result[key] = obj[key]
-        }
-      }
-    }
-  }
-  return result
+type LangType = {
+  antd: () => Promise<any>
+  local: () => Promise<any>
 }
 
-const usLocal = deepMerge(zhCN, enUS)
-
-export const lang: { [key: string]: any } = {
+export const langDict: { [key: string]: LangType } = {
   'zh-cn': {
-    antd: antdZhCN,
-    local: zhCN
-  },
-  'zh-CN': {
-    antd: antdZhCN,
-    local: zhCN
+    antd: () => import(/* @vite-ignore  */ 'ant-design-vue/es/locale/zh_CN'),
+    local: () => import(/* @vite-ignore  */ './locales/zh_cn.json')
   },
   'en-us': {
-    antd: antdEnUS,
-    local: usLocal
-  },
-  'en-US': {
-    antd: antdEnUS,
-    local: usLocal
+    antd: () => import(/* @vite-ignore  */ 'ant-design-vue/es/locale/en_US'),
+    local: () => import(/* @vite-ignore  */ './locales/en_us.json')
   }
 }
 
-const i18n = createI18n<I18nLocaleType>({
+const i18n = createI18n<Record<string, any>>({
   legacy: false,
-  locale: 'zh-cn', //'en-us', // 默认显示语言
-  messages: Object.keys(lang).reduce((pre: { [key: string]: any }, key: string) => {
-    const { local } = lang[key]
-    pre[key] = local
-    return pre
-  }, {}),
+  // locale: 'zh-cn', // 默认显示语言
   warnHtmlMessage: false
 })
+
+export const changeLang = async (langKey: string) => {
+  langKey = langKey.toLowerCase()
+  const lang = langDict[langKey || 'zh-cn']
+  await loadLanguageAsync(langKey, lang)
+  return await lang.antd()
+}
+
+export const setI18nLanguage = (langKey: string) => {
+  // @ts-ignore
+  i18n.global.locale = langKey
+  return langKey
+}
+export const loadLanguageAsync = async (langKey: string, langDict: LangType) => {
+  const langFile = await langDict.local()
+  // 动态加载对应的语言包
+  i18n.global.setLocaleMessage(langKey, langFile)
+  return setI18nLanguage(langKey) // 返回并且设置
+}
 
 export default i18n
 
