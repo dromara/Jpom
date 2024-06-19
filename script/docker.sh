156db6ca5cd9bcd07ba9705771fab774f3148c88
bwcx_jzy <bwcx_jzy@163.com>
2024-06-19 10:22:37 +0000

    commit release 2.11.7

--- script/docker.sh
+++ script/docker.sh
@@ -18,7 +18,7 @@
 # docker buildx create --use
 
 # 服务端
-docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le,linux/arm64/v8 -t jpomdocker/jpom:2.11.6.1 -t jpomdocker/jpom:latest -f ./modules/server/DockerfileRelease --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le,linux/arm64/v8 -t jpomdocker/jpom:2.11.7.1 -t jpomdocker/jpom:latest -f ./modules/server/DockerfileRelease --push .
 
 
 
@@ -27,7 +27,7 @@ docker buildx build --platform linux/amd64,linux/arm64,linux/ppc64le,linux/arm64
 #docker buildx build --platform linux/amd64,linux/arm64 -t jpomdocker/jpom:latest -f ./modules/server/DockerfileRelease --push .
 
 # docker logs --tail="100" jpom-server
-# docker run -d -p 2122:2122 --name jpom-server -v /etc/localtime:/etc/localtime:ro -v jpom-server-vol:/usr/local/jpom-server jpomdocker/jpom:mac-arm-2.11.6.1
+# docker run -d -p 2122:2122 --name jpom-server -v /etc/localtime:/etc/localtime:ro -v jpom-server-vol:/usr/local/jpom-server jpomdocker/jpom:mac-arm-2.11.7.1
 # docker run -d -p 2122:2122 --name jpom-server -v D:/home/jpom-server/logs:/usr/local/jpom-server/logs -v D:/home/jpom-server/data:/usr/local/jpom-server/data -v D:/home/jpom-server/conf:/usr/local/jpom-server/conf jpomdocker/jpom
 # docker stop jpom-server
 # docker rm jpom-server
