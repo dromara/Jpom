3cef8216ea05dda2c490972b78d12efc5453d825
a20070322 <1031839775@qq.com>
2024-06-19 10:31:02 +0800

    feat 优化切换语言相关

--- web-vue/src/stores/guide.ts
+++ web-vue/src/stores/guide.ts
@@ -1,4 +1,4 @@
-import { t } from '@/i18n'
+import { supportLang, t } from '@/i18n'
 ///
 /// Copyright (c) 2019 Of Him Code Technology Studio
 /// Jpom is licensed under Mulan PSL v2.
@@ -265,8 +265,7 @@ export const useGuideStore = defineStore('guide', {
     getLocale: (state) => {
       return () => {
         const locale = state.guideCache.locale || navigator.language
-        const array = ['zh-cn', 'en-us', 'zh_cn', 'en_us']
-        if (array.includes(locale.toLowerCase())) {
+        if (supportLang.map((item) => item.value).includes(locale.toLowerCase())) {
           // 避免非法字符串
           return locale
         }
