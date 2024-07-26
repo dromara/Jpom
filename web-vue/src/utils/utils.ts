c71b629e59d26706369b9ec50f42581251a0ea05
小吾立 <fyjc_wlj@163.com>
2024-07-26 13:27:17 +0800

    🐞 fix(web): 部分组件层级错乱

--- web-vue/src/utils/utils.ts
+++ web-vue/src/utils/utils.ts
@@ -17,9 +17,17 @@ export function getHashQuery() {
   return querys
 }
 
-const zIndexStart = 1009
+// root 元素
+const root = document.documentElement
+// 获取计算后的样式
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+const style = getComputedStyle(root)
+const zIndexStart = Number(style.getPropertyValue('--increase-z-index'))
 let incCount = 0
 
 export function increaseZIndex() {
-  return zIndexStart + incCount++
+  const useIndex = zIndexStart + incCount++
+  // 设置全局变量,避免全局提示，下拉框，下拉菜单显示错位
+  document.documentElement.style.setProperty('--increase-z-index', String(zIndexStart + incCount))
+  return useIndex
 }
